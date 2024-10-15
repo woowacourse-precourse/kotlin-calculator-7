5efdc3d742e0e87c6ebb6d33c00f2e74be6e8600
MEDA-DESKTOP\Meda <med4ndro@gmail.com>
2024-10-16 02:30:00 +0900

    feat: 커맨드를 검증하여 구분자를 저장하는 Set 생성
    - CommandVerifyer.kt에 문자열에 커맨드가 포함되어 있는지 검증하는 코드 작성
    - DelimiterParser.kt에 문자열을 입력받아 구분자를 Set으로 반환하는 코드 작성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,5 +2,6 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    val input = readLine()  // 문자열 입력받기
+    val input: String = readLine()  // 문자열 입력 받기
+    val delimiter: Set<Char> = DelimiterParser.getDelimiter(input) //구분자 Set 추출 하기
 }
---
+++ src/main/kotlin/calculator/DelimiterParser.kt
@@ -0,0 +1,23 @@
+package calculator
+
+// 구분자를 추출 및 반환하는 작업을 하는 객체
+object DelimiterParser {
+
+    // 구분자 추출 메서드
+    private fun parse(input: String): Set<Char> {
+        val delimiterSet: MutableSet<Char> = mutableSetOf(',', ':') // 기본 구분자로 이루어진 가변 Set
+
+        // input 문자열에 커맨드의 존재 여부를 체크한뒤 커맨드에 해당하는 문자를 Set에 추가하기
+        if (CommandVerifyer.verifyCommand(input)) {
+            delimiterSet.add(input[2])
+        }
+
+        return delimiterSet
+    }
+
+    // input에서 추출된 구분자를 반환하는 메소드
+    fun getDelimiter(input: String): Set<Char> {
+        return parse(input)
+    }
+
+}
