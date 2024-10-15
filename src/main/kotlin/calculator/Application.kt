6858a21e36d2bc337732a5e3f226c8e76c9d7270
윤승일(Seung-Il Yoon) <liqurt@gmail.com>
2024-10-15 17:53:13 +0900

    feat(input) : 문자열 입력
    문자열을 입력 받는다

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,11 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    // 문자열 입력
+    println("덧셈할 문자열을 입력해 주세요.")
+    val str = Console.readLine()
+    Console.close()
+
 }
