ec926bda3977ec3a93a552fafac45c2283af27ad
yrsel <xxknu88@gmail.com>
2024-10-15 20:57:45 +0900

    feat(Application): 커스텀 구분자 추출

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,11 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 private val CUSTOM_DELIMITER_BEFORE_FLAG = "//"
 private val CUSTOM_DELIMITER_AFTER_FLAG = "\\n"
+private val CUSTOM_DELIMITER_START_INDEX = 2
+private var delimiter = ",:"
 
 fun main() {
 }
@@ -9,3 +13,8 @@ fun main() {
 private fun hasCustomDelimiters(input: String) =
     input.startsWith(CUSTOM_DELIMITER_BEFORE_FLAG) &&
             input.indexOf(CUSTOM_DELIMITER_AFTER_FLAG) != -1
+
+private fun setCustomDelimiter(input: String): String {
+    val customDelimiterEndIndex = input.indexOf(CUSTOM_DELIMITER_AFTER_FLAG)
+    return input.substring(CUSTOM_DELIMITER_START_INDEX, customDelimiterEndIndex)
+}
