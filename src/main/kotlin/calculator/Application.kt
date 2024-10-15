d59d599b9516a5de6f79bd1e82239092141d6073
ChoHJin <cjc9739@naver.com>
2024-10-15 17:31:29 +0900

    feat : 커스텀 구분자 지정 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,12 +3,20 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
 
     val inputString = readLine()
+    val result : Int
 
-    val result = inputString.split(',', ':').sumOf { it.toInt() }
+    if(inputString.startsWith("//")) {
+        val endIndex = inputString.indexOf("\\n")
+
+        val customDelimiter = inputString.substring(2, endIndex)
+
+        val numbers = inputString.substring(endIndex + 2)
+
+        result = numbers.split(customDelimiter).sumOf { it.toInt() }
+    } else result = inputString.split(',', ':').sumOf { it.toInt() }
 
     println(result)
 }
