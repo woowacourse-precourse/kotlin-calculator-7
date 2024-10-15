efa6b0e0db1a641a22a7c07ac60dc4c0244edcd0
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-15 20:20:46 +0900

    feat: 두 개의 수 합산
    두 개의 수를 더하기 연산을 한 결과값을 String 반환
    합산한 수가 현재 사용하는 자료형을 넘어갈 경우 자료형 타입 플래그를 바꿔서 메모리 최적화

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,26 +1,104 @@
 package calculator
 
 import camp.nextstep.edu.missionutils.Console
+import java.math.BigInteger
+import kotlin.jvm.Throws
+
+enum class TypeState {
+    INT, LONG, BIG_INTEGER;
+
+    fun upgradeType(): TypeState {
+        return when (this) {
+            INT -> LONG
+            LONG -> BIG_INTEGER
+            BIG_INTEGER -> BIG_INTEGER
+        }
+    }
+}
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
 
     val info = Console.readLine()
-    val separatorSet = mutableSetOf<Char>()
+    val separatorList: MutableSet<Char> = mutableSetOf()
 
-    separatorSet.add(',')
-    separatorSet.add(':')
-    val cursor: Int
+    separatorList.add(',')
+    separatorList.add(':')
+    val startPoint: Int
 
     try {
-        cursor = findSeparator(info, separatorSet)
+        startPoint = findSeparator(info, separatorList)
     } catch (e: IllegalArgumentException) {
         Console.close(); return
     }
 
-
-
     Console.close()
+}
+
+@Throws(IllegalArgumentException::class)
+fun sum(total: String, sum: String, flag: TypeState): Pair<String, TypeState> {
+    if (flag == TypeState.BIG_INTEGER) {
+        val totalConvert = BigInteger(total)
+        val operand = BigInteger(sum)
+        val result = totalConvert + operand
+
+        return Pair(result.toString(), TypeState.BIG_INTEGER)
+    }
+
+    var currentState = flag
+
+    while (currentState != TypeState.BIG_INTEGER) {
+        var maxStr = when (currentState) {
+            TypeState.INT -> Int.MAX_VALUE.toString()
+            TypeState.LONG -> Long.MAX_VALUE.toString()
+            else -> ""
+        }
+        val maxLength = Integer.max(sum.length, maxStr.length)
+        val compareSum = sum.padStart(maxLength, '0')
+        maxStr = maxStr.padStart(maxLength, '0')
+
+        /**
+         * T.MAX_VALUE >= total + sum 인 경우 현재 타입으로 연산하고 그렇지 않을 경우 타입을 올려줘야 하지만 우변이 범위를 넘어가 정상적인 연산이 불가능할 수 있음
+         * T.MAX_VALUE - sum >= total 의 식을 사용할 시 타입 내에서 안전하게 비교할 수 있음
+         */
+        if (maxStr >= compareSum) {
+            when (currentState) {
+                TypeState.INT -> {
+                    val totalConvert: Int = total.toInt()
+                    val sumConvert: Int = sum.toInt()
+
+                    if (Int.MAX_VALUE - sumConvert >= totalConvert) {
+                        break
+                    }
+                }
+                TypeState.LONG -> {
+                    val totalConvert: Long = total.toLong()
+                    val sumConvert: Long = sum.toLong()
+
+                    if (Long.MAX_VALUE - sumConvert >= totalConvert) {
+                        break
+                    }
+                }
+                else -> {}
+            }
+        }
+
+        currentState = currentState.upgradeType()
+    }
+
+    val resultStr = when (currentState) {
+        TypeState.INT -> {
+            (total.toInt() + sum.toInt()).toString()
+        }
+        TypeState.LONG -> {
+            (total.toLong() + sum.toLong()).toString()
+        }
+        TypeState.BIG_INTEGER -> {
+            (BigInteger(total) + BigInteger(sum)).toString()
+        }
+    }
+
+    return Pair(resultStr, currentState)
 }
 
 @Throws(IllegalArgumentException::class)
