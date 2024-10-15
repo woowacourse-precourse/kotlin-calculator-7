94c162399a18d9c40319679ce457db3761abda47
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-10-15 18:51:36 +0900

    ✨ Feat: 기본 구조 설정
    Index로 구분자 추출

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,36 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = readLine() ?: ""
+
+    try {
+        val result = add(input)
+        println("결과 : $result")
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+}
+
+fun add(input: String): Int {
+    if (input.isEmpty()) return 0
+
+    var delimiters = listOf(",", ":")
+    var numbers = input
+
+    // 입력 문자열이 "//"로 시작하는 경우
+    if (input.startsWith("//")) {
+        val customDelimiter = input[2].toString()
+        delimiters = delimiters + customDelimiter
+
+        numbers = input.substring(5)
+    }
+
+    val tokens = numbers.split(*delimiters.toTypedArray())
+
+    val sum = tokens.map {
+        it.toIntOrNull() ?: throw IllegalArgumentException("Error")
+    }.sum()
+
+    return sum
 }
