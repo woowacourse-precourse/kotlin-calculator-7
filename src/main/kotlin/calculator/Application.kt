8551e709b339b856868e2f354e6a505e5bfd94c9
ChoHJin <cjc9739@naver.com>
2024-10-15 19:24:29 +0900

    feat : 예외처리 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,15 +8,48 @@ fun main() {
     val inputString = readLine()
     val result : Int
 
-    if(inputString.startsWith("//")) {
+    if(inputString.isEmpty()) {
+        throw IllegalArgumentException("Empty String.")
+    }
+
+    if(inputString.startsWith("//")) {  // 커스텀 구분자
         val endIndex = inputString.indexOf("\\n")
+        if(endIndex == -1) throw IllegalArgumentException()    //\n 이 없는 경우
 
         val customDelimiter = inputString.substring(2, endIndex)
+        if(customDelimiter.isEmpty()) throw IllegalArgumentException()     //구분자가 없는 경우
+
 
         val numbers = inputString.substring(endIndex + 2)
+        if(numbers.isEmpty()) throw IllegalArgumentException()  //숫자가 없는 경우
 
-        result = numbers.split(customDelimiter).sumOf { it.toInt() }
-    } else result = inputString.split(',', ':').sumOf { it.toInt() }
+        val numberList = numbers.split(customDelimiter)
+        if(numberList.any { it.isEmpty() }) throw IllegalArgumentException()    //구분자 사이에 숫자가 없는 경우
 
-    println(result)
+        validateNumber(numberList)
+        result = numberList.sumOf { it.toInt() }
+    } else {    //기본 구분자
+        val delimiters = listOf(',',':')
+
+        val numberList = if(delimiters.any {it in inputString}) {
+            val numbers = inputString.split(',', ':')
+
+            if(numbers.any {it.isEmpty()}) throw IllegalArgumentException()     //구분자 사이에 숫자가 없는 경우
+
+            numbers
+        } else listOf(inputString)
+
+        validateNumber(numberList)
+        result = numberList.sumOf { it.toInt() }
+    }
+
+    println("결과 : $result")
+}
+
+fun validateNumber(numbers : List<String>) {    //숫자 체크 함수
+    for(number in numbers) {
+        if(number.isNotEmpty() && !number.all { it.isDigit() }) {
+            throw IllegalArgumentException()
+        }
+    }
 }
