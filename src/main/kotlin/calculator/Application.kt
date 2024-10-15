4e35d35cb3a63d0fe3d59b73942121c98b942762
ChoHJin <cjc9739@naver.com>
2024-10-15 16:17:56 +0900

    feat : 문자열 입력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,14 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
     // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    val inputString = readLine()
+
+
+
+
 }
