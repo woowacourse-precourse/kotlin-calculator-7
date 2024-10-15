b3221a82c79033de5e2bf032d34db7e33f3954d1
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-15 19:02:30 +0900

    feat: 문자열에 커스텀 구분자 추출
    커스텀 구분자가 있는지 확인하고, 있다면 구분자 집합에 추가한다.
    "//"로 시작했지만 "\n"으로 닫지 않는 경우의 잘못된 입력값 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,50 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    val info = Console.readLine()
+    val separatorSet = mutableSetOf<Char>()
+
+    separatorSet.add(',')
+    separatorSet.add(':')
+    val cursor: Int
+
+    try {
+        cursor = findSeparator(info, separatorSet)
+    } catch (e: IllegalArgumentException) {
+        Console.close(); return
+    }
+
+
+
+    Console.close()
+}
+
+@Throws(IllegalArgumentException::class)
+fun findSeparator(inputString: String, separatorSet: MutableSet<Char>): Int {
+    if (inputString.length < 4) {
+        return 0
+    }
+
+    val opener = "//"
+    val closer = """\n"""
+    val openerCheck = inputString.slice(0..1) == opener
+
+    if (!openerCheck) {
+        return 0
+    }
+
+    val separator = inputString[2]
+    val closerCheck = inputString.slice(3..4) == closer
+
+    if (!closerCheck) {
+        throw IllegalArgumentException()
+    }
+
+    separatorSet.add(separator)
+
+    return 5
 }
