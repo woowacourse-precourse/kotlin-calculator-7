a86fcb45818d6f5dd0d2eaaf1d5ea0cf9bfeb55c
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-16 18:04:08 +0900

    feat(Validator): Validator 클래스 생성
    TODO :
    - 문자열 유효성 검증하는 기능
    - 커스텀 구분자를 반환하는 기능

---
+++ src/main/kotlin/calculator/Validator.kt
@@ -0,0 +1,12 @@
+package calculator
+
+class Validator {
+    fun validateString(inputString: String): String {
+        // TODO: 문자열의 유효성 검증
+        return returnCustomDelimiter(inputString)
+    }
+
+    private fun returnCustomDelimiter(inputString: String): String {
+        // TODO: 커스텀 구분자 리턴, 없으면 "" 리턴
+    }
+}
