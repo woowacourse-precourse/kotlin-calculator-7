75204ccfb2851ec6039a94747ad7b8e7a6accf9c
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-10-17 05:55:30 +0900

    feat(readline()) : 입력 값 받기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,11 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
+    //입력 안내 메세지 출력
     println("덧셈할 문자열을 입력해 주세요.")
+
+    //입력 값 받기
+    val input = Console.readLine()
 }
