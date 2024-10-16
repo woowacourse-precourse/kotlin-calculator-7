bce8b6218ddd8a3c118b50cb0bd483e41df567b7
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-16 18:49:18 +0900

    feat(Validator): 첫 문자가 '/' 인 경우 앞 5문자의 유효성을 검증하는 기능 추가
    - "//" 인 경우 앞 5문자에만 유효성을 검증함
    - 유효성을 검증하고, 유효하다면 커스텀 구분자를 리턴해 저장함
    - 커스텀 문자열이 있는 경우에, checkString <- checkString.subString(5, checkString.length)
    TODO: 남은 부분에 대해 유효성을 검증해야함

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -1,12 +1,46 @@
 package calculator
 
 class Validator {
-    fun validateString(inputString: String): String {
+    fun validateString(inputString: String): Boolean {
         // TODO: 문자열의 유효성 검증
-        return returnCustomDelimiter(inputString)
+
+        var checkString = inputString
+
+        var delimiter = ",|:"
+        if (checkString[0] == '/') {
+            // 첫 문자가 '/' 인 경우 앞 5문자의 유효성을 검증함.
+            val customDelimiter = validateCustomDelimiter(checkString)
+            delimiter = "$delimiter|$customDelimiter"
+            checkString = checkString.substring(5, checkString.length)
+        }
+
+        for (char in checkString) {
+        }
     }
 
-    private fun returnCustomDelimiter(inputString: String): String {
-        // TODO: 커스텀 구분자 리턴, 없으면 "" 리턴
+    // 앞 5문자의 유효성을 검증하는 기능
+    fun validateCustomDelimiter(inputString: String): String {
+        val slashIndex = inputString.substring(0, 2) == "//"
+        val enterIndex = inputString.substring(3, 5) == "\\n"
+        val customDelimiter = inputString[2]
+
+        if (slashIndex and enterIndex and !customDelimiter.isDigit()) {
+            return customDelimiter.toString()
+        } else {
+            // 첫 두 문자가 "//" 가 아니고,
+            // 3번째 문자가 숫자가 아니고,
+            // 4,5 번째 문자가 각각 '\' , 'n' 이 아니면,
+            // IllegalArgumentException() 발생
+            throw IllegalArgumentException()
+        }
     }
+
+    private fun Char.isNotValid(delimiterList: List<String>): Boolean {
+        val isValidDigit = this.isDigit()
+        val isValidDelimiter = this.isDelimiter(delimiterList)
+
+        return !(isValidDigit && isValidDelimiter)
+    }
+
+    private fun Char.isDelimiter(delimiterList: List<String>): Boolean = this.toString() in delimiterList
 }
