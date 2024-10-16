951d45b9f56a16cf3353fe3393f2ba90ff4c10ed
UiHyeon <hyunkim6790@gmail.com>
2024-10-16 17:35:07 +0900

    feat: 문자열 구분 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,7 +5,20 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val str = Console.readLine()
 
+    val num = splitString(str)
+
     println(str)
+    println(num)
 
     Console.close()
+}
+
+// 방법1: 문자열을 구분자로 나누는 함수
+fun splitString(str: String): List<Int> {
+    return str.split(",", ":").map { it.toInt() }
+}
+
+// 방법2: 문자열에서 숫자만 필터링하는 함수
+fun filteringString(str: String) {
+
 }
