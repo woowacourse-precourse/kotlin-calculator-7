7bd6cd936ac7129b0e50dbc257c7a64067c4dea3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-16 20:08:04 +0900

    feat(Calculator): 문자열을 분리하는 기능 추가
    - Validator 클래스를 사용해 유효성한 문자열과 커스텀 구분자를 받고(있다면) 이를 분리한다.

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,10 +2,29 @@ package calculator
 
 class Calculator {
     fun calculateString(inputString: String) {
-        // 1. 커스텀 구분자가 있다면 커스텀 구분자를 추가하고, 문자열을 분리한다. divide
-        // 2. 구분자를 기준으로 각 숫자들을 분리한다. split
-        // 3. 각 숫자들의 총 합을 구한다.
-        // 4. 결과를 출력한다.
+        // 1. 커스텀 구분자가 있다면 커스텀 구분자를 추가하고, 각 문자를 분리한다.
+        val splitList = splitString(inputString)
+
+        // 2. 각 숫자들의 총 합을 구한다.
+        // 3. 결과를 출력한다.
+    }
+
+    fun splitString(inputString: String): List<String> {
+        var delimiter = "[,:]"
+        if (inputString[0] == '/') {
+            val customDelimiter = validator.validateCustomDelimiter(inputString)
+            delimiter =
+                if (customDelimiter.isNotEmpty()) {
+                    "$delimiter|$customDelimiter"
+                } else {
+                    delimiter
+                }
+        }
+
+        val checkString = validator.validateString(inputString)
+        val splitList = checkString.split(delimiter.toRegex())
+
+        return splitList
     }
 
     companion object {
