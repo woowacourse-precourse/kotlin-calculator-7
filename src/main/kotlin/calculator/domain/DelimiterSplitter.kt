7e696a61aa5ffbb4b8c76b1ef035b8027fbf92a0
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-17 01:35:23 +0900

    Feat: 커스텀 구분자 사용 선언 포맷 확인 기능 구현

---
+++ src/main/kotlin/calculator/domain/DelimiterSplitter.kt
@@ -0,0 +1,19 @@
+package calculator.domain
+
+class DelimiterSplitter(
+    private val value: String,
+) {
+    init {
+        require(customDelimiterFormatCheck())
+    }
+
+    // 커스텀 구분자 사용 선언 포맷 확인
+    private fun customDelimiterFormatCheck(): Boolean = value.startsWith(CUSTOM_DELIMITER_FRONT) == value.contains(CUSTOM_DELIMITER_BACK)
+
+    companion object {
+        private const val COMMA_DELIMITER = ","
+        private const val COLON_DELIMITER = ":"
+        private const val CUSTOM_DELIMITER_FRONT = "//"
+        private const val CUSTOM_DELIMITER_BACK = "\\n"
+    }
+}
