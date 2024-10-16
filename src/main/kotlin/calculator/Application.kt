0daa6d4240b81af1b376dce50283ec57745cad01
junseo511 <prime54@g.skku.edu>
2024-10-16 16:40:37 +0900

    feat: 입력값 유효성 검증 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,30 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine().split(CUSTOM_DIVIDER_PREFIX, CUSTOM_DIVIDER_SUFFIX)
+
+    if (!checkInputValid(input)) throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
 }
+
+private fun checkInputValid(input: List<String>): Boolean {
+    return when {
+        input.size > CUSTOM_DIVIDER_EXCEEDING_COUNT -> false
+
+        input.size == CUSTOM_DIVIDER_NONE_COUNT -> true
+
+        input.first().isNotBlank() -> false
+
+        else -> true
+    }
+}
+
+private const val INVALID_INPUT_MESSAGE = "유효하지 않은 입력입니다."
+
+private const val CUSTOM_DIVIDER_PREFIX = "//"
+private const val CUSTOM_DIVIDER_SUFFIX = "\\n"
+
+private const val CUSTOM_DIVIDER_EXCEEDING_COUNT = 3
+private const val CUSTOM_DIVIDER_NONE_COUNT = 1
