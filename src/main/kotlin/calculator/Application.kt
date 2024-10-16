3bf9eb8cf29c2fea588ecdc7a091916a5b4b9d38
eeazzy <khs2498@gmail.com>
2024-10-16 21:31:12 +0900

    feat(calculator): 빈 문자열 입력 시 0을 반환하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,6 +10,10 @@ fun main() {
 }
 
 fun calculateSum(input: String): Int { // 계산 함수
+    if (input.isBlank()) { // 빈 문자열일 경우 0을 반환
+        return 0
+    }
+
     val delimiters = arrayOf(",", ":") // 구분자 정의
     val numbers = input.split(*delimiters).map { it.toInt() } // 구분자로 숫자 분리
 
