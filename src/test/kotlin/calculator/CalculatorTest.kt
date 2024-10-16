2d68e3824c5f8d7acb0953d8c35e12f285604f6f
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-16 22:39:46 +0900

    test(calculatorTest): 테스트 인수와 결과 타입 변경
    타입이 나뉘어지면 각 값에 따라 이해하기 힘들어져서 BigDemical로 통일해서 구현

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -15,28 +15,33 @@ class CalculatorTest {
 
     @Test
     fun `더하기 예외 테스트`() {
-        val result = calculator.sum(listOf(1, 8, 12))
-        val expect = 21
+        val result = calculator.sum(listOf("1", "8", "12").map { it.toBigDecimal() })
+        val expect = "21".toBigDecimal()
         assertEquals(expect, result)
     }
 
     @Test
     fun `더하기(마이너스 포함) 예외 테스트`() {
-        val result = calculator.sum(listOf(1, 8, -12))
-        val expect = -3
+        val result = calculator.sum(listOf("1", "8", "-12").map { it.toBigDecimal() })
+        val expect = "-3".toBigDecimal()
         assertEquals(expect, result)
     }
 
     @Test
     fun `더하기(소수점 포함) 예외 테스트`() {
-        val result = calculator.sum(listOf(1, 8, -9.5))
-        val expect = -0.5
+        val result = calculator.sum(listOf("1", "8", "-9.5").map { it.toBigDecimal() })
+        val expect = "-0.5".toBigDecimal()
         assertEquals(expect, result)
     }
 
     @Test
     fun `더하기(매우 큰수 포함) 예외 테스트`() {
-        val result = calculator.sum(listOf(1, 8.toBigDecimal(), "1328391241341341343123123512324872837282312324241.1234522".toBigDecimal()))
+        val result = calculator.sum(
+            listOf(
+                "1",
+                "8",
+                "1328391241341341343123123512324872837282312324241.1234522"
+            ).map { it.toBigDecimal() })
         val expect = "1328391241341341343123123512324872837282312324250.1234522".toBigDecimal()
         assertEquals(expect, result)
     }
