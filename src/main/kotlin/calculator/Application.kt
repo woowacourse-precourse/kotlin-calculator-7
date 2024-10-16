c705176e1505b098fbb9b2e1fbdebc3fb056a933
DoableTuple <doabletuple@gmail.com>
2024-10-17 01:06:12 +0900

    feat: 구분자로 분리된 숫자들을 더하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,10 +1,10 @@
 package calculator
 
 fun main() {
-    while (true) {
-        val input = readln()
-        val inputNumbers = splitInput(input)
-    }
+    val input = readln()
+    val inputNumbers = splitInput(input)
+    val sum = addNumbers(inputNumbers)
+    println("결과: $sum")
 }
 
 
@@ -15,10 +15,23 @@ fun splitInput(input: String): List<String> {
         val inputParts = input.substring(2).split("\\n", limit = 2)
         val customDelimiter = inputParts[0]
         if (customDelimiter.isEmpty() || customDelimiter.contains("[0-9]".toRegex()) || inputParts.size < 2) {
-            throw IllegalArgumentException("Invalid input: $input")
+            throw IllegalArgumentException("Invalid input")
         }
         numbers = inputParts[1]
         delimiters += "|$customDelimiter"
     }
     return numbers.split(delimiters.toRegex())
+}
+
+
+fun addNumbers(numbers: List<String>): Int {
+    var sum = 0
+    numbers.filter { it.isNotEmpty() }.forEach {
+        val number = it.toIntOrNull()
+        if (number == null || number < 0 || number > Int.MAX_VALUE - sum) {
+            throw IllegalArgumentException("Invalid input")
+        }
+        sum += number
+    }
+    return sum
 }
