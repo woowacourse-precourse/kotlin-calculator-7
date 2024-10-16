8db0bd5d985668ad35c26f97db63a5142c9c467a
Jang Muyeong <jmy8876@gmail.com>
2024-10-16 23:54:30 +0900

    feat: 빈 문자열 또는 단일 숫자 입력 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,17 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine() ?: ""
+    val result = add(input)
+    println("결과 : $result")
+}
+
+fun add(input: String?): Int {
+    if (input.isNullOrEmpty()) {
+        return 0
+    }
+    return input.toIntOrNull() ?: 0
 }
