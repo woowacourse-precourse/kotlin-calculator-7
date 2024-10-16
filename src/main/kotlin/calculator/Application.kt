903da36de20fdcd47907bb2f30c365252f35905b
eeazzy <khs2498@gmail.com>
2024-10-16 21:22:47 +0900

    feat(calculator): 쉼표(,)와 콜론(:)을 구분자로 문자열 분리 및 덧셈 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,17 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine() // 입력 받기
+    val result = calculateSum(input)
+    println("결과 : $result")
+}
+
+fun calculateSum(input: String): Int { // 계산 함수
+    val delimiters = arrayOf(",", ":") // 구분자 정의
+    val numbers = input.split(*delimiters).map { it.toInt() } // 구분자로 숫자 분리
+
+    return numbers.sum() // 더한 숫자 반환
 }
