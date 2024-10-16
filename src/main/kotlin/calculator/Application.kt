49bca51a006014aae9fa48e3e85f5e1ca65135cc
eunaj99 <esfjge@gmail.com>
2024-10-17 03:14:36 +0900

    feat: enable I/O
    Test I/O functions and set the calculator format.
    - print guide message for user when application starts
    - get input string from user
    - replace white space to null for string processing test
    - print processed input
    
    This change makes the skeleton for calculator, making it ready for further features.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,21 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
+class Calculator {
+    private val INIT_MESSAGE = "덧셈할 문자열을 입력해 주세요."
+    var input: String? = ""
+
+    fun initCalculator() {
+        println(INIT_MESSAGE)
+        input = readLine()
+        input = input?.replace(" ", "")
+        println(input)
+    }
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    val calculator = Calculator()
+    calculator.initCalculator()
 }
