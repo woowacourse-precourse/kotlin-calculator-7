e4b4530328f23f61987a73a84b80a4d85d9c8d2c
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-16 19:58:15 +0900

    feat(Calculator): Calculator 클래스 생성
    - 내부 로직 주석 추가
    - companion obejct 로 validator 를 가짐

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,14 @@
+package calculator
+
+class Calculator {
+    fun calculateString(inputString: String) {
+        // 1. 커스텀 구분자가 있다면 커스텀 구분자를 추가하고, 문자열을 분리한다. divide
+        // 2. 구분자를 기준으로 각 숫자들을 분리한다. split
+        // 3. 각 숫자들의 총 합을 구한다.
+        // 4. 결과를 출력한다.
+    }
+
+    companion object {
+        private val validator = Validator()
+    }
+}
