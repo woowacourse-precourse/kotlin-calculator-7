eec5bd9b338ea1f3c5228a44b07dea8df88d0568
DoableTuple <doabletuple@gmail.com>
2024-10-17 00:41:53 +0900

    feat: 구분자를 기준으로 문자열을 분리하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,24 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    while (true) {
+        val input = readln()
+        val inputNumbers = splitInput(input)
+    }
+}
+
+
+fun splitInput(input: String): List<String> {
+    var delimiters = ",|;"
+    var numbers = input
+    if (input.startsWith("//")) {
+        val inputParts = input.substring(2).split("\\n", limit = 2)
+        val customDelimiter = inputParts[0]
+        if (customDelimiter.isEmpty() || customDelimiter.contains("[0-9]".toRegex()) || inputParts.size < 2) {
+            throw IllegalArgumentException("Invalid input: $input")
+        }
+        numbers = inputParts[1]
+        delimiters += "|$customDelimiter"
+    }
+    return numbers.split(delimiters.toRegex())
 }
