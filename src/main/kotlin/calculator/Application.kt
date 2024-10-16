2206d766bc9c3f461c6f8efae22a7f08eb4d14a6
UiHyeon <hyunkim6790@gmail.com>
2024-10-16 22:40:02 +0900

    feat: 커스텀 구분자 함수 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,9 +2,17 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
+/*
+* 예상 입력 형식
+* 2,2,3:3:5     15
+* //;\n1;2;3    6
+* 1,2:3;4//;\n  10
+* 1,2:3/4///\n  10
+* 10,10:10      30
+*/
 fun main() {
     val str = try {
-        Console.readLine()
+        Console.readLine().trim()
     } catch (e: IllegalArgumentException) {
         return
     }
@@ -21,7 +29,13 @@ fun main() {
 
 // 방법1: 문자열을 구분자로 나누는 함수
 fun splitString(str: String): List<Int> {
-    return str.split(",", ":").map { it.toInt() }
+    return if (str.contains("/")) {                 // 커스텀 문자열이 있을 경우
+        val index = str.indexOf("//")
+        val delimiter = str[index + 2].toString()
+        var result = str.removeRange(index, index + 5)
+        result.split(",", ":", delimiter).map { it.toInt() }
+    } else str.split(",", ":").map { it.toInt() }
+
 }
 
 // 방법2: 문자열에서 숫자만 필터링하는 함수
