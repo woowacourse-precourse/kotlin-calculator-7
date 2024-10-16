34bf03a6f97e91205707fd87bb9a2fe77da3241d
MEDA-DESKTOP\Meda <med4ndro@gmail.com>
2024-10-17 04:52:17 +0900

    feat: 양의 실수를 포함한 입력의 허용 및 예외 처리 개선
    - 입력 요구사항이 "양수"이므로 양의 실수도 처리하도록 개선
    - 인풋을 구분자로 단순 분할하여 문자열 리스트를 반환하는 코드를 getSplitStringList 메서드로 분할
    - InputStringManager.kt에 실수가 포함된 입력값인지 판별하는 detectAllIntInput 메서드 추가
    - 인풋을 정수 리스트로 분할하는 기존 메서드에 실수로 분할하는 메서드 추가
    - 문자열 리스트로 분할 후 각각의 원소를 검사하여 정수와 실수 둘 다 예외 탐지가 가능하도록 개선

--- src/main/kotlin/calculator/ErrorChecker.kt
+++ src/main/kotlin/calculator/ErrorChecker.kt
@@ -4,8 +4,7 @@ package calculator
 object ErrorChecker {
     // 인풋 오류를 검사하는 메소드
     fun invalidInputCheck(input: String) {
-        // 구분자와 구분자가 제거된 인풋 변수에 저장
-        val delimiter = DelimiterParser.getDelimiter(input)
+        val isAllInt = InputStringManager.detectAllIntInput(input)
         val commandRemovedInput = InputStringManager.getCommandRemovedInput(input)
         // 비어있는 값이 입력될 경우 예외처리를 건너뜀
         if (commandRemovedInput.isEmpty()) {
@@ -20,29 +19,34 @@ object ErrorChecker {
             throw IllegalArgumentException("오류: 구분자로 시작할 수 없습니다.")
         }
 
-        // 구분자가 2회 연속 등장하는지 탐지하기 위한 플래그 변수
-        var beforeNumber = true
-        // input 문자열을 char 단위로 순회
-        for (c in commandRemovedInput) {
-            if (validDigits.contains(c)) { // 숫자에 해당하면 플래그를 true으로 바꾸고 건너뛰기
-                beforeNumber = true
-                continue
-            }
+        // 구분자로 분할한 String타입 리스트를 불러옴
+        val strList = InputStringManager.getSplitStringList(input)
 
-            // 구분자에 해당하며 이전이 숫자였다면 플래그를 false로 바꾸고 건너뛰기
-            if (delimiter.contains(c) && beforeNumber) {
-                beforeNumber = false
-                continue
+        // 구분자로 분할된 각각의 원소를 검사
+        strList.map {
+            // 점이 2번 이상 사용되었는지 검사하기 위한 플래그
+            var pointFlag = true
+            // 원소의 값을 문자단위로 다시 쪼개서 검사
+            for ((index, char) in it.withIndex()) {
+                // 숫자이면 다음 문자로 이동
+                if (validDigits.contains(char)) {
+                    continue
+                }
+                // .이 2번 연속 나오지 않고 0번 인덱스가 아니면 다음문자로 이동
+                if (pointFlag && index != 0 && char == '.') {
+                    pointFlag = false
+                    continue
+                }
+                // .이 두번 등장하거나 .이 맨 앞에 등장하거나 .이나 숫자가 아닌경우 예외처리됨
+                throw IllegalArgumentException("오류: 잘못된 구분자 입력입니다.")
             }
+        }
 
-            // 숫자 혹은 구분자가 아니거나 구분자가 연속 등장하는 경우 예외 발생
-            throw IllegalArgumentException("오류: 잘못된 입력입니다. 에러 문자:'${c}'")
+        // 0 또는 0.0 입력을 탐지하여 예외 발생
+        if (isAllInt && InputStringManager.parseInputStringToIntList(input).contains(0)) {
+            throw IllegalArgumentException("오류: 양수만 입력해 주세요")
         }
-        if (!beforeNumber) { // 구분자로 끝나는 문자열을 탐지하여 예외 발생
-            throw IllegalArgumentException("오류: 구분자 뒤에 숫자를 입력해 주세요")
-        }
-        // 0 입력을 탐지하여 예외 발생
-        if (InputStringManager.parseInputStringToNumbers(input).contains(0)) {
+        if (InputStringManager.parseInputStringToDoubleList(input).contains(0.0)) {
             throw IllegalArgumentException("오류: 양수만 입력해 주세요")
         }
     }
--- src/main/kotlin/calculator/InputStringManager.kt
+++ src/main/kotlin/calculator/InputStringManager.kt
@@ -2,8 +2,8 @@ package calculator
 
 // input 문자열을 조작하거나 연산을 수행하는 객체
 object InputStringManager {
-    // input 문자열을 구분자로 쪼개서 List<Int>로 반환하는 메서드
-    fun parseInputStringToNumbers(input: String): List<Int> {
+
+    fun getSplitStringList(input: String): List<String> {
         // 커맨드를 제외한 값이 비어있다면 비어있는 리스트를 반환
         if (getCommandRemovedInput(input).isEmpty()) {
             return emptyList()
@@ -18,17 +18,49 @@ object InputStringManager {
         // 결합한 문자열을 대괄호로 감싸서 각각의 구분자를 선택하는 정규식으로 변환
         val regex = "[${joinedDelimiters}]".toRegex()
         // 정규식을 사용해 커맨드를 제거한 인풋 문자열을 슬라이싱
-        val strList = getCommandRemovedInput(input).split(regex)
+        return getCommandRemovedInput(input).split(regex)
+    }
 
+    // input 문자열을 구분자로 쪼개서 List<Int>로 반환하는 메서드
+    fun parseInputStringToIntList(input: String): List<Int> {
+        val strList = getSplitStringList(input)
         // 슬라이싱한 String타입의 리스트를 Int형 List로 변환하여 반환
         return strList.map { it.toInt() }
     }
 
+    // input 문자열을 구분자로 쪼개서 List<Float>로 반환하는 메서드
+    fun parseInputStringToDoubleList(input: String): List<Double> {
+        val strList = getSplitStringList(input)
+
+        // 슬라이싱한 String타입의 리스트를 Double형 List로 변환하여 반환
+        return strList.map { it.toDouble() }
+    }
+
+    // input값이 정수만 사용되었는지 감지하여 Boolean을 반환하는 함수
+    fun detectAllIntInput(input: String): Boolean {
+        // 커맨드를 제거한 인풋과 구분자를 불러오기
+        val noCommandInput = getCommandRemovedInput(input)
+        val delimiters: Set<Char> = DelimiterParser.getDelimiter(input)
+
+        // 구분자에 .이 있을경우 무조건 정수로 나뉘게 되므로 true반환
+        if (delimiters.contains('.')) {
+            return true
+        }
+        // 구분자에 .이 없으며 입력값에 .이 포함되어 있으면 실수이므로 false반환
+        if (noCommandInput.contains('.')) {
+            return false
+        }
+        // 구분자와 인풋에 .이 없으면 정수이므로 true반환
+        return true
+    }
+
     // 인풋 문자열을 합산하여 반환하는 메소드
-    fun sumString(input: String): Int {
+    fun sumString(input: String): String {
         // input 문자열을 구분자를 통해 슬라이싱된 int타입 List로 불러옴
-        val intList = parseInputStringToNumbers(input)
-        return intList.sum()
+        if (detectAllIntInput(input)) {
+            return parseInputStringToIntList(input).sum().toString()
+        }
+        return parseInputStringToDoubleList(input).sum().toString()
     }
 
     // 인풋 문자열에서 커맨드를 제거한 문자열을 반환하는 메소드
