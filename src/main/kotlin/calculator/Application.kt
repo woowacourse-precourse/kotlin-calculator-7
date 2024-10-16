6cc52e9d2aaa10a354c6149864d4721da710dd2c
eeazzy <khs2498@gmail.com>
2024-10-16 22:05:07 +0900

    feat(calculator): 예외처리 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -19,18 +19,31 @@ fun calculateSum(input: String): Int { // 계산 함수
     // 커스텀 구분자 처리
     val (numbersString, customDelimiter) = if (input.startsWith("//")) {
         val customDelimiter = input.substring(2, input.indexOf("\\n")) // "//"와 "\n" 사이의 구분자 추출
+        if ( input.indexOf("\\n") == -1)  {
+            throw IllegalArgumentException() // 구분자 정의 없이 잘못된 형식일 경우 예외 처리
+        }
         val numbersString = input.substring(input.indexOf("\\n") + 2) // 숫자 문자열 부분 추출
         Pair(numbersString, customDelimiter)
     } else {
-        Pair(input, null)
+        Pair(input, null) // 커스텀 구분자가 없으면 기본 구분자 사용
     }
 
-    // 커스텀 구분자가 있을 경우 포함하여 split
-    val numbers = if (customDelimiter != null) {
-        numbersString.split(customDelimiter)
-    } else {
-        numbersString.split(*delimiters) // 기본 구분자일 경우
-    }.map { it.toInt() } // 숫자로 변환
+    // 숫자 split
+    val numbers = try {
+        if (customDelimiter != null) {
+            numbersString.split(customDelimiter) // 커스텀 구분자일 경우
+        } else {
+            numbersString.split(*delimiters) // 기본 구분자일 경우
+        }.map {
+            val number = it.toIntOrNull() ?: throw IllegalArgumentException() // 숫자가 아닌 값이 있을 경우 예외 처리
+            if (number < 0) {
+                throw IllegalArgumentException() // 음수가 있을 경우 예외 처리
+            }
+            number
+        }
+    } catch (e: Exception) {
+        throw IllegalArgumentException()
+    }
 
     return numbers.sum() // 더한 숫자 반환
 }
