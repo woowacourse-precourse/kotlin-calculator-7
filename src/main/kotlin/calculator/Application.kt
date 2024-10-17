d8a7503e8226f828a5266d5da13b7d02d038e674
LeeHaiLim <hailim1997@gmail.com>
2024-10-17 23:13:44 +0900

    feat: 숫자 더하기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,8 +5,11 @@ private const val INPUT_PREFIX = "덧셈할 문자열을 입력해 주세요."
 fun main() {
     val reader = MyReader()
     val writer = MyWriter()
+    val parser = MyParser()
 
     writer.println(INPUT_PREFIX)
+    val input = reader.readLine()
+    val result = parser.parse(input).sum()
 
     reader.close()
     writer.close()
