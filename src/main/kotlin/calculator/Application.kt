2013f5ec9fd6110280b2d28a1e2fc908d69a1629
지원 <cheerfulboys@naver.com>
2024-10-17 21:27:47 +0900

    feat(isValid): 문자열의 유효값을 확인하는 함수 추가
    입력받은 문자열이 숫자, 콤마(,), 콜론(:)으로만 구성되어 있는지 확인 후 Boolean 값을 반환한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -23,10 +23,17 @@ fun sum(numbers: List<Int>): Int {
     return numbers.sum()
 }
 
+fun isValid(str: String): Boolean {
+    str.forEach { if (it !in '0'..'9' && it != ',' && it != ':') return false }
+    return true
+}
+
 fun main() {
     println("덧셈할 문자열을 입력해주세요.")
 
     val inputString = input()
+
+    if (!isValid(inputString)) { println("유효하지 않은 식") }
     val numbers = splitNumber(inputString)
     val result = sum(numbers)
 
