a8f8039ed085d99bc49b7b6d124c3f1f10177851
Jang Muyeong <jmy8876@gmail.com>
2024-10-17 14:39:17 +0900

    feat: 쉼표와 콜론 구분자를 사용한 숫자의 합 계산

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,8 +10,23 @@ fun main() {
 }
 
 fun add(input: String?): Int {
-    if (input.isNullOrEmpty()) {
+    if (input.isNullOrEmpty()) { // 빈 문자열 또는 null 입력할 경우 0 반환
         return 0
     }
-    return input.toIntOrNull() ?: 0
+    val numbers = parseNumbers(input)
+    return numbers.sum()
+}
+
+fun parseNumbers(input: String): List<Int> {
+    val delimiters = listOf(",", ":") // 쉼표와 콜론을 구분자로 설정
+    val tokens = input.split(*delimiters.toTypedArray()) // 구분자를 기준으로 숫자 분리
+
+    return tokens.map {
+        val number = it
+        if (number.isEmpty()) {
+            0 // 빈 문자열이 있을 경우 0으로 처리
+        } else {
+            number.toInt() // 문자열을 숫자로 변환
+        }
+    }
 }
