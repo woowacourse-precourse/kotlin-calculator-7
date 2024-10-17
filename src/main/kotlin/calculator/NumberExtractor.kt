6c61252c670477a848320f4e82c2c855f369cfa8
Potato-Y <projectpotatoy@gmail.com>
2024-10-17 14:59:08 +0900

    feat(NumberExtractor): 추출할 수 있는 문자열인지 검증하는 메서드 구현
    - 추출 문자열에 사용할 수 있는 숫자 문자열을 상수로 선언
    - 사용할 수 있는 숫자 문자열을 리스트 형식으로 반환하는 메서드 추가
    - 문자열이 사용할 수 있는 구분자와 숫자로 이루어있는지 검증하는 메서드 추가

--- src/main/kotlin/calculator/NumberExtractor.kt
+++ src/main/kotlin/calculator/NumberExtractor.kt
@@ -11,8 +11,33 @@ class NumberExtractor {
         separators.add(separator)
     }
 
+    private fun inputValidator(input: String) {
+        val validNumbers = getNumberList()
+
+        for (char in input) {
+            val charAsString = char.toString()
+            if (!validNumbers.contains(charAsString) && !separators.contains(charAsString)) {
+                throw IllegalArgumentException("잘못된 값이 입력되었습니다.")
+            }
+        }
+    }
+
     companion object {
         const val COMMA = ","
         const val COLON = ":"
+
+        private const val ONE = "1"
+        private const val TWO = "2"
+        private const val THREE = "3"
+        private const val FOUR = "4"
+        private const val FIVE = "5"
+        private const val SIX = "6"
+        private const val SEVEN = "7"
+        private const val EIGHT = "8"
+        private const val NINE = "9"
+
+        fun getNumberList(): List<String> {
+            return listOf(ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE)
+        }
     }
 }
