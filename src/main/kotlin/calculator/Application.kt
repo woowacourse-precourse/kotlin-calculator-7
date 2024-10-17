3813b7e1bf4deac5119ba122e88e84738de3ac3b
UiHyeon <hyunkim6790@gmail.com>
2024-10-17 19:25:09 +0900

    feat: 결과 출력 함수 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,6 +10,7 @@ import camp.nextstep.edu.missionutils.Console
 * 1,2:3/4///\n  10
 * 10,10:10      30
 */
+
 fun main() {
     val str = try {
         Console.readLine().trim()
@@ -17,19 +18,16 @@ fun main() {
         return
     }
 
-    val num = splitString(str)
-
-    // 출력 형식
-    println("덧셈할 문자열을 입력해주세요.")
-    println(str)
-    println(sumNumbers(num))
+    val numbers = splitString(str)
+    val sum = sumNumbers(numbers)
+    printResult(str, sum)
 
     Console.close()
 }
 
 // 방법1: 문자열을 구분자로 나누는 함수
 fun splitString(str: String): List<Int> {
-    return if (str.contains("/")) {                 // 커스텀 문자열이 있을 경우
+    return if (str.contains("/")) {
         val index = str.indexOf("//")
         val delimiter = str[index + 2].toString()
         var result = str.removeRange(index, index + 5)
@@ -37,13 +35,19 @@ fun splitString(str: String): List<Int> {
     } else {
         str.split(",", ":").map { it.toInt() }
     }
-
 }
 
 // 방법2: 문자열에서 숫자만 필터링하는 함수
 /*fun filteringString(str: String): List<Int> {
-    return str.filter { it.isDigit() }.map { it.toString().toInt() }
+
 }*/
 
 // 숫자의 합을 반환하는 함수
 fun sumNumbers(numbers: List<Int>): Int = numbers.sum()
+
+// 출력 형식대로 출력하는 함수
+fun printResult(str: String, sum: Int) {
+    println("덧셈할 문자열을 입력해 주세요.")
+    println(str)
+    println("결과 : $sum")
+}
