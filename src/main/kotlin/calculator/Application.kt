ae43219635bead5e23ef8f03a81860e6a06911a1
LeeHaiLim <hailim1997@gmail.com>
2024-10-17 23:04:18 +0900

    feat: 입력값 prefix 문구 출력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,14 @@
 package calculator
 
+private const val INPUT_PREFIX = "덧셈할 문자열을 입력해 주세요."
+
 fun main() {
-    // TODO: 프로그램 구현
+    val reader = MyReader()
+    val writer = MyWriter()
+
+    writer.println(INPUT_PREFIX)
+
+    reader.close()
+    writer.close()
 }
+
