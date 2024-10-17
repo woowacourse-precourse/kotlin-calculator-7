2255933940aec410a4c9f641bc11930125f3e7d1
Jaemin <kamja990510@gmail.com>
2024-10-17 16:47:12 +0900

    feat(Application): handle exception in input
    If a NumberFormatException occurs when converting split values, whether custom or default, to Int type, an IllegalArgumentException is thrown because a character other than a delimiter was received.Even if everything is converted to type Int, the separated list is searched and an IllegalArgumentException is thrown if there is a negative number.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,5 +5,35 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.  ")
     val input = Console.readLine()
-    println(input)
+    var isCustom = false
+    var customDelimeter = ";"
+
+    try {
+        if (isCustom) {
+            val numberList = input
+                .substring(5)
+                .split(customDelimeter)
+                .map {
+                    if (it == "") 0 else it.toInt()
+                }
+            numberList.forEach {
+                if (it < 0) {
+                    throw IllegalArgumentException()
+                }
+            }
+        } else {
+            val numberList = input
+                .split(regex = Regex("[,:]"))
+                .map {
+                    if (it == "") 0 else it.toInt()
+                }
+            numberList.forEach {
+                if (it < 0) {
+                    throw IllegalArgumentException()
+                }
+            }
+        }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException()
+    }
 }
