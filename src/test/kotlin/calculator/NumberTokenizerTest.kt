b76e41392b99d42a140ac4e04a240b465dfd48c9
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-17 19:45:17 +0900

    test(NumberTokenizerTest): 커스텀 구분자를 추출 테스트 추가
    사용자가 정의한 커스텀 구분자를 통해 숫자를 구분하기 위해 추출
    - 사용자가 구분자를 사용하다가 후에 커스텀 해도 적용 되도록 전체 문자열에서 추출

--- src/test/kotlin/calculator/NumberTokenizerTest.kt
+++ src/test/kotlin/calculator/NumberTokenizerTest.kt
@@ -6,31 +6,38 @@ import org.junit.jupiter.api.Test
 
 class NumberTokenizerTest {
 
-    private lateinit var stringSplitter: NumberTokenizer
+    private lateinit var numberTokenizer: NumberTokenizer
 
     @BeforeEach
     fun setUp() {
-        stringSplitter = NumberTokenizer()
+        numberTokenizer = NumberTokenizer()
     }
 
     @Test
     fun `문자열을 쉼표를 통해 나누기`() {
-        val result = stringSplitter.tokenize("1,3,-56.5,7")
+        val result = numberTokenizer.tokenize("1,3,-56.5,7")
         val expect = listOf("1", "3", "-56.5", "7").map { it.toBigDecimal() }
         assertEquals(expect, result)
     }
 
     @Test
     fun `문자열을 콜론을 통해 나누기`() {
-        val result = stringSplitter.tokenize("1:3:56.5:7")
+        val result = numberTokenizer.tokenize("1:3:56.5:7")
         val expect = listOf("1", "3", "56.5", "7").map { it.toBigDecimal() }
         assertEquals(expect, result)
     }
 
     @Test
     fun `문자열을 콜론과 쉼표를 통해 나누기`() {
-        val result = stringSplitter.tokenize("1,3:-531236.512873921873298472394827928719242591283792187413984731984713:7,-82:")
+        val result = numberTokenizer.tokenize("1,3:-531236.512873921873298472394827928719242591283792187413984731984713:7,-82:")
         val expect = listOf("1", "3", "-531236.512873921873298472394827928719242591283792187413984731984713", "7", "-82").map { it.toBigDecimal() }
+        assertEquals(expect, result)
+    }
+
+    @Test
+    fun `문자열에서 커스텀 구분자 추출 테스트`(){
+        val result = numberTokenizer.getCustomDelimiters("//;\\n1;2and3//and\\n//&&\\n")
+        val expect = listOf(";","and","&&")
         assertEquals(expect, result)
     }
 }
