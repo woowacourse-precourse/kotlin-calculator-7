6ff25426bbb54702b544d602c47b52aea79a3372
jklom0326 <jklom0326@gmail.com>
2024-10-17 21:25:01 +0900

    refactor: 문자열 입력 함수로 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,6 @@
 package calculator
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
     val user = User()
 
     user.run()
--- src/main/kotlin/calculator/User.kt
+++ src/main/kotlin/calculator/User.kt
@@ -3,8 +3,16 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 class User {
+    private val calculator = Calculator()
 
     fun run() {
-        val input = Console.readLine()
+        // 입력 받기
+        val input = readLine()
+
+    }
+
+    private fun readLine(): String {
+        println("덧셈할 문자열을 입력해 주세요.")
+        return Console.readLine()
     }
 }
