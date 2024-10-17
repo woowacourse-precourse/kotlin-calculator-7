2c248fbbb588b6a604ceda23474649d4fb222baa
LeeHaiLim <hailim1997@gmail.com>
2024-10-17 23:09:02 +0900

    feat: 구분자 등록 부 분리

--- src/main/kotlin/calculator/MyParser.kt
+++ src/main/kotlin/calculator/MyParser.kt
@@ -1,6 +1,21 @@
 package calculator
 
 class MyParser {
+    fun parse(input: String) {
+        val tokens = tokenize(input)
+        val delimiters = getDelimiters(tokens[2])
+    }
+
+    private fun tokenize(input: String): List<String> =
+        delimiterInputRegex
+            .find(input)
+            ?.groupValues
+            ?: throw IllegalArgumentException()
+
+    private fun getDelimiters(extraDelimiter: String): List<String> =
+        if (extraDelimiter.isNotEmpty()) defaultDelimiters + extraDelimiter
+        else defaultDelimiters
+
     companion object {
         private val defaultDelimiters = listOf(",", ":")
         private val delimiterInputRegex = "(//(.+)\\\\n)?(.*)".toRegex()
