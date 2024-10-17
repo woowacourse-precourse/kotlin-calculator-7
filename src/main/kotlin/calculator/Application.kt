05247d50c1959f0016e0b87e479ace7259016bf3
taegn <taegyunn123@gmail.com>
2024-10-18 01:05:05 +0900

    feat Application Calculator class 정의

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,41 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
+class Calculator(input: String) {
+    private val separator: Array<String>
+    private val numbers: IntArray
+
+    init {
+        separator = getSeparator(input) ?: arrayOf(",", ":")
+        numbers = getNumbers(input)
+    }
+
+    private fun getSeparator(input: String): Array<String>? {
+        val startIdx = input.indexOf("""//""").let { if (it == -1) return null else it + 2 }
+        val endIdx = input.indexOf("""\n""").let { if (it <= startIdx) throw IllegalArgumentException() else it }
+        return arrayOf(input.substring(startIdx, endIdx))
+    }
+
+    private fun getNumbers(input: String): IntArray {
+        try {
+            val startIdx = input.indexOf("""//""") + 2
+            val endIdx = input.indexOf("""\n""")
+            val numbers = (if (startIdx in 2 until endIdx) input.substring(endIdx + 2) else input)
+                .split(*separator).map(String::toInt).toIntArray()
+            if (numbers.any { it <= 0 }) throw IllegalArgumentException()
+            return numbers
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun result() = "결과 : ${numbers.sum()}"
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+    val calculator = Calculator(input)
+    println(calculator.result())
 }
