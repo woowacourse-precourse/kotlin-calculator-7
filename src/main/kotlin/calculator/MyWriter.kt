541e99dd58f8650e45a394db2af54cc2bc70a94f
LeeHaiLim <hailim1997@gmail.com>
2024-10-17 22:51:51 +0900

    feat: 출력 클래스 구현

---
+++ src/main/kotlin/calculator/MyWriter.kt
@@ -0,0 +1,36 @@
+package calculator
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+class MyWriter {
+    private val writer = BufferedWriter(OutputStreamWriter(System.out))
+    fun print(a: Any?) =
+        runCatching {
+            writer.write(a.toString())
+            writer.flush()
+        }.onFailure {
+            throw IllegalArgumentException()
+        }
+
+    fun printf(format: String, vararg args: Any?) {
+        print(String.format(format, *args))
+    }
+
+
+    fun println(a: Any?) =
+        runCatching {
+            print(a)
+            writer.newLine()
+            writer.flush()
+        }.onFailure {
+            throw IllegalArgumentException()
+        }
+
+    fun close() =
+        runCatching {
+            writer.close()
+        }.onFailure {
+            throw IllegalArgumentException()
+        }
+}
