2b141b79b1902e3c3afffad801c5d32bdd504bda
yrsel <xxknu88@gmail.com>
2024-10-17 17:11:39 +0900

    fix(Constant): 상수 변수 추가

--- src/main/kotlin/calculator/util/Constant.kt
+++ src/main/kotlin/calculator/util/Constant.kt
@@ -2,6 +2,7 @@ package calculator.util
 
 object Constant {
     val CUSTOM_DELIMITER_DETERMINE_REGEX = "//(.*)\\\\n(.*)".toRegex()
-    const val WELCOME_MESSAGE = "덧셈할 문자열을 입력해 주세요."
-    const val RESULT_FORMAT = "결과 : %d"
+    const val WELCOME_MESSAGE = "덧셈할 문자열을 입력해 주세요. 프로그램 종료를 원할 경우 -1 입력"
+    const val RESULT_FORMAT = "결과 : %s \n"
+    const val ERROR_MESSAGE = "잘못된 입력입니다. [현재 입력 : %s]\n"
 }
