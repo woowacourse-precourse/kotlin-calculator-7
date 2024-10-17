143a7b978e6624fff315ba7fdf4d1b94ca37520a
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-17 14:55:56 +0900

    Style(DelimiterSplitter.kt): 코드 포맷팅 수정

--- src/main/kotlin/calculator/domain/DelimiterSplitter.kt
+++ src/main/kotlin/calculator/domain/DelimiterSplitter.kt
@@ -1,7 +1,7 @@
 package calculator.domain
 
 class DelimiterSplitter(
-    private val value: String,
+    private val value: String
 ) {
     init {
         require(customDelimiterFormatCheck())
@@ -13,13 +13,15 @@ class DelimiterSplitter(
         splitResult.all { it.matches(Regex("^(?:0|[1-9]\\d*)(\\.\\d+)?\$")) }
 
     // 커스텀 구분자 사용 선언 포맷 확인
-    private fun customDelimiterFormatCheck(): Boolean = value.startsWith(CUSTOM_DELIMITER_FRONT) == value.contains(CUSTOM_DELIMITER_BACK)
+    private fun customDelimiterFormatCheck(): Boolean =
+        value.startsWith(CUSTOM_DELIMITER_FRONT) == value.contains(CUSTOM_DELIMITER_BACK)
 
     // 커스텀 구분자 사용 여부
     private fun customDelimiterIsUsed(): Boolean = value.startsWith(CUSTOM_DELIMITER_FRONT)
 
     // 커스텀 구분자 찾기
-    private fun findCustomDelimiter() = value.substring(CUSTOM_DELIMITER_FRONT.length, value.lastIndexOf(CUSTOM_DELIMITER_BACK))
+    private fun findCustomDelimiter() =
+        value.substring(CUSTOM_DELIMITER_FRONT.length, value.lastIndexOf(CUSTOM_DELIMITER_BACK))
 
     // 커스텀 구분자로 나누기
     private fun customSplit(): List<String> {
