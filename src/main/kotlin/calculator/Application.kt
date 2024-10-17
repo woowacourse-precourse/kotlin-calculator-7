6640ff3648611eeebe1d41eb6b36ac64f38ea7f7
eunaj99 <esfjge@gmail.com>
2024-10-18 01:43:46 +0900

    feat: detect custom delimiters
    커스텀 구분자를 식별 가능하도록 수정
    - 커스텀 구분자가 1개 이상일 때에도 대응
    - 입력된 문자열의 중간에 커스텀 구분자를 정의하는 경우에 대응
    - 문자열의 초입에서 커스텀 구분자를 정의하지 않는 경우에 대응
    
    계산기의 초기화와 종료가 별개의 함수로 분리됨
    개발 중 initCalculator() 함수를 더 쉽게 변경하기 위한 임시적 조치로서 후에 다시 합칠 예정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,15 +1,39 @@
 package calculator
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.StringBuilder
 
 class Calculator {
     private val INIT_MESSAGE = "덧셈할 문자열을 입력해 주세요."
-    var input: String? = ""
+    private var input: String? = ""
+    private lateinit var inputProcessing: StringBuilder
+    private var answer: Int = 0
+    private var delimiter: String = ",:"
+    private val CUSTOMDEVIDER_RULES = """//(.+)\\n""".toRegex()
 
-    fun initCalculator() {
+    fun initCalculator() { // 계산기 초기화
         println(INIT_MESSAGE)
         input = readLine()
-        input = input?.replace(" ", "")
+        inputProcessing = StringBuilder(input)
+        identifyDelimiters()
+    }
+
+
+
+    fun identifyDelimiters() { // 커스텀 구분자 식별
+        while (CUSTOMDEVIDER_RULES.containsMatchIn(inputProcessing)) {
+            val startIndex = inputProcessing.indexOf("//")
+            val endIndex = inputProcessing.indexOf("\\n")
+            delimiter += inputProcessing.substring(startIndex+2, endIndex)
+            inputProcessing.delete(startIndex, endIndex+2)
+        }
+    }
+
+    // TODO: 숫자 식별 및 합산
+
+    fun finCalculator() { // 계산기 종료
+        input = inputProcessing.toString()
+        println(delimiter)
         println(input)
     }
 }
@@ -18,4 +42,6 @@ fun main() {
     // TODO: 프로그램 구현
     val calculator = Calculator()
     calculator.initCalculator()
+
+    calculator.finCalculator()
 }
