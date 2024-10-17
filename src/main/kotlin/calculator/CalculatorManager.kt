d6085816766cfab654e7c424acc785e92acf9c12
TaejungKim <tae0803@naver.com>
2024-10-17 13:48:29 +0900

    feat: 입력값 및 기타 검증을 위한 Validator Class 추가
    - 유저의 입력 값 검증을 하기 위한 Validator class 추가

--- src/main/kotlin/calculator/CalculatorManager.kt
+++ src/main/kotlin/calculator/CalculatorManager.kt
@@ -5,9 +5,12 @@ import camp.nextstep.edu.missionutils.Console
 
 class CalculatorManager {
     fun start(){
-        println(INPUT_PROMPT_TEXT)
+        val userInput = getUserInput()
+        Validator.validateUserInput(userInput)
     }
+
     private fun getUserInput(): String?{
+        println(INPUT_PROMPT_TEXT)
         return Console.readLine()
     }
 }
---
+++ src/main/kotlin/calculator/Validator.kt
@@ -0,0 +1,8 @@
+package calculator
+
+object Validator {
+
+    fun validateUserInput(userInput: String?){
+        // 입력받은 userInput을 검증하는 함수
+    }
+}
