dfd05a38202c0664b302c5504c86e37d2fdf571f
changzin <changzin0126@gmail.com>
2024-10-17 18:37:08 +0900

    feat(main): 커스텀 구분자를 제외한 입력에서의 계산기 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,29 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    // 입력 받기
+    println("덧셈할 문자열을 입력해 주세요.")
+    var input:String = Console.readLine().toString();
+
+    // 구분자 리스트 생성
+    var sepList = mutableListOf<Char>(':',',');
+    
+    // 덧셈 계산기
+    var num = 0
+    var answer = 0
+
+    for(c in input){
+        if(c==',' || c==':'){
+            answer += num
+            num = 0
+            continue
+        }
+        num = (num * 10) + (c.code - '0'.code)
+    }
+    answer += num
+
+    print("결과 : $answer");
 }
+
