c710fdc8fa4328501f60eae3a3a672408f975bf7
yrsel <xxknu88@gmail.com>
2024-10-17 14:14:04 +0900

    fix(Constant): 정규표현식에 해당하는 상수 코드 수정

--- src/main/kotlin/calculator/util/Constant.kt
+++ src/main/kotlin/calculator/util/Constant.kt
@@ -1,7 +1,7 @@
 package calculator.util
 
 object Constant {
-    val CUSTOM_DELIMITER_DETERMINE_REGEX = "//(.*)\\n(.*)".toRegex()
+    val CUSTOM_DELIMITER_DETERMINE_REGEX = "//(.*)\\\\n(.*)".toRegex()
     const val WELCOME_MESSAGE = "덧셈할 문자열을 입력해 주세요."
     const val RESULT_FORMAT = "결과 : %d"
 }
