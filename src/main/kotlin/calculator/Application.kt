0f5d83ef025c60b45cfe7e3e908a32aa19980a30
LeeHaiLim <hailim1997@gmail.com>
2024-10-17 23:14:27 +0900

    feat: 결과 출력하기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,6 +1,7 @@
 package calculator
 
 private const val INPUT_PREFIX = "덧셈할 문자열을 입력해 주세요."
+private const val RESULT_FORMAT = "결과 : %s\n"
 
 fun main() {
     val reader = MyReader()
@@ -10,6 +11,7 @@ fun main() {
     writer.println(INPUT_PREFIX)
     val input = reader.readLine()
     val result = parser.parse(input).sum()
+    writer.printf(RESULT_FORMAT, result)
 
     reader.close()
     writer.close()
