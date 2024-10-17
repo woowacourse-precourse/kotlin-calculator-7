b8704ab6e79a8174d69346bd7d5be69f4c57f268
지원 <cheerfulboys@naver.com>
2024-10-17 20:47:52 +0900

    feat(splitNumber): 숫자를 List에 담아 반환하는 함수 추가
    문자열을 받아 숫자만 List에 담아 반환한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,8 +6,22 @@ fun input(): String {
     return Console.readLine()
 }
 
+fun splitNumber(str: String): List<Int> {
+    var numberList: List<Int> = listOf()
+
+    str.forEach {
+        println(it)
+        if (it == ',' || it == ':') {
+            return@forEach
+        }
+        numberList += it.toString().toInt()
+    }
+
+    return numberList
+}
+
 fun main() {
     println("덧셈할 문자열을 입력해주세요.")
     val inputString = input()
-
+    val numbers = splitNumber(inputString)
 }
