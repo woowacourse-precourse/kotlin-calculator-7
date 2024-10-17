706cb53ea353552461276a08b7688b64b25acd43
wondreaming <5959.wondreaming@gmail.com>
2024-10-18 01:42:26 +0900

    fix(separation): null check 추가와 ""값 추가
    null == ""이라고 생각하여, null로 로직을 생성함
    이에 bug가 발생하여 코드 수정

--- src/main/kotlin/calculator/separation/Separation.kt
+++ src/main/kotlin/calculator/separation/Separation.kt
@@ -4,8 +4,9 @@ import calculator.constant.DelimiterConst
 import calculator.delimiter.CustomDelimiter
 
 class Separation {
-    fun splitInput(input: String?, customDelimiter: CustomDelimiter): List<String> {
-        if (input == null) return listOf<String>()
+    fun splitInput(input: String?, customDelimiter: CustomDelimiter): List<String>? {
+        if (input == null) return null
+        if (input == "") return listOf<String>()
         return if (customDelimiter.customDelimiter != null) {
             input.split(DelimiterConst.CLONE, DelimiterConst.COMMA, customDelimiter.customDelimiter!!)
         } else {
