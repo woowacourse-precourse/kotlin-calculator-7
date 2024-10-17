3a6b932d3775e93e4f804f046fcc13943ee733e5
jklom0326 <jklom0326@gmail.com>
2024-10-18 03:35:57 +0900

    feat: 구분자 유효성 검사 기능 추가

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -5,19 +5,32 @@ import kotlin.IllegalArgumentException
 class Calculator {
 
     fun calculate(input: String) {
+        val splitInput = input.split(",", ":")
+        validateInput(splitInput)
     }
 
     fun calculateCustom(input: String) {
-        validateInputCustom(input)
+        val (customSeparator, customInput) = input
+            .split("//", "\\n")
+            .filter { it.isNotEmpty() }
+        validateInputCustom(customSeparator, customInput)
     }
 
-    private fun validateInputCustom(input: String) {
-        val custom = input.split("//", "\\n").first { it.isNotEmpty() }
-        val splitInput = input.split("//", "\\n").last { it.isNotEmpty() }
+    private fun validateInput(splitInput: List<String>) {
+        splitInput.forEach {
+            // 값이 구분자로 나누어지지 않았을 경우
+            if (it.toBigIntegerOrNull() == null) throw IllegalArgumentException("입력된 값이 잘못됐습니다.")
+            // 입력된 값이 음수일 경우
+            if (it[0] == '-') throw IllegalArgumentException("입력된 값이 잘못됐습니다.")
+        }
+    }
 
+    private fun validateInputCustom(customSeparator: String, customInput: String) {
         // 커스텀 문자가 여러개일 경우
-        if (custom.length != 1) throw IllegalArgumentException("입력이 잘못되었습니다")
-        // 커스텀 문자가 문자가 아닐경우
-        if (!custom[0].isLetter()) throw IllegalArgumentException("입력이 잘못되었습니다")
+        if (customSeparator.length != 1) throw IllegalArgumentException("커스텀 문자 입력이 잘못되었습니다")
+        // 커스텀 문자가 문자가 아닐 경우
+        if (customSeparator.first().isDigit()) throw IllegalArgumentException("커스텀 문자 입력이 잘못되었습니다")
+
+        validateInput(customInput.split(customSeparator))
     }
 }
