b54e9c9681e07b6eabfb947db230c2f3d959c4d5
yrsel <xxknu88@gmail.com>
2024-10-17 14:59:00 +0900

    feat(Validator): 공백 입력에 대한 검증

--- src/main/kotlin/calculator/util/Validator.kt
+++ src/main/kotlin/calculator/util/Validator.kt
@@ -3,10 +3,14 @@ package calculator.util
 object Validator {
 
     fun isValidNumber(input: String, delimiter: Array<String>): Boolean {
-        val numbers = input.split(*delimiter).toList()
+        if (isZero(input)) return true
 
+        val numbers = input.split(*delimiter).toList()
         numbers.forEach { it.toIntOrNull() ?: return false }
+
         return true
     }
+
+    private fun isZero(input: String) = input.isEmpty()
 
 }
