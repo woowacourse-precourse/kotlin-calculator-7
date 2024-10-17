33fe436de5bdd2c9f690a83f1756c70340dd19bd
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-18 00:30:16 +0900

    test(ApplicationTest): 입력 값에 대한 테스트 추가

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -22,6 +22,49 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `숫자만 입력`() {
+        assertSimpleTest {
+            run("1234")
+            assertThat(output()).contains("결과 : 1234")
+        }
+    }
+
+    @Test
+    fun `입력 값 예외 테스트_숫자 없이 구분자만 입력`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException(",,:") }
+        }
+    }
+
+    @Test
+    fun `입력 값 예외 테스트_공밴만 입력`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("    ") }
+        }
+    }
+
+    @Test
+    fun `입력 값 예외 테스트_기본 구분자로 시작`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException(",2:3") }
+        }
+    }
+
+    @Test
+    fun `입력 값 예외 테스트_기본 구분자로 끝남`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("2:3,") }
+        }
+    }
+
+    @Test
+    fun `입력 값 예외 테스트_커스텀 구분자로 시작`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("//#\n2#") }
+        }
+    }
+
     override fun runMain() {
         main()
     }
