9c9939bc8fc3c9ecc4762f4ef26e5e6ce8358f1a
yrsel <xxknu88@gmail.com>
2024-10-17 21:32:25 +0900

    fix(Application): 테스트 케이스에 맞는 형태로 구현 수정 제공된 테스트 케이스를 만족시키기 위한 구현 변경
    while 반복문을 사용해서 사용자가 프로그램 종료를 원하기 이전 또는 예외발생 시키기전까지 받을 수 있게 구현하였다.
    -> 제공된 테스트 케이스를 확인하니 한번의 입력으로 한정되어 있어서 변경하였다.
    
    또한 try-catch를 통해 예외를 잡아서 main() 실행을 종료시키는 방식으로 구현하였다.
    -> 예외가 발생해 프로그램이 종료되는 것이 요구사항인 것을 반영해 throw로 예외만 상위로 던져주는 방식으로 main()에서 결국 예외를 발생 시키며 종료되는 방식으로 구현을 변경하였다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,18 +6,9 @@ import calculator.util.Converter
 
 fun main() {
     val calculator = Calculator()
-    while (true) {
-        println(WELCOME_MESSAGE)
-        val input = Input.readLine()
-        if (exitCalculator(input)) break
-        try {
-            val numbers = Converter.extractNumberByInput(input)
-            System.out.format(Constant.RESULT_FORMAT, calculator.sum(numbers))
-        } catch (e: IllegalArgumentException) {
-            System.out.format(Constant.ERROR_MESSAGE, input)
-            return
-        }
-    }
-}
 
-private fun exitCalculator(input: String) = input == "-1"
+    println(WELCOME_MESSAGE)
+    val input = Input.readLine()
+    val numbers = Converter.extractNumberByInput(input)
+    System.out.format(Constant.RESULT_FORMAT, calculator.sum(numbers))
+}
