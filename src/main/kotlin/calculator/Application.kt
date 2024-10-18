06f2f3f488876f38141a275b4a7e76343dadefbb
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-18 23:08:59 +0900

    feat(Application): 기능을 종합해서 프로그램 구현
    - 안내 메세지를 통해 문자열 입력과 커스텀 구분자 설명
    - 사용자 입력을 통해 숫자화 시켜서 합산을 출력
    - 사용자 입력을 통해 커스텀 구분자를 표시
     - 사용자가 구분자를 잘못 사용한 경우 알리기 위해서

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,19 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    val userInput = UserInput()
+    val guideOutput = GuideOutput()
+    val resultOutput = ResultOutput()
+    val calculator = Calculator()
+    val numberTokenizer = NumberTokenizer()
+
+    guideOutput.guideDelimiters()
+    guideOutput.guideCustomDelimiters()
+    guideOutput.guideAdditionInput()
+    val input = userInput.getString()
+    val numbers = numberTokenizer.tokenize(input)
+    val customDelimiters = numberTokenizer.extractCustomDelimiters(input)
+    val sum = calculator.sum(numbers)
+    resultOutput.outputCustomDelimiters(customDelimiters)
+    resultOutput.outputAddition(sum)
 }
