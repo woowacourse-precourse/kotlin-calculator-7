cc44144676f3d52993195695562a15082b55cdea
박지민 <pjm5489@naver.com>
2024-10-18 23:10:10 +0900

    refactor: 변수명 변겅

--- src/main/kotlin/calculator/model/Calculator.kt
+++ src/main/kotlin/calculator/model/Calculator.kt
@@ -7,18 +7,18 @@ class Calculator {
     fun defaultCal(inputValue: String): Int {
         val inputValueList = inputValue.split(COMMA, COLON).map { it.toInt() }
         if (negativeNumberCheck(inputValueList)) return -1
-        val value = inputValueList.sum()
+        val total = inputValueList.sum()
 
-        return value
+        return total
     }
 
     fun customCal(inputValue: String): Int {
         val customDivider = inputValue[2]
         val inputValueList = inputValue.substring(5).split(customDivider).map { it.toInt() }
         if (negativeNumberCheck(inputValueList)) return -1
-        val value = inputValueList.sum()
+        val total = inputValueList.sum()
 
-        return value
+        return total
     }
 
     private fun negativeNumberCheck(inputValueList: List<Int>): Boolean {
