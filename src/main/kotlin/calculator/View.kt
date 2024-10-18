f222a99e12e9480a13dbda812b25e9f981964d6e
Grove1212 <stella223@korea.ac.kr>
2024-10-18 16:34:12 +0900

    feat: add input handling and result output functionality

--- src/main/kotlin/calculator/View.kt
+++ src/main/kotlin/calculator/View.kt
@@ -5,12 +5,17 @@ class View() {
         println("문자열 덧셈 계산기입니다.")
     }
 
-    fun getInput() {
-
+    fun getInput(): String {
+        println("더할 숫자를 구분자로 구분하여 입력해주세요.")
+        println("디폴트 구분자 : ',', ':'")
+        println("다른 구분자를 사용하려면 \"//*\\n\"과 같이 구분자를 지정하고 사용해주세요.")
+        println("입력 : ")
+        return readlnOrNull() ?: ""
     }
 
-    fun showResult() {
-
+    fun showResult(result: Int, numbs: List<Int>) {
+        val numAsString = numbs.joinToString(separator = "+")
+        println("$numAsString 는 $result 입니다.")
     }
 
     fun showError(e: Exception) {
