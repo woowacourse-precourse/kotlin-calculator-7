e6d4928b72224e0cc3bb0471bf3a529d67c218af
bbangiee <yoojin1117@gmail.com>
2024-10-18 23:01:50 +0900

    feat: 분리된 숫자들을 정수로 형변환
    형변환하기 전에 숫자인지 검사하는 함수 생성:
    - 빈 값인지 확인
    - 숫자인지 확인
    확인된 숫자 형변환하는 함수 생성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,8 +1,10 @@
 package calculator
 
 fun main() {
-//    inputNumber()
-    println(splitString(inputNumber()))
+    val input = inputNumber()
+    val numberList = splitString(input)
+    didEnterCorrect(numberList)
+    println(translate2Int(numberList))
 }
 
 fun inputNumber(): String? {
@@ -12,24 +14,53 @@ fun inputNumber(): String? {
     return input
 }
 
-fun splitString(input: String?): List<String?> {
-    var delimiters = "[.:]".toRegex()
+fun splitString(input: String?): List<String> {
+    println("spliting elements...")
+    var delimiters = "[,:]".toRegex()
     var numbers = input ?: return listOf("0")
 
     if (numbers.take(2) == "//") {
         val delimiterLastIndex = findCustomDelimiterIndex(numbers)
 
-        delimiters = "[.:${numbers.slice(2..delimiterLastIndex)}]".toRegex()
+        delimiters = "[,:${numbers.slice(2..delimiterLastIndex)}]".toRegex()
         numbers = numbers.drop(delimiterLastIndex + 3)
     }
     return numbers.split(delimiters)
 }
 
 fun findCustomDelimiterIndex(input: String): Int {
+    println("searching custom delimiter...")
     for (i in 3..<input.length) {
         if (input[i] == '\\' && input[i + 1] == 'n') {
             return i - 1
         }
     }
     throw IllegalArgumentException()
+}
+
+fun translate2Int(numbers: List<String>): List<Int> {
+    println("translating...")
+    return numbers.map { it.toInt() }
+}
+
+fun didEnterCorrect(numbers: List<String>) {
+    println("checking elements...")
+    if (doesListHaveEmptyValue(numbers) || isListNotDigit(numbers)) {
+        throw IllegalArgumentException()
+    }
+}
+
+fun doesListHaveEmptyValue(numbers: List<String>): Boolean {
+    println("checking empty value...")
+    return numbers.contains("")
+}
+
+fun isListNotDigit(numbers: List<String>): Boolean {
+    println("checking not digit...")
+    for (n in numbers.map { it.single() }) {
+        if (!n.isDigit()) {
+            return true
+        }
+    }
+    return false
 }
