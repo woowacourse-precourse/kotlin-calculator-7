2889eaf1030ec2220b06ed295bf1fecc4096e2df
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-10-18 16:23:11 +0900

    ✨ Feat: 소수점 계산 기능 추가
    Int를 Double로 변경, .0 소수점 처리 로직 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,19 +6,25 @@ fun main() {
 
     try {
         val result = add(input)
-        println("결과 : $result")
+
+        // 결과 소수점 구분 출력 로직
+        if (result == result.toInt().toDouble()) {
+            println("결과 : ${result.toInt()}")
+        } else {
+            println("결과 : $result")
+        }
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
 }
 
-fun add(input: String): Int {
-    if (input.isEmpty()) return 0
+fun add(input: String): Double {
+    if (input.isEmpty()) return 0.0
 
     var delimiters = listOf(",", ":")
     var numbers = input
 
-    // //와 \n 사이 문자열을 커스텀 구분자로 추가
+    // //와 \n 사이 문자열을 커스텀 구분자로 추가하는 로직
     if (input.startsWith("//")) {
         val endOfDelimiterIndex = input.indexOf("\\n")
 
@@ -32,10 +38,10 @@ fun add(input: String): Int {
         numbers = input.substring(endOfDelimiterIndex + 2)
     }
 
-    // numbers를 저장된 구분자(delimiters)로 split 후 sum 계산
+    // numbers를 저장된 구분자(delimiters)로 split 후 sum 계산하는 로직
     val tokens = numbers.split(*delimiters.toTypedArray())
     val sum = tokens.map {
-        it.toIntOrNull() ?: throw IllegalArgumentException("-> Error) 숫자 입력 오류")
+        it.toDoubleOrNull() ?: throw IllegalArgumentException("-> Error) 숫자 입력 오류")
     }.sum()
 
     return sum
