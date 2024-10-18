4ebe1bd67d055c920e201e2ffbed8823bfd18716
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-10-18 20:39:49 +0900

    feat: rename numEmpty to defaultSum and handle default delimiters
    Renamed the numEmpty function to defaultSum. Updated function to handle default delimiters ( , and : ) and calculate the sum of split values. Adjusted the main function to use defaultSum for input.
    
    BREAKING CHANGE: Renamed numEmpty to defaultSum and added functionality to split input by default delimiters, and changed how inputs are processed.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,18 +1,21 @@
 package calculator
 import camp.nextstep.edu.missionutils.Console
 
-fun main() {
-
-    val input = Console.readLine()
-    val result = numEmpty(input)
-
-    println("결과 : $result")
-}
-// input이 빈문자열이거나 null값인지 확인하는 함수
-fun numEmpty(input: String?): Int {
+// 기본구분자에 대한 숫자 합산하는 함수
+fun defaultSum(input: String?): Int {
     if (input.isNullOrEmpty()) {
         return 0
     }
-    return -1   // 임시반환값, 이후 기능에서 처리
+    // 기본 구분자에 대한 숫자 합산
+    val sep1 = input.split(",", ":")
+    return sep1.sumOf { it.toInt() }
+}
+
+fun main() {
+
+    val input = Console.readLine()
+    val result = defaultSum(input)
+
+    println("결과 : $result")
 }
 
