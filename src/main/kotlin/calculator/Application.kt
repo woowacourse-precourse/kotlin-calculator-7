5c548f33865b242ec0fa8169648ec2927aea7fea
yewon-yw <kimyewon0401@gmail.com>
2024-10-18 17:35:51 +0900

    feat: 구분자 리스트 추가
    구분자 리스트를 추가했습니다
    - 기본 구분자
    - 커스텀 구분자는 있는 경우에만 리스트에 추가
    - 정규표현식을 사용해서 커스텀 구분자 추출

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,5 +5,18 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // 사용자 입력
     val input = Console.readLine()
-    print(input)
+
+    // 구분자 리스트
+    val delimiters = mutableListOf(",", ":")
+    val customDelimiterRegex = Regex("^(//)(.*)(\\\\n).*")
+
+    if (hasCustomDelimiter(customDelimiterRegex, input)) {
+        val customDelimiter = getCustomDelimiter(customDelimiterRegex, input)
+            ?: throw IllegalArgumentException("not found custom delimiter")
+        delimiters.add(customDelimiter)
+    }
 }
+
+fun hasCustomDelimiter(regex: Regex, input: String) = input.matches(regex)
+
+fun getCustomDelimiter(regex: Regex, input: String) = regex.find(input)?.groupValues?.get(2)
