f0ffc0307ca3d5096064be050c8ec255ff09e4e6
박지민 <pjm5489@naver.com>
2024-10-18 22:19:31 +0900

    feat: 음수 포함시 예외처리 기능 추가

--- src/main/kotlin/calculator/model/Calculator.kt
+++ src/main/kotlin/calculator/model/Calculator.kt
@@ -3,6 +3,7 @@ package calculator.model
 class Calculator {
     fun defaultCal(inputValue: String): Int {
         val inputValueList = inputValue.split(',', ':').map { it.toInt() }
+        if (negativeNumberCheck(inputValueList)) return -1
         val value = inputValueList.sum()
 
         return value
@@ -10,9 +11,17 @@ class Calculator {
 
     fun customCal(inputValue: String): Int {
         val customDivider = inputValue[2]
-        val inputValueList = inputValue.split(customDivider).map { it.toInt() }
+        val inputValueList = inputValue.substring(5).split(customDivider).map { it.toInt() }
+        if (negativeNumberCheck(inputValueList)) return -1
         val value = inputValueList.sum()
 
         return value
+    }
+
+    private fun negativeNumberCheck(inputValueList: List<Int>): Boolean {
+        for (i in inputValueList.indices) {
+            if (inputValueList[i] < 0) return true
+        }
+        return false
     }
 }
