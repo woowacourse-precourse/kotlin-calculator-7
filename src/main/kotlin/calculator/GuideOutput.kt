2fa684dc52a2dbb9ba365cd0bfa0ee4623b90901
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-18 21:32:36 +0900

    feat(GuideOutput): 사용 방식을 안내하는 기능 추가
    - 덧셈할 문자열 예시를 포함한 안내 기능
    - 기초 구분자가 무엇인지 안내 기능
    - 커스텀 구분자 선언 방식과 예시를 포함한 안내 기능

---
+++ src/main/kotlin/calculator/GuideOutput.kt
@@ -0,0 +1,10 @@
+package calculator
+
+class GuideOutput {
+
+    fun guideAdditionInput() = println("덧셈할 문자열을 구분자로 나눠서 입력해 주세요.(예시 1,3,5,6)\n")
+
+    fun guideDelimiters() = println("기초 구분자는 ,(반점)과 :(콜론) 입니다. ")
+
+    fun guideCustomDelimiters() = println("커스텀 구분자 선언 방식 : //구분자\\n (예시 //;\\n3;6,8:7)")
+}
