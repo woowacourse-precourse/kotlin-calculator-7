a65f99d7b30da592fa82adc2b3a6db479ec017e0
박지민 <pjm5489@naver.com>
2024-10-19 02:17:25 +0900

    style: 줄바꿈

--- src/main/kotlin/calculator/model/Calculator.kt
+++ src/main/kotlin/calculator/model/Calculator.kt
@@ -7,7 +7,9 @@ class Calculator {
     private val errorCheck = ErrorCheck()
 
     fun defaultCal(inputValue: String): Int {
-        val inputValueList = inputValue.split(COMMA, COLON).map { it.toInt() }
+        val inputValueList = inputValue
+            .split(COMMA, COLON)
+            .map { it.toInt() }
         if (errorCheck.negativeNumberCheck(inputValueList)) return -1
         val total = inputValueList.sum()
 
@@ -16,7 +18,10 @@ class Calculator {
 
     fun customCal(inputValue: String): Int {
         val customDivider = inputValue[2]
-        val inputValueList = inputValue.substring(5).split(customDivider).map { it.toInt() }
+        val inputValueList = inputValue
+            .substring(5)
+            .split(customDivider)
+            .map { it.toInt() }
         if (errorCheck.negativeNumberCheck(inputValueList)) return -1
         val total = inputValueList.sum()
 
