1e87e5487e092de9d1bdb4f464b4891df1e197e4
wondreaming <5959.wondreaming@gmail.com>
2024-10-18 14:52:47 +0900

    style(constant): ErrorConst 상수에 String 타입 추가
    기존 ErrorConst 객체의 상수들에 명시적으로 : String 타입을 추가하여 코드의 명확성을 높임

--- src/main/kotlin/calculator/constant/ErrorConst.kt
+++ src/main/kotlin/calculator/constant/ErrorConst.kt
@@ -1,12 +1,12 @@
 package calculator.constant
 
 object ErrorConst {
-    const val INPUT_ERROR_MSG = "잘못된 입력입니다: "
-    const val INPUT_NULL_EXCEPTION_MSG = "사용자가 enter를 포함하여 아무것도 입력을 하지 않았습니다."
-    const val INPUT_LETTER_EXCEPTION_MSG = "에 문자가 포함되어 있습니다."
-    const val INPUT_WHITESPACE_EXCEPTION_MSG = "에 공백 문자가 포함되어 있습니다."
-    const val INPUT_SURROGATE_EXCEPTION_MSG = "에 유니코드 문자 또는 이모지가 포함되어 있습니다."
-    const val INPUT_ESCAPE_EXCEPTION_MSG = "에 escape 문자가 포함되어 있습니다."
-    const val INPUT_NOT_NUMBER_EXCEPTION_MSG = "는 숫자로 변환할 수 없습니다."
-    const val INPUT_NOT_POSITIVE_NUMBER_EXCEPTION_MSG = "은 양수가 아닙니다."
+    const val INPUT_ERROR_MSG: String = "잘못된 입력입니다: "
+    const val INPUT_NULL_EXCEPTION_MSG: String = "사용자가 enter를 포함하여 아무것도 입력을 하지 않았습니다."
+    const val INPUT_LETTER_EXCEPTION_MSG: String = "에 문자가 포함되어 있습니다."
+    const val INPUT_WHITESPACE_EXCEPTION_MSG: String = "에 공백 문자가 포함되어 있습니다."
+    const val INPUT_SURROGATE_EXCEPTION_MSG: String = "에 유니코드 문자 또는 이모지가 포함되어 있습니다."
+    const val INPUT_ESCAPE_EXCEPTION_MSG: String = "에 escape 문자가 포함되어 있습니다."
+    const val INPUT_NOT_NUMBER_EXCEPTION_MSG: String = "는 숫자로 변환할 수 없습니다."
+    const val INPUT_NOT_POSITIVE_NUMBER_EXCEPTION_MSG: String = "은 양수가 아닙니다."
 }
