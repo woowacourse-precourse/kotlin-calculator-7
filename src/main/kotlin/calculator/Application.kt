037bf77a5389468f6cf936c01405d323dcf7ade4
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-10-19 03:23:11 +0900

    feat and refactor: defaultSum와 customSum 함수를 runException 함수에 통합
    defaultSum과 customSum 함수 제거 후  runException 함수 내에 통합하여 코드 중복 줄임, totalSum 함수에서 숫자의 합산만 담당하도록 역할을 분리함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,51 +2,38 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 // 잘못된 숫자나 음수 입력에 대한 예외 처리 함수
-fun runException(input: String) {
+fun runException(input: String): List<Int> {
     // 문자열을 구분자 기준으로 분리
-    val numbers = input.split(",", ":")
+    val numbers = if (input.startsWith("//")) {
+        val part = input.split("\\n")
+        val delimiter = part[0][2]
+        part[1].split(delimiter)
+    } else {
+        input.split(",", ":")
+    }
     // 각 숫자에 대한 유효성 검사
-    for (number in numbers) {
+    return numbers.map { number ->
         // 문자열을 정수로 변환, 변환할 수 없으면 예외 발생
         val value = number.toIntOrNull() ?: throw IllegalArgumentException("잘못된 숫자입니다: $number")
         // 음수일 경우 예외 발생
         if (value < 0) {
             throw IllegalArgumentException("음수는 허용되지 않습니다: $value")
         }
+        value
     }
 }
 
-// 기본 구분자에 대한 숫자 합산하는 함수
-fun defaultSum(input: String?): Int {
-
-    if (input.isNullOrEmpty()) {
-        return 0
-    }
-    runException(input)
-    val sep1= input.split(",",":")   // 기본 구분자를 기준으로 분리
-    return sep1.sumOf { it.toInt() }
-}
-
-// 커스텀 구분자에 대한 숫자 합산하는 함수
-fun customSum(input: String): Int {
-
-    val part = input.split("\\n")   // \n 기준으로 두 파트로 분리
-    val delimiter = part[0][2]  // 구분자 추출
-    val sep2 = part[1].split(delimiter)  // 커스텀 구분자를 기준으로 분리
-
-    return sep2.sumOf { it.toInt() }
+// 기본 구분자 또는 커스텀 구분자에 대한 숫자 합산하는 함수
+fun totalSum(numbers: List<Int>): Int {
+    return numbers.sum()
 }
 
 fun main() {
 
     val input = Console.readLine()
-    val result: Int
-    // 기본 구분자와 커스텀 구분자의 경우 분리
-    if (input.startsWith("//")) {
-        result = customSum(input)
-    } else {
-        result = defaultSum(input)
-    }
+    val numbers = runException(input)   //예외 처리, 숫자 리스트 반환
+    val result = totalSum(numbers)
+
     println("결과 : $result")
 }
 
