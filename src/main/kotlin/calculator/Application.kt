08683ef377eb0ab226afca22ebeb410d03ce31cf
윤승일(Seung-Il Yoon) <liqurt@gmail.com>
2024-10-18 14:41:41 +0900

    feat(processing) : 숫자의 합 계산

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,18 +3,20 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    var result : Int = 0
+
     // 문자열 입력
     println("덧셈할 문자열을 입력해 주세요.")
     val inputStr = Console.readLine()
     Console.close()
 
-    // 문자열 연산
     if(inputStr.isNotEmpty()){
         if(inputStr[0] == '/' && inputStr[1] == '/' && inputStr[3]=='\\' && inputStr[4] == 'n'){ // 커스텀 구분자 있음
             val customGubun = inputStr[2].toString()
-            inputStr.substring(5).split(",",":",customGubun).sumOf { it.toInt() }
+            result = inputStr.substring(5).split(",",":",customGubun).sumOf { it.toInt() }
         }else{
-            inputStr.split(",",":").sumOf { it.toInt() }
+            result = inputStr.split(",",":").sumOf { it.toInt() }
         }
     }
+
 }
