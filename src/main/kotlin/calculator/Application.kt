61fee02dd059869f0656684ad85325070ebe9735
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-10-18 22:26:51 +0900

    feat(Application): 커스텀 구분자 찾는 함수 작성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -13,6 +13,23 @@ fun splitStr(str: String): List<Int> {
     if (str == "") {
         return arrayListOf(0)
     } else {
-        return str.split(",", ":").filter { it.isNotEmpty() }.map { it.toInt() }
+        val customSeparator = findCustomSeparator(str)
+        // 커스텀 구분자 입력 부분을 제거
+        val strWithoutSep = Regex("""//.+\\n""").replace(str, "")
+        return strWithoutSep.split(",", ":", customSeparator).filter { it.isNotEmpty() }.map { it.toInt() }
     }
+}
+
+fun findCustomSeparator(str: String): String {
+    // "//"의 끝 위치와 "\n"의 시작 위치 찾기
+    val startIndex = str.indexOf("//") + 2
+    val endIndex = str.indexOf("\\n")
+
+    // 구분자가 없으면 빈 문자열 반환
+    if (startIndex < 2 || endIndex == -1 || startIndex >= endIndex) {
+        return ""
+    }
+
+    // "//"와 "\n" 사이의 문자열을 추출하여 반환
+    return str.substring(startIndex, endIndex)
 }
