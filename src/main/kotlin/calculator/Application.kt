9d14103eae3ec2f81fdb3498cf0f86aea871b9f7
Jordan <leesoyoung0112@gmail.com>
2024-10-18 11:40:49 +0900

    feat(Application.kt) : seperator ',' ':' sum calculator Event

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,35 @@
 package calculator
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main() {
-    // TODO: 프로그램 구현
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    println("덧셈할 문자열을 입력해 주세요.")
+    val calc = br.readLine()
+    when(calc.length) {
+        0 -> {
+            // 아무것도 입력하지 않았을 때 0 출력
+            println("결과 : 0")
+            return
+        }
+        1 -> {
+            // 하나만 입력했을 때 숫자인지 확인 & 결과 출력
+            println("결과 : ${calc.toIntOrNull()?:throw IllegalArgumentException("string must be...")}")
+        }
+        else -> {
+            // 커스텀 연산자인지 판단
+            if (calc.substring(0..1) == "//") {
+                // 커스텀 연산자인 경우
+            }
+            else {
+                // 커스텀 연산자를 활용하지 않은 경우
+                val numbers = calc.split(",", ":").map { it.toInt() }
+                println("결과 : ${numbers.sum()}")
+            }
+
+        }
+    }
+
+
 }
