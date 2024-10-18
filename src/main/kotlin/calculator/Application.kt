4769a686660262ccb971e38e7a15ab7abf2183cb
rmadmsjm <dmswjdals12@naver.com>
2024-10-18 23:02:54 +0900

    feat(Application): 기본 덧셈 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,21 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    val inputString = Console.readLine()
+    val result = sumNumbersInString(inputString)
+
+    println("결과 : $result")
+}
+
+fun sumNumbersInString(input: String): Int {
+    if (input.isBlank()) return 0
+
+    val delimiters = listOf(",", ":")
+    val numbers = input.split(*delimiters.toTypedArray())
+
+    return numbers.mapNotNull { it.toIntOrNull() }.sum()
 }
