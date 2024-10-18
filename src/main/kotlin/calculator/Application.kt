123375ec129f33d93d37f171d41a6c9134f4acab
park-hwaan <parkhwan1129@gmail.com>
2024-10-18 19:01:11 +0900

    feat: 구분자 처리 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,35 @@
 package calculator
 
-fun main() {
+import camp.nextstep.edu.missionutils.Console
 
+fun main() {
+    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val userInput = Console.readLine()
+
+}
+
+fun parseUserInput(input: String): List<Int>{
+    //구분자 처리 기능
+    if (input.isEmpty()) return emptyList()
+
+    val customDelimiterRegex = Regex("//(.)\n(.*)")
+    val result = customDelimiterRegex.find(input)
+
+    val delimiters = if (result != null) {
+        val customDelimiter = result.groupValues[1]
+        "[,$customDelimiter:]"
+    } else {
+        "[,:]"
+    }
+
+    val numberString = result?.groupValues?.get(2) ?: input
+
+    return try {
+        numberString.split(Regex(delimiters)).map { it.toInt() }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException()
+    }
 }
 
 
