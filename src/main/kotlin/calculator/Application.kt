e30f002cac531e1d4e44c707299514bbea162480
Jang Muyeong <jmy8876@gmail.com>
2024-10-18 12:13:46 +0900

    refactor:중복된 코드  제거

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -34,32 +34,27 @@ fun parseNumbers(input: String): List<Int> {
         if (customDelimiter.length != 1) {
             throw IllegalArgumentException("커스텀 구분자는 한 글자여야 합니다.")
         }
-        if (customDelimiter.all{it.isDigit()}) {
+        if (customDelimiter.all { it.isDigit() }) {
             throw IllegalArgumentException("커스텀 구분자로 숫자는 사용할 수 없습니다.")
         }
-        if (customDelimiter.any{it.isWhitespace()}) {
+        if (customDelimiter.any { it.isWhitespace() }) {
             throw IllegalArgumentException("커스텀 구분자로 공백 문자는 사용할 수 없습니다.")
         }
 
-        delimiters = listOf(Regex.escape(customDelimiter),",",":")// 커스텀 구분자 설정, 기본 구분자도 함께 사용 가능하도록 수정
+        delimiters =
+            listOf(Regex.escape(customDelimiter), ",", ":")// 커스텀 구분자 설정, 기본 구분자도 함께 사용 가능하도록 수정
         numbersSection = input.substring(delimiterEnd + 2) // 숫자 부분 추출
     }
-    val tokens = numbersSection.split(Regex(delimiters.joinToString ("|"))) // 구분자를 기준으로 숫자 분리
+    val tokens = numbersSection.split(Regex(delimiters.joinToString("|"))) // 구분자를 기준으로 숫자 분리
         .filter { it.isNotBlank() } // 구분자 뒤 공백이 올 경우 무시하고 값 처리
 
     return tokens.map {
-        val number = it.trim() //리스트의 공백 제거(ex:1, 2, 3을 입력 할 경우 공백 제거되서 6 반환)
+        val number = it.trim().toIntOrNull()// 정수만, 소수는 허용하지 않음, 커스텀 구분자가 "."일 경우 혼동 가능성 있음
+            ?: throw IllegalArgumentException("유효한 숫자가 아닙니다.") //리스트의 공백 제거(ex:1, 2, 3을 입력 할 경우 공백 제거되서 6 반환), 예외처리 한번에
 
-        if (number.isEmpty()) {
-            0 // 빈 문자열이 있을 경우 0으로 처리
-
-        } else { // 숫자가 아닌 값이나 음수에 대한 예외 처리
-            val num = number.toIntOrNull() ?: throw IllegalArgumentException("유효한 숫자가 아닙니다.")
-
-            if (num < 0) {
-                throw IllegalArgumentException("음수는 허용되지 않습니다.")
-            }
-            num // 예외 조건을 통과할 경우 수 반환
+        if (number < 0) {
+            throw IllegalArgumentException("음수는 허용되지 않습니다.")
         }
+        number // 예외 조건을 통과할 경우 수 반환
     }
 }
