855cd2d29d13eb4030b59828d7e94f863daee053
TaejungKim <tae0803@naver.com>
2024-10-18 21:48:07 +0900

    Feat: 추가 PromptText 메시지
    - 사용자가 입력한 문자열의 커스텀 구분자 지정 및 하나의 문자 오류 추가

--- src/main/kotlin/calculator/PromptText.kt
+++ src/main/kotlin/calculator/PromptText.kt
@@ -5,6 +5,8 @@ class PromptText {
         const val INPUT_PROMPT_TEXT = "덧셈할 문자열을 입력해 주세요."
         const val OUTPUT_PROMPT_TEXT = "결과 : "
         const val ERROR_INPUT_NULL = "입력 값이 null일 수 없습니다."
+        const val ERROR_INPUT_CUSTOM_DELIMITER = "입력 값의 커스텀 구분자를 지정하는 문자를 정확히 입력해주세요"
+        const val ERROR_INPUT_CUSTOM_DELIMITER_COUNT = "입력 값의 커스텀 구분자를 하나의 문자로만 입력해주세요."
         const val INPUT_DELIMITER_FRONT = "//"
         const val INPUT_DELIMITER_END = "\\n"
     }
