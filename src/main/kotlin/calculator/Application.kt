260511336ea1bda395ff438638a91039a9b49b71
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-10-18 15:53:29 +0900

    feat: split numbers a separator basis
    cuts based on multiple strings using split function

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,5 +2,8 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
-    println("하하")
+    val userInput = readlnOrNull()
+    val result = userInput?.split("[,:]".toRegex())
+
+    println(result)
 }
