433c73f5b8b1ae099aa6eceeb041cafd58b9658f
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-10-18 21:41:01 +0900

    feat: custom separator
    If custom separator exist, add regex.
    And split after separator input string.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,8 +2,19 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
-    val userInput = readlnOrNull()
-    val result = userInput?.split("[,:]".toRegex())
+    val SEPARATOR_COMMAND_FRONT = "//"
+    val SEPARATOR_COMMAND_BACK = "\\n"
 
-    println(result)
+    val userInput = readlnOrNull() ?: ""
+
+    val separatorRegex = Regex("(?<=${SEPARATOR_COMMAND_FRONT})(.*?)(?=${SEPARATOR_COMMAND_BACK})") // 정규식
+    val userSeparator = separatorRegex.find(userInput)?.value
+
+    val onlyNumberInputString =
+        userSeparator?.length?.plus(SEPARATOR_COMMAND_FRONT.length + SEPARATOR_COMMAND_BACK.length - 1) ?: 0
+
+    val result = userInput.substring(onlyNumberInputString).split(",|:|${userSeparator}".toRegex())
+
+    println("결과 : ${result}")
+
 }
