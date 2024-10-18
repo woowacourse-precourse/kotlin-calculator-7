be6d72f811cad3ab26e68d114fa3b40f55bf5134
박지민 <pjm5489@naver.com>
2024-10-19 02:06:30 +0900

    refactor: private 제거, 메서드 위치 변경

--- src/main/kotlin/calculator/model/Divider.kt
+++ src/main/kotlin/calculator/model/Divider.kt
@@ -6,19 +6,11 @@ import calculator.constants.Constants.DOUBLE_BACKSLASH_N
 import calculator.constants.Constants.DOUBLE_SLASH
 
 class Divider {
-    fun checkDivider(inputValue: String): Int {
-        when {
-            isDefault(inputValue) -> return 0
-            isCustom(inputValue) -> return 1
-        }
-        return 2
-    }
-
-    private fun isDefault(inputValue: String): Boolean {
+    fun isDefault(inputValue: String): Boolean {
         return !isCustom(inputValue) && inputValue.contains(COMMA) || inputValue.contains(COLON)
     }
 
-    private fun isCustom(inputValue: String): Boolean {
+    fun isCustom(inputValue: String): Boolean {
         return (inputValue.contains(DOUBLE_SLASH) && inputValue.contains(DOUBLE_BACKSLASH_N))
     }
 }
