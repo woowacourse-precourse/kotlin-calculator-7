b89ef8d9ba70a8931be2dfd1aba5f952c7caf1a8
yewon-yw <kimyewon0401@gmail.com>
2024-10-18 20:58:13 +0900

    feat: 숫자 추출
    구분자를 기준으로 숫자를 추출
    - 구분자로 문자열을 분리하는 함수 추가
    - 문자열을 정수로 변환하는 함수 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,13 +10,27 @@ fun main() {
     val delimiters = mutableListOf(",", ":")
     val customDelimiterRegex = Regex("^(//)(.*)(\\\\n).*")
 
+    var parsedInput = input
     if (hasCustomDelimiter(customDelimiterRegex, input)) {
         val customDelimiter = getCustomDelimiter(customDelimiterRegex, input)
             ?: throw IllegalArgumentException("not found custom delimiter")
         delimiters.add(customDelimiter)
+        parsedInput = input.substring(customDelimiter.length + 4)
+    }
+
+    // 숫자 추출
+    try {
+        val numbers = splitByDelimiters(parsedInput, delimiters).toNumbers()
+        println(numbers)
+    } catch (e: Exception) {
+        throw IllegalArgumentException("unable to extract numbers")
     }
 }
 
 fun hasCustomDelimiter(regex: Regex, input: String) = input.matches(regex)
 
 fun getCustomDelimiter(regex: Regex, input: String) = regex.find(input)?.groupValues?.get(2)
+
+fun splitByDelimiters(input: String, delimiters: List<String>) = input.split(*delimiters.toTypedArray()).filter { it.isNotBlank() }
+
+fun List<String>.toNumbers() = this.map { it.toInt() }
