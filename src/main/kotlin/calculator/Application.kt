de1adcfac48c267f55f7d963574a325ea1f5d130
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-10-18 19:57:36 +0900

    feat(Application): 구분자를 기준으로 문자열 분리하는 기능 작성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,6 +5,14 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val str = Console.readLine()
-    println(str)
+    val splitStr = splitStr(str)
+    println(splitStr)
 }
 
+fun splitStr(str: String): List<Int> {
+    if (str == "") {
+        return arrayListOf(0)
+    } else {
+        return str.split(",", ":").filter { it.isNotEmpty() }.map { it.toInt() }
+    }
+}
