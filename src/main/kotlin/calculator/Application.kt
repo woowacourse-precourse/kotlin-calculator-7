a1fd41d2a971cb0b5b581498b2ed914c3a47b8ce
Jieun <lje2000lje@naver.com>
2024-10-19 03:36:38 +0900

    feat(Application.kt): 입력 유효성 검사 및 구분자 관리를 위한 상수 정의

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,6 +2,12 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
+private const val CUSTOM_DELIMITER_PREFIX = "//"
+private const val CUSTOM_DELIMITER_SUFFIX = "\\n"
+
+private const val NEWLINE_INDEX_NOT_FOUND = -1
+private const val CUSTOM_DELIMITER_PREFIX_LENGTH = 2
+
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
