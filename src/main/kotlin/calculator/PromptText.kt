994963a524c7fc9bce75b049fb16f0bec6c25303
TaejungKim <tae0803@naver.com>
2024-10-18 20:41:02 +0900

    feat: PromptText 메시지 추가
    - 문자열 덧셈 계산기 메시지 관리를 위한 메시지 변수 추가

--- src/main/kotlin/calculator/PromptText.kt
+++ src/main/kotlin/calculator/PromptText.kt
@@ -4,5 +4,6 @@ class PromptText {
     companion object{
         const val INPUT_PROMPT_TEXT = "덧셈할 문자열을 입력해 주세요."
         const val OUTPUT_PROMPT_TEXT = "결과 : "
+        const val ERROR_INPUT_NULL = "입력 값이 null일 수 없습니다."
     }
 }
