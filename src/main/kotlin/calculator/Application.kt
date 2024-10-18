c2c8e48cf91e7eee214cc5e5368143120ce2da83
bbangiee <yoojin1117@gmail.com>
2024-10-18 21:01:31 +0900

    feat: 입력받은 문자열 분리하기
    문자열 분리해서 list 생성하는 함수 생성:
    - 커스텀 구분자 사용 가능
    커스텀 구분자 인덱스 반환하는 함수 생성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,8 @@
 package calculator
 
 fun main() {
-    inputNumber()
+//    inputNumber()
+    println(splitString(inputNumber()))
 }
 
 fun inputNumber(): String? {
@@ -9,4 +10,26 @@ fun inputNumber(): String? {
     val input = readLine()
 
     return input
+}
+
+fun splitString(input: String?): List<String?> {
+    var delimiters = "[.:]".toRegex()
+    var numbers = input ?: return listOf("0")
+
+    if (numbers.take(2) == "//") {
+        val delimiterLastIndex = findCustomDelimiterIndex(numbers)
+
+        delimiters = "[.:${numbers.slice(2..delimiterLastIndex)}]".toRegex()
+        numbers = numbers.drop(delimiterLastIndex + 3)
+    }
+    return numbers.split(delimiters)
+}
+
+fun findCustomDelimiterIndex(input: String): Int {
+    for (i in 3..<input.length) {
+        if (input[i] == '\\' && input[i + 1] == 'n') {
+            return i - 1
+        }
+    }
+    throw IllegalArgumentException()
 }
