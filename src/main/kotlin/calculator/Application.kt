7361b248b97c5f337c13bda964da1bfe19ac4da3
joonhokil <kiljh@me.com>
2024-10-18 20:23:17 +0900

    feat: 구분자로 ":" 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -7,7 +7,7 @@ fun main() {
 fun calculate(string: String): Int {
     if (string == "") return 0
     
-    val strings = string.split(",")
+    val strings = string.split(",", ":")
     val numbers = strings.map { it.toInt() }
 
     return numbers.sum()
