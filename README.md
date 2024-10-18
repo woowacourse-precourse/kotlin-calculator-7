93c17a988879a7da40aaf436e19430835f457736
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-10-18 19:47:33 +0900

    feat: add function to handle empty or null input
    Add numEmpty function to return 0 when the input is empty or null.
    This change prevents null pointer exceptions and provides a default output for empty inputs.
    
    BREAKING CHANGE: The application now includes validation for empty and null inputs.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kotlin-calculator-precourse
+
+1. 빈 문자열 또는 null 값 처리 (numEmpty 함수)
+2. 기본 구분자로 숫자 합산 기능 (defaultSum 함수)
+3. 커스텀 구분자로 숫자 합산 기능 (customSum 함수)
+4. 분리된 숫자들 합산 기능 (totalSum 함수)
+5. 음수와 잘못된 숫자 입력 예외 처리 (runException 함수)
+6. 커스텀 구분자 함수에서의 예외 처리 (customSum 함수 개선)
+7. main 함수에 최종 기능 구현
--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,18 @@
 package calculator
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // TODO: 프로그램 구현
+
+    val input = Console.readLine()
+    val result = numEmpty(input)
+
+    println("결과 : $result")
 }
+// input이 빈문자열이거나 null값인지 확인하는 함수
+fun numEmpty(input: String?): Int {
+    if (input.isNullOrEmpty()) {
+        return 0
+    }
+    return -1   // 임시반환값, 이후 기능에서 처리
+}
+
