cfa6bced9bb4903f9f46466513bee5b3abb977fb
Jieun <lje2000lje@naver.com>
2024-10-19 03:55:46 +0900

    feat(Application.kt): 입력값 유효성 검증 함수 및 숫자 유효성 검증 함수 구현
    빈 값을 입력한 경우, 구분자를 제대로 작성하지 않은 경우,
    입력한 값이 올바르지 않은 경우 유효성 검사 후 false를 리턴

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -12,3 +12,29 @@ fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
 }
+
+private fun checkInputValidity(input: String): Triple<Boolean, String?, String> {
+    if (input.isEmpty()) return Triple(false, null, "")
+
+    val (delimiter, numbers) = if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+        val newlineIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX)
+
+        if (newlineIndex != CUSTOM_DELIMITER_SUFFIX_NOT_FOUND) {
+            input.substring(CUSTOM_DELIMITER_PREFIX_LENGTH, newlineIndex) to input.substring(newlineIndex + CUSTOM_DELIMITER_PREFIX_LENGTH)
+        } else return Triple(false, null, "")
+
+    } else {
+        null to input
+    }
+
+    val isValid =
+        numbers.split(delimiter ?: ",", ":").all { it.isEmpty() || isValidNumber(it.trim()) }
+
+    return Triple(isValid, delimiter, numbers)
+}
+
+private fun isValidNumber(value: String): Boolean {
+    val number = value.toDoubleOrNull()
+
+    return number != null && number > 0 && number <= Int.MAX_VALUE
+}
