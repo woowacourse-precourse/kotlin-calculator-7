fe8e23ffde2473382f91a90c08e9a2553c7206bc
dioo1461 <dioo1461@gmail.com>
2024-10-19 21:19:58 +0900

    feat: 잘못된 입력에 대한 에러 처리 구현
    에러 처리 로직은 다른 기능들과 완전히 책임 분리하여 별개의 메서드로 구현하였음.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     val input: String = getInput("덧셈할 문자열을 입력해 주세요.")
+    inspectError(input)
     val operands: MutableList<Long> = parseInput(input)
     printSum("결과 : ", operands)
 }
@@ -28,8 +29,7 @@ fun parseInput(input: String): MutableList<Long> {
                 delimiterList.addLast(input[idx])
                 idx++
             }
-            // input[idx] = '\\' 인 상태
-
+            // input[idx] = '\\' (backslash)인 상태
             if (input[idx + 1] == 'n') {
                 expression = input.substring(idx + 2)
                 break
@@ -48,8 +48,8 @@ fun parseExpression(expression: String, delimiterList: MutableList<Char>): Mutab
     var number = mutableListOf<Char>()
     for (character in expression) {
         var isDelimiter = false
-        delimiterList.forEach { delimiter ->
-            if (character == delimiter) isDelimiter = true
+        if (delimiterList.find { it == character } != null) {
+            isDelimiter = true
         }
         if (isDelimiter) {
             numberList.add(number.joinToString("").toLong())
@@ -70,5 +70,50 @@ fun printSum(responseMessage: String, numberList: MutableList<Long>) {
 }
 
 fun inspectError(input: String) {
+    val delimiterList = mutableListOf<Char>(',', ':')
+    var inspectIndex = 0
+    var prevCharacter: Char? = null
 
+    if (input.startsWith("//")) {
+        inspectIndex = 2
+        while (inspectIndex < input.length) {
+            while (input[inspectIndex] != '\\') {
+                delimiterList.addLast(input[inspectIndex])
+                inspectIndex++
+                prevCharacter = input[inspectIndex]
+            }
+            // input[idx] = '\\' (backslash)인 상태
+            if (input[inspectIndex + 1] == 'n') {
+                inspectIndex += 2
+                prevCharacter = 'n'
+                break
+            }
+
+            delimiterList.addLast(input[inspectIndex])
+            prevCharacter = input[inspectIndex]
+            inspectIndex++
+        }
+    }
+    
+    while (inspectIndex < input.length) {
+        var isDelimiter = false
+        if (delimiterList.find { it == input[inspectIndex] } != null) {
+            isDelimiter = true
+        }
+
+
+        if (isDelimiter) {
+            // 첫 번째 character로 구분자가 나타난 경우
+            if (prevCharacter == null) throw IllegalArgumentException()
+            // 구분자가 연속 두 번 나타난 경우
+            if (delimiterList.find { it == prevCharacter } != null) throw IllegalArgumentException()
+        } else {
+            // 숫자가 아닌 경우
+            if (!input[inspectIndex].isDigit()) throw IllegalArgumentException()
+        }
+        prevCharacter = input[inspectIndex]
+        inspectIndex++
+    }
+
+    if (delimiterList.find { it == prevCharacter } != null) throw IllegalArgumentException()
 }
