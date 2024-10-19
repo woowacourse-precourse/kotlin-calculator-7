ede624ec9ed4defc77e974921982115eb8765ed9
지원 <cheerfulboys@naver.com>
2024-10-20 01:38:56 +0900

    fix(.): NoSuchElementException 발생 에러 수정
    테스트 중 입력 값이 없을 때 발생하는 에러를 수정함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,9 +2,7 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
-fun input(): String {
-    return Console.readLine()
-}
+fun input(): String = Console.readLine()
 
 fun splitNumber(str: String): List<Int> {
     var numberList: List<Int> = listOf()
@@ -19,9 +17,8 @@ fun splitNumber(str: String): List<Int> {
     return numberList
 }
 
-fun sum(numbers: List<Int>): Int {
-    return numbers.sum()
-}
+fun sum(numbers: List<Int>): Int = numbers.sum()
+
 
 fun isValid(str: String): Boolean {
     str.forEach { if (it !in '0'..'9' && it != ',' && it != ':') return false }
@@ -31,7 +28,7 @@ fun isValid(str: String): Boolean {
 fun checkCustom(str: String): Boolean {
     var str = str
 
-    if(str.contains("//") && str.contains("\\n")) {
+    if (str.contains("//") && str.contains("\\n")) {
         return true
     }
     return false
@@ -57,18 +54,20 @@ fun replaceCustom(str: String): String {
 fun main() {
     println("덧셈할 문자열을 입력해주세요.")
 
-    var inputString = input()
-
-    if (!isValid(inputString) && checkCustom(inputString)) {
-        inputString = replaceCustom(inputString)
-    }
-
     try {
+        var inputString = input()
+
+        if (!isValid(inputString) && checkCustom(inputString)) {
+            inputString = replaceCustom(inputString)
+        }
+
         val numbers = splitNumber(inputString)
         val result = sum(numbers)
         println("결과 : $result")
+
+    } catch (e: NoSuchElementException) {
+        println("결과 : 0")
     } catch (e: IllegalArgumentException) {
         throw e
     }
-
 }
