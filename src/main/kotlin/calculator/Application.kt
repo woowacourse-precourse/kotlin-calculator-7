186b20f4f1478a5b726f44fe585fa1a437f62b78
minjun011026 <jun011026@gmail.com>
2024-10-19 17:06:11 +0900

    docs: 기능 목록 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,6 +1,11 @@
 package calculator
 import camp.nextstep.edu.missionutils.Console
+
+fun sumOfNumber(input : String) : Int{
+    val regex = Regex("[,;]")
+    val ()
+}
 fun main() {
     val input = Console.readLine()
-    println(input)
+    println("출력 : ${sumOfNumber(input)}")
 }
