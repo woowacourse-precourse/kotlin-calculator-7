efdf5f1207344bc0381d07ba9b7ba7e44e9faeb2
JCTA0125 <jcta0125@duksung.ac.kr>
2024-10-20 02:32:41 +0900

    feat: 문자열 입력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,16 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해주세요.")
+    var inputString = Console.readLine()
+    try {
+        // Classification
+        //taskClassification(inputString)
+    } catch (e: IllegalArgumentException){
+        println(e.message)
+    }
 }
+
