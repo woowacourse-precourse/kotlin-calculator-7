27f244d65a1bbfc3a7490b2e7d8700104d3e819c
choiminjun <mj010504@naver.com>
2024-10-19 18:20:34 +0900

    refactor: 에러 메시지 상수로 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,6 +2,11 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
+const val ERROR_INVALID_CUSTOM_DELIMITER = "구분자로 숫자는 사용할 수 없습니다."
+const val ERROR_INVALID_DELIMITER = "올바르지 않은 구분자를 사용했습니다."
+const val ERROR_DELIMITER_POSITION = "구분자는 숫자 사이에 있어야 합니다."
+
+
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     var str : String = Console.readLine()
@@ -18,10 +23,10 @@ fun main() {
            if(!str[2].isDigit()) {
                delimiterList.add(str[2])
                str = str.substring(5, str.length)
-           } else throwInvalidStringException("구분자로 숫자는 사용할 수 없습니다.")
+           } else throwInvalidStringException(ERROR_INVALID_CUSTOM_DELIMITER)
 
         }
-    
+
         var currentNumber = ""
         for(c in str) {
             if(c.isDigit()) currentNumber += c
@@ -31,7 +36,7 @@ fun main() {
                     currentNumber = ""
                 }
                 else {
-                    val errorMessage = if(!delimiterList.contains(c)) "올바르지 않은 구분자를 사용했습니다." else "구분자는 숫자 사이에 있어야 합니다."
+                    val errorMessage = if(!delimiterList.contains(c)) ERROR_INVALID_DELIMITER else ERROR_DELIMITER_POSITION
                     throwInvalidStringException(errorMessage)
                 }
             }
