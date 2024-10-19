003dc6fb35a5dfcce8c7cdf9b05be1e7a558e437
DDangDin <Kcorelec7123@gmail.com>
2024-10-19 23:29:18 +0900

    feat: 커스텀 구분자까지 사용하는 덧셈 구현
    - 커스텀 구분자까지 사용하는 덧셈 기능을 구현
    - 'splitByDelimiters' 함수 파라미터 추가
    - 'splitByDelimiters' 함수에서 커스텀 구분자 접두어 삭제 로직 추가
    - 'getCustomDelimiters' 함수 내부에 있던 delimitersConditions를 전역 적으로 관리 하게끔 수정
    - 최종 구분자 리스트를 만들어주는 'createFinalDelimiters' 함수 추가
    
    *최종 구분자 리스트: 기본 구분자 리스트 + 커스텀 구분자

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,18 +3,25 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 private val DEFAULT_DELIMITERS = listOf(",", ":")
+private val DELIMITERS_CONDITIONS = listOf("//", "\\n")
 
 fun main() {
     val input = readLine()
-    val inputStringList = splitByDelimiters(input)
+    val customDelimiters = getCustomDelimiter(input)
+    val finalDelimiters = createFinalDelimiters(customDelimiters)
+    val inputStringList = splitByDelimiters(input, finalDelimiters)
     val inputIntList = inputStringList.map { it.toInt() }
     val sumOfIntList = inputIntList.sum()
     println(sumOfIntList)
 }
 
 // 구분자를 기준으로 문자열을 나눈다.
-private fun splitByDelimiters(input: String): List<String> {
-    return input.split(*DEFAULT_DELIMITERS.toTypedArray())
+private fun splitByDelimiters(
+    input: String,
+    delimiters: List<String>,
+): List<String> {
+    val removedPrefixInput = input.split(DELIMITERS_CONDITIONS[1]).last()
+    return removedPrefixInput.split(*delimiters.toTypedArray())
 }
 
 /**
@@ -22,14 +29,21 @@ private fun splitByDelimiters(input: String): List<String> {
  * 만약 있다면 반환, 없다면 null을 반환한다.
  */
 private fun getCustomDelimiter(input: String): String? {
-    val delimiterConditions = listOf("//", "\\n")
     var cnt = 0
-    delimiterConditions.forEach { dc ->
+    DELIMITERS_CONDITIONS.forEach { dc ->
         if (dc in input) cnt++
     }
-    if (cnt != delimiterConditions.size) {
+    if (cnt != DELIMITERS_CONDITIONS.size) {
         return null
     }
-    val customDelimiter = input.split(*delimiterConditions.toTypedArray())
+    val customDelimiter = input.split(*DELIMITERS_CONDITIONS.toTypedArray())
     return customDelimiter[1]
+}
+
+// 최종적으로 사용할 구분자 리스트를 만든다.
+fun createFinalDelimiters(customDelimiter: String?): List<String> {
+    if (customDelimiter == null) return DEFAULT_DELIMITERS
+    val mutableDefaultDelimiters = DEFAULT_DELIMITERS.toMutableList()
+    mutableDefaultDelimiters.add(customDelimiter)
+    return mutableDefaultDelimiters
 }
