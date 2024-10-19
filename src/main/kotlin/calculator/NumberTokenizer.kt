19ec6e4b708092843b918824e6c6543e8212276e
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-19 18:06:24 +0900

    refactor(NumberTokenizer): 하드코딩 값 상수로 추출
    - 각 값이 어떤 의미인지 명확히 전달하기 위해서
    - 각 값을 재사용하기 위해서

--- src/main/kotlin/calculator/NumberTokenizer.kt
+++ src/main/kotlin/calculator/NumberTokenizer.kt
@@ -2,38 +2,46 @@ package calculator
 
 import java.math.BigDecimal
 
+const val DELIMITER_COMMA = ","
+const val DELIMITER_COLON = ":"
+const val DELIMITER_UNIFIED = " "
+const val NUMBER_MINIMUM_LIMIT = 0
+const val CUSTOM_DELIMITER_PREFIX = "//"
+const val CUSTOM_DELIMITER_SUFFIX = "\\n"
+const val INDEX_NOT_FOUND = 1
+
 class NumberTokenizer {
 
-    private val delimiters = listOf(",", ":")
+    private val delimiters = listOf(DELIMITER_COMMA, DELIMITER_COLON)
 
     fun tokenize(input: String): List<BigDecimal> {
         val customDelimiters = extractCustomDelimiters(input)
         val filteringInput = filterCustom(input, customDelimiters)
         val totalDelimiters = customDelimiters + delimiters
         return totalDelimiters.fold(filteringInput) { acc, delimiter ->
-            acc.replace(delimiter, " ")
-        }.split(" ").map { numberInput ->
+            acc.replace(delimiter, DELIMITER_UNIFIED)
+        }.split(DELIMITER_UNIFIED).map { numberInput ->
             val number = numberInput.toBigDecimal()
-            require(number >= 0.toBigDecimal())
+            require(number >= NUMBER_MINIMUM_LIMIT.toBigDecimal())
             number
         }
     }
 
     fun extractCustomDelimiters(input: String): List<String> {
         val customDelimiters = mutableListOf<String>()
-        val parts = input.split("\\n")
+        val parts = input.split(CUSTOM_DELIMITER_SUFFIX)
         for (part in parts) {
-            val delimiterStartIndex = part.indexOf("//")
-            if (delimiterStartIndex == -1) break
-            val customDelimiter = part.substring(delimiterStartIndex + 2)
-            customDelimiters.add(customDelimiter)
+            val delimiterStartIndex = part.indexOf(CUSTOM_DELIMITER_PREFIX)
+            if (delimiterStartIndex == INDEX_NOT_FOUND) break
+            val customDelimiter = part.substring(delimiterStartIndex)
+            customDelimiters.add(customDelimiter.removeSuffix(CUSTOM_DELIMITER_PREFIX))
         }
         return customDelimiters
     }
 
     private fun filterCustom(input: String, customDelimiters: List<String>): String {
         val filteringInput = customDelimiters.fold(input) { acc, removeDelimiters ->
-            acc.replace("//$removeDelimiters\\n", "")
+            acc.replace(CUSTOM_DELIMITER_PREFIX + removeDelimiters + CUSTOM_DELIMITER_SUFFIX, "")
         }
         return filteringInput
     }
