3c1c9edb617c986b58acbea138ae5b9de6728265
DDangDin <Kcorelec7123@gmail.com>
2024-10-19 22:54:26 +0900

    refactor: 함수 명, 변수 위치 변경
    - 'splitWithDefaultDelimiters' 함수 명을 'splitByDelimiters'로 변경
    - DEFAULT_DELIMITERS가 전역 적으로 관리 되게끔 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,18 +2,19 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console.readLine
 
+private val DEFAULT_DELIMITERS = listOf(",", ":")
+
 fun main() {
     val input = readLine()
-    val inputStringList = splitWithDefaultDelimiters(input)
+    val inputStringList = splitByDelimiters(input)
     val inputIntList = inputStringList.map { it.toInt() }
     val sumOfIntList = inputIntList.sum()
     println(sumOfIntList)
 }
 
-// 기본 구분자를 기준으로 문자열을 나눈다.
-private fun splitWithDefaultDelimiters(input: String): List<String> {
-    val defaultDelimiters = listOf(",", ":")
-    return input.split(*defaultDelimiters.toTypedArray())
+// 구분자를 기준으로 문자열을 나눈다.
+private fun splitByDelimiters(input: String): List<String> {
+    return input.split(*DEFAULT_DELIMITERS.toTypedArray())
 }
 
 /**
