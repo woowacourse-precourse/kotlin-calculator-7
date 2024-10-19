b4706dfd23123f11ba1576a240554c26343e96b9
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-10-19 17:36:03 +0900

    feat(Application): 커스텀 구분자를 반환하는 함수 작성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,6 +2,8 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
+val sepList = mutableListOf(",", ":")
+
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val str = Console.readLine()
@@ -11,23 +13,33 @@ fun main() {
 }
 
 fun splitStr(str: String): List<Int> {
-    if (str == "") {
-        return arrayListOf(0)
+    if (str.trim().isEmpty()) {
+        throw IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.")
     } else {
-        val customSeparator = findCustomSeparator(str)
-        // 커스텀 구분자 입력 부분을 제거
-        val strWithoutSep = Regex("""//.+\\n""").replace(str, "")
-        return strWithoutSep.split(",", ":", customSeparator).filter { it.isNotEmpty() }.map { convertToIntOrThrow(it) }
+        val strWithoutSep = findCustomSeparator(str)
+        println(strWithoutSep)
+        return strWithoutSep.split(*sepList.toTypedArray()).filter { it.isNotBlank() }.map { convertToIntOrThrow(it) }
     }
 }
 
 fun findCustomSeparator(str: String): String {
+    if (str.startsWith("//") && str.contains("\\n")) {
+        val customSeparator = getCustomSeparator(str)
+        println("커스텀 구분자: ${customSeparator.length}")
+        sepList.add(customSeparator)
+        println(sepList)
+        return Regex("""//.*\\n""").replace(str, "")
+    }
+    return str
+}
+
+fun getCustomSeparator(str: String): String {
     // "//"의 끝 위치와 "\n"의 시작 위치 찾기
     val startIndex = str.indexOf("//") + 2
     val endIndex = str.indexOf("\\n")
 
     // 구분자가 없으면 빈 문자열 반환
-    if (startIndex < 2 || endIndex == -1 || startIndex >= endIndex) {
+    if (startIndex <= 2 || endIndex == -1 || startIndex >= endIndex) {
         return ""
     }
 
@@ -45,7 +57,7 @@ fun calculate(strList: List<Int>): Int {
 
 fun convertToIntOrThrow(str: String): Int {
     // 음수인 경우 예외 발생
-    if (str.startsWith("-")) {
+    if (str.toInt() < 0) {
         throw IllegalArgumentException("음수는 입력할 수 없습니다.")
     }
     return str.toInt()
