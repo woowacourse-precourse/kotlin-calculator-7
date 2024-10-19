b606c7ac510f26afcf592980be42da2cec3bd4ae
choiminjun <mj010504@naver.com>
2024-10-19 16:13:02 +0900

    feat: 커스텀 구분자 입력시 delimiterList에 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,20 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val str : String = readln()
+
+    var sum : Int = 0
+    val delimiterList : MutableList<Char> = mutableListOf(',', ':')
+
+    if(str.length >= 5
+        && str.substring(0,2) == "//"
+        && str.substring(3,5) == "\\n"
+        && !str[2].isDigit()
+        ) {
+            delimiterList.add(str[2])
+       }
+
+    
+
 }
