bd00d0988987abf01ce5f4a266ae24e20081965c
PIYUJIN <yjyj020201@gmail.com>
2024-10-20 04:43:49 +0900

    feat($Application.kt): 기본 구분자를 기준으로 분리한 각 숫자의 합 반환 기능 구현
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열 전달시 구분자를 기준으로 분리한 각 숫자의 합 반환 및 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생 시킨후 애플리케이션 종료 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -7,4 +7,41 @@ fun main() {
     // 문자열 입력 받기 (구분자와 양수로 구성된 문자열)
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
+
+    // 구분자를 통한 숫자 추출
+    val splitResult = splitInputData(input)
+
+    // 덧셈 결과 출력
+    println("결과 : ${sumNumber(splitResult)}")
+}
+
+fun splitInputData(input: String): MutableList<Int> {
+
+    var splitIntegerData = mutableListOf<Int>()
+
+    // 아무 값도 입력되지 않은 경우 0 반환
+    if(input.isBlank()) return splitIntegerData
+
+    // 기본 구분자(쉼표, 콜론)을 통한 문자열 구분
+    val splitInputData = input.split(":",",")
+
+    for(i in splitInputData.indices) {
+        // 구분자를 여러번 입력하거나 마지막에 입력한 경우
+        if(splitInputData[i].isNotBlank()) {
+            // 잘못된 값을 입력한 경우
+            splitInputData[i].toIntOrNull() ?: throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
+            // 음수를 입력한 경우
+            if(splitInputData[i].toInt() < 0) throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
+            splitIntegerData.add(splitInputData[i].toInt())
+        } else {
+            continue
+        }
+    }
+
+    return splitIntegerData
+}
+
+fun sumNumber(inputNumber: MutableList<Int>): Int {
+
+    return inputNumber.sumOf{ it }
 }
