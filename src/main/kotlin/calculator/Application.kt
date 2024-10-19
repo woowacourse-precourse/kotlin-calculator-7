8f55567561e314bec72bc9b471cc8d2a15609304
DDangDin <Kcorelec7123@gmail.com>
2024-10-19 21:03:54 +0900

    feat: 기본 구분자를 사용한 덧셈 구현
    기본 구분자를 사용해서 문자열 리스트를 만들고
    문자열 리스트를 기준으로 정수형 매핑 후 합산해주는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,18 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = readLine()
+    val inputStringList = splitWithDefaultDelimiters(input)
+    val inputIntList = inputStringList.map { it.toInt() }
+    val sumOfIntList = inputIntList.sum()
+    println(sumOfIntList)
 }
+
+// 기본 구분자를 기준으로 문자열을 나눈다.
+private fun splitWithDefaultDelimiters(input: String): List<String> {
+    val defaultDelimiters = listOf(",", ":")
+    return input.split(*defaultDelimiters.toTypedArray())
+}
+
