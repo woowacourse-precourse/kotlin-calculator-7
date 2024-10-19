b62629cf3d46a9b15abe4120d46e671768cf5f22
Jokwanhee <jonotch1@naver.com>
2024-10-19 22:08:10 +0900

    feat(Application.kt) : 커스텀 구분자 정규 표현식 구현
    Pattern 클래스를 사용해서 커스텀 구분자 "//\n" 를 구분하는 표현식을 구현합니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,7 @@
 package calculator
 
+import java.util.regex.Pattern
+
 fun main() {
     val calculator = CalculatorImpl()
     calculator.start()
@@ -9,5 +11,12 @@ class CalculatorImpl {
     fun start() {
         println("덧셈할 문자열을 입력해 주세요.")
         process(readln())
+    }
+    private fun isCustomSeparator(input: String): Boolean =
+        customSeparatorPattern.matcher(input).find()
+
+    companion object {
+        private const val CUSTOM_SEPARATOR_PATTERN = """^//.*\\n.*$"""
+        private val customSeparatorPattern = Pattern.compile(CUSTOM_SEPARATOR_PATTERN)
     }
 }
