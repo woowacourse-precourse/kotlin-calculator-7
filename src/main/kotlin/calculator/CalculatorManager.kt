f59301b2646db5b2d8809c2692e636f6a51db679
TaejungKim <tae0803@naver.com>
2024-10-19 15:35:04 +0900

    Feat: 추가 printCalculator 함수
    - 계산하여 반환된 수를 프롬프트에 출력하기 위한 printCalculator 함수 추가

--- src/main/kotlin/calculator/CalculatorManager.kt
+++ src/main/kotlin/calculator/CalculatorManager.kt
@@ -1,16 +1,23 @@
 package calculator
 
 import calculator.PromptText.Companion.INPUT_PROMPT_TEXT
+import calculator.PromptText.Companion.OUTPUT_PROMPT_TEXT
 import camp.nextstep.edu.missionutils.Console
 
 class CalculatorManager {
     fun start(){
         val userInput = getUserInput()
         Validator.validateUserInput(userInput)
+        val result = NumberCalculator.addNumbers(userInput)
+        printCalculate(result)
     }
 
     private fun getUserInput(): String?{
         println(INPUT_PROMPT_TEXT)
         return Console.readLine()
+    }
+
+    private fun printCalculate(result: Int){
+        println(OUTPUT_PROMPT_TEXT + result)
     }
 }
