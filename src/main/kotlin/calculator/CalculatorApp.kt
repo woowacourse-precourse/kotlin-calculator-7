2dd63a278c28362be52c82b4f01131a45c349989
Tnalxmsk <alstnwkd990@naver.com>
2024-10-19 23:42:16 +0900

    refactor: 계산기 프로그램 단계별 함수 분리
    추출, 계산, 결과 세 단계로 분리하여 각각의 메서드로 정의

--- src/main/kotlin/calculator/CalculatorApp.kt
+++ src/main/kotlin/calculator/CalculatorApp.kt
@@ -10,13 +10,20 @@ class CalculatorApp(
 
     fun run() {
         val input = inputView.readString()
-        val result = calculate(input)
+        val result = extract(input)
+        val sum = calculate(result)
+        displayResult(sum)
+    }
 
-        outputView.printResultOfSum(result)
+    private fun extract(input: String): List<Int> {
+        val result = Extractor.extractNumbers(input)
+        return result
     }
 
     private fun calculate(input: List<Int>): Int {
         Calculator.sum(input)
         return Calculator.getResult()
     }
+
+    private fun displayResult(sum: Int) = outputView.printResultOfSum(sum)
 }
