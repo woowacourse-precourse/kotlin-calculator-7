bcac636aea209eb685a2c42639df4a3101f8cd2d
dioo1461 <dioo1461@gmail.com>
2024-10-19 19:56:07 +0900

    refactor: 입력을 저장하는 자료구조를 MutableList -> String 으로 변경
    입력에서 주어지는 n 을 단순 줄바꿈으로 생각하고 있었으나, readLine()이 문자열에 포함된 n을 줄바꿈으로 인식하지 못하는 것을 확인하고, 입력 방식을 변경

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,51 +1,65 @@
 package calculator
+
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    val input: MutableList<String> = getInput("덧셈할 문자열을 입력해 주세요.")
+    val input: String = getInput("덧셈할 문자열을 입력해 주세요.")
     val operands: MutableList<Long> = parseInput(input)
-
+    // 피연산자 출력 확인
+    println(operands)
 }
+
 /** 문자열을 입력받고, 이를 MutableList로 반환
  *
  * @param requestMessage 문자열 입력을 요청하기 위해 사용자에게 표시되는 메시지
  * */
-fun getInput(requestMessage: String): MutableList<String> {
+fun getInput(requestMessage: String): String {
     println(requestMessage)
-
-    var lines = mutableListOf<String>()
-    lines.add(readLine())
-    lines.add(readLine())
-    return lines
+    return readLine()
 }
 
-fun parseInput(input: MutableList<String>): MutableList<Long> {
+fun parseInput(input: String): MutableList<Long> {
     val delimiterList = mutableListOf<Char>(',', ':')
+    var expression = input
 
-    if (input[0].startsWith("//")) {
+    if (input.startsWith("//")) {
         var idx = 2
-        while (input[0][idx] != '\n') {
-            delimiterList.addLast(input[0][idx])
+        while (true) {
+            while (input[idx] != '\\') {
+                delimiterList.addLast(input[idx])
+                idx++
+            }
+            // input[idx] = '\\' 인 상태
+
+            if (input[idx + 1] == 'n') {
+                expression = input.substring(idx + 2)
+                break
+            }
+
+            delimiterList.addLast(input[idx])
+            idx++
         }
-        return parseExpression(input[1], delimiterList)
     }
-    return parseExpression(input[0], delimiterList)
+    return parseExpression(expression, delimiterList)
 }
+
 
 fun parseExpression(expression: String, delimiterList: MutableList<Char>): MutableList<Long> {
     val numberList = mutableListOf<Long>()
     var number = mutableListOf<Char>()
     for (character in expression) {
         var isDelimiter = false
-        delimiterList.forEach{ delimiter ->
+        delimiterList.forEach { delimiter ->
             if (character == delimiter) isDelimiter = true
         }
         if (isDelimiter) {
-            numberList.add(number.joinToString().toLong())
+            numberList.add(number.joinToString("").toLong())
             number = mutableListOf<Char>()
             continue
         }
         number.add(character)
     }
+
+    if (number.size > 0) numberList.add(number.joinToString("").toLong())
     return numberList
 }
