329c9572c29acefdd73bf92a997e6ee7b30f887e
jhseo12 <jhseo1778@gmail.com>
2024-10-20 01:13:32 +0900

    refactor(Extract): 정규 표현식 관련 코드 리팩토링

--- src/main/kotlin/calculator/Extract.kt
+++ src/main/kotlin/calculator/Extract.kt
@@ -1,21 +1,20 @@
 package calculator
 
 class Extract {
-    private val customPattern = Regex("""^(//(.)\\n)(.*)""")
+    private val customPattern = Regex("""^//(.)\\n(.*)""")
 
     fun extractDelimiter(inputString: String): List<String> {
         val delimiters = mutableListOf(",", ":")
-        val isCustom = customPattern.matches(inputString)
-        if (isCustom) {
-            delimiters.add(inputString[2].toString())
+        val pattern = customPattern.matchEntire(inputString)
+        if (pattern != null) {
+            delimiters.add(pattern.groupValues[1])
         }
         return delimiters
     }
 
     fun extractString(inputString: String): String {
-        val pattern = customPattern.find(inputString)
-        val inputFiltered = pattern?.groups?.get(3)?.value ?: inputString
-        return inputFiltered
+        val pattern = customPattern.matchEntire(inputString)
+        return if (pattern != null) pattern.groupValues[2] else inputString
     }
 
     fun extractNumbers(inputString: String): List<Int> {
