90c2841309b101c8f6c684dd814b32fc64d77d87
dioo1461 <dioo1461@gmail.com>
2024-10-19 17:31:23 +0900

    feat: 사용자로부터 문자열을 입력받는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,18 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    val input: MutableList<String> = getInput("덧셈할 문자열을 입력해 주세요.")
+}
+/** 문자열을 입력받고, 이를 MutableList로 반환
+ *
+ * @param requestMessage 문자열 입력을 요청하기 위해 사용자에게 표시되는 메시지
+ * */
+fun getInput(requestMessage: String): MutableList<String> {
+    println(requestMessage)
+
+    var lines = mutableListOf<String>()
+    lines.add(readlnOrNull() ?: "")
+    lines.add(readlnOrNull() ?: "")
+
+    return lines
 }
