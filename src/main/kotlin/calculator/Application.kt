f834b0b6142dd29e00c879d052d0470ad7338976
rkqls4764 <rkqls4764@naver.com>
2024-10-19 21:06:52 +0900

    feat: 공백 문자열 예외 처리 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,11 +4,17 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     var input: String //입력 문자열
-    var result: Int = 0 //결과값
+    var result: Int = 0//결과값
 
     //입력 문자열 받기
     println("덧셈할 문자열을 입력해 주세요.")
     input = Console.readLine()
+
+    //입력 문자열이 공백이면 결과값은 0
+    if (input.isBlank()) {
+        print("결과 : 0")
+        return
+    }
 
     //결과 출력
     print("결과 : ${result}")
