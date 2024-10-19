a588021bb6d46d444d5f09c69ce76190b030dbc5
TaejungKim <tae0803@naver.com>
2024-10-19 15:08:24 +0900

    Feat: 추가 userInputRightExpression 함수
    - 사용자가 입력한 문자열이 정확한 식의 형태를 이루고 있는지 판단하는 로직 추가

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -4,6 +4,7 @@ import calculator.PromptText.Companion.DELIMITER_TYPE_COLON
 import calculator.PromptText.Companion.DELIMITER_TYPE_COMMA
 import calculator.PromptText.Companion.ERROR_INPUT_CUSTOM_DELIMITER
 import calculator.PromptText.Companion.ERROR_INPUT_CUSTOM_DELIMITER_COUNT
+import calculator.PromptText.Companion.ERROR_INPUT_NOT_EXPRESSION
 import calculator.PromptText.Companion.ERROR_INPUT_NULL
 import calculator.PromptText.Companion.INPUT_DELIMITER_END
 import calculator.PromptText.Companion.INPUT_DELIMITER_FRONT
@@ -13,7 +14,9 @@ object Validator {
 
     fun validateUserInput(userInput: String?) {
         userInputNull(userInput)
-        userInputCustomDelimiter(userInput!!)
+        if (userInput!!.isEmpty()) return
+        userInputCustomDelimiter(userInput)
+        userInputRightExpression(userInput)
     }
 
     private fun userInputNull(userInput: String?) {
@@ -33,6 +36,18 @@ object Validator {
                 throw IllegalArgumentException(ERROR_INPUT_CUSTOM_DELIMITER_COUNT)
             }
             delimiterList.add(userInput.substring(frontOfCustomDelimiter+2, endOfCustomDelimiter))
+        }
+    }
+
+    private fun userInputRightExpression(userInput: String) {
+        val endOfCustomDelimiter = if (userInput.startsWith(INPUT_DELIMITER_FRONT)) {
+            userInput.indexOf(INPUT_DELIMITER_END) + 2
+        } else {
+            0
+        }
+        val expression = userInput.substring(endOfCustomDelimiter)
+        if (!expression.matches(Regex("^\\d+([${delimiterList.joinToString("")}]\\d+)*\$"))) {
+            throw IllegalArgumentException(ERROR_INPUT_NOT_EXPRESSION)
         }
     }
 }
