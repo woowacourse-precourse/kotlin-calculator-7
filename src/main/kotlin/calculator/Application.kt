4ef0caf2028cfe18d3f0da1f60962491a2b5d07e
PIYUJIN <yjyj020201@gmail.com>
2024-10-20 04:38:45 +0900

    feat($Application.kt): 사용자 문자열 입력 기능 구현
    camp.nextstep.edu.missionutils.Console의 readLine() 활용을 통한 사용자 문자열 입력 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,10 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    // 문자열 입력 받기 (구분자와 양수로 구성된 문자열)
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
 }
