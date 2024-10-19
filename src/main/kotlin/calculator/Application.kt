713369eebb187cdaadb79a87dbfab2c0f66720dd
minjun011026 <jun011026@gmail.com>
2024-10-19 18:32:40 +0900

    feat: 합 도출

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,7 +5,15 @@ fun sumOfNumber(input : String) : Int{
     if (input.isEmpty()) return 0
 
     val customDelimiterPattern = """//(.)\n(.*)""".toRegex()
+    val matchResult = customDelimiterPattern.matchEntire(input)
 
+    return if (matchResult != null) {
+        val delimiter = matchResult.groupValues[1]
+        val numbers = matchResult.groupValues[2]
+        numbers.split("[$delimiter,:]".toRegex()).sumOf { it.toInt() }
+    } else {
+        input.split("[,:]".toRegex()).sumOf { it.toInt() }
+    }
 }
 fun main() {
     val input = Console.readLine()
