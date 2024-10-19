15f5d632baa1d387437cb1fba3ab7b3debd06323
Jordan <leesoyoung0112@gmail.com>
2024-10-19 16:57:06 +0900

    refactor(Application.kt) : integrate duplicate code

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -13,41 +13,34 @@ fun main() {
             println("결과 : 0")
             return
         }
+
         1 -> {
             // 하나만 입력했을 때 숫자인지 확인 & 결과 출력
             println("결과 : ${calc.toIntOrNull()?:throw IllegalArgumentException("string must be...")}")
         }
-        else -> {
 
-            val delimiters = arrayOf(",", ":", )
+        else -> {
+            var custom : String? = null
+            var delimiters = mutableListOf(",", ":")
+
             // 커스텀 구분자인지 판단
             if (calc.substring(0..1) == "//") {
                 // 커스텀 구분자인 경우
-                val custom = calc.substring(2 until calc.indexOf("\\n"))
+                custom = calc.substring(2 until calc.indexOf("\\n"))
                 calc = calc.substring(calc.indexOf("\\n")+2 until calc.length)
-                val numbers = calc.split(",", ":", custom).map {
-                    try {
-                        it.toInt()
-                    } catch (e: NumberFormatException){
-                        throw IllegalArgumentException("string must be...")
-                    }
-
-                }
-                println("결과 : ${numbers.sum()}")
+                delimiters.add(custom)
 
             }
-            else {
-                // 커스텀 구분자를 활용하지 않은 경우
-                val numbers = calc.split(",", ":").map {
-                    try {
-                        it.toInt()
-                    } catch (e: NumberFormatException){
-                        throw IllegalArgumentException("string must be...")
-                    }
-
+            
+            val numbers = calc.split(*delimiters.toTypedArray()).map {
+                try {
+                    it.toInt()
+                } catch (e: NumberFormatException){
+                    throw IllegalArgumentException("string must be...")
                 }
-                println("결과 : ${numbers.sum()}")
+
             }
+            println("결과 : ${numbers.sum()}")
 
         }
     }
