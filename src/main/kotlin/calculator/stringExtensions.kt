22fa3c8ab6fea50884c3cdf6d5ed318ff541d9a7
Tnalxmsk <alstnwkd990@naver.com>
2024-10-19 23:31:20 +0900

    feat: String ExtensionFunction 추가
    1. 구분자를 통해 숫자 리스트로 반환하는 확장함수
    
    2. 커스텀 구분자가 존재하는지 확인하는 확장함수
    
    3. 기본 구분자와 커스텀 구분자를 통해 추출하는 확장함수

---
+++ src/main/kotlin/calculator/stringExtensions.kt
@@ -0,0 +1,23 @@
+package calculator
+
+import calculator.eums.DefaultOperator.*
+import calculator.eums.RegexPattern.*
+
+fun String.toIntListByOperator(): List<Int> {
+    return split(COLON.value, COMMA.value).map { it.toInt() }
+}
+
+fun String.isContainCustomOperator(): Boolean {
+    val regex = Regex(CUSTOM_OPERATOR.pattern)
+    val result = regex.containsMatchIn(this)
+    return result
+}
+
+
+fun String.splitWithCustomOperator(): List<String> {
+    val regex = Regex(SPLIT_OPERATOR.pattern);
+    val matchResult = regex.find(this)
+    val delimiter = matchResult!!.groupValues[1]
+    val numberPart = matchResult.groupValues[2]
+    return numberPart.split(COLON.value, COMMA.value, delimiter)
+}
