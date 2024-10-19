d7fb51f429719159aa2369efc566fb033a4c465a
rkqls4764 <rkqls4764@naver.com>
2024-10-19 20:58:54 +0900

    feat: 문자열 입력 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,11 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    var input: String //입력 문자열
+
+    //입력 문자열 받기
+    println("덧셈할 문자열을 입력해 주세요.")
+    input = Console.readLine()
 }
