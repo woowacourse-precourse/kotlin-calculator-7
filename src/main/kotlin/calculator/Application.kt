9a72bf678e810538b929ca512b04c6efad6cd42e
DaeYoung Jeong <eodud1025@naver.com>
2024-10-19 20:36:35 +0900

    feat: 커스텀구분 유무 확인 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,4 +2,16 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    val str = readLine()
+    if (hasCustomDelimiter(str)) {
+
+    } else {
+
+    }
 }
+
+
+fun readLine(): String = camp.nextstep.edu.missionutils.Console.readLine()
+fun hasCustomDelimiter(str: String) = Regex("//.*\\n").containsMatchIn(str.replace("\\n", "\n"))
