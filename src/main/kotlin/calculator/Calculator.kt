589a8788ef9d9aeb1f3fbc7be5f20c4d45cef65a
HamBeomJoon <hbj0802@naver.com>
2024-10-20 00:42:42 +0900

    feat(Calculator): 커스텀 구분자 여부 확인 함수
    1. 입력값이 //로 시작할때
        - \n이 포함되지 않으면 IllegalArgumentException 발생시킴
        - \n이 포함되어 있지만 커스텀 구분자가 숫자이면 IllegalArgumentException 발생시킴
        - \n이 포함되어 있으면 구분자 리스트에 커스텀 구분자 add한 뒤 true를 반환
    
    2. 입력값이 //로 시작하지 않을 때
        - false를 반환

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,6 +3,8 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 class Calculator {
+    var separators = mutableListOf(':', ',')
+
     fun start() {
         printInput()
 
@@ -12,8 +14,25 @@ class Calculator {
 
     private fun printInput() = println(INPUT_MESSAGE)
 
+    fun customSeparatorCheck(input: String): Boolean {
+        if (input.startsWith("//")) {
+            val newLineIndex = input.indexOf("\\n")
+
+            if (newLineIndex == -1) throw IllegalArgumentException(ERROR_MESSAGE)
+
+            val customSeparator = input.substring(2, newLineIndex)
+            if (customSeparator.length != 1) throw IllegalArgumentException(ERROR_MESSAGE)
+
+            if (customSeparator[0].isDigit()) throw IllegalArgumentException(ERROR_MESSAGE)
+
+            separators.add(customSeparator[0])
+            return true
+        }
+        return false
+    }
 
     companion object {
         const val INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요."
+        const val ERROR_MESSAGE = "잘못된 입력값입니다."
     }
 }
