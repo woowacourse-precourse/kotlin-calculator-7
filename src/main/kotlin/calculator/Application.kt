294de3661250d7ebb755b27ef176a5c0f1ad4381
Labomem-No-004 <kurikame73@gmail.com>
2024-10-20 02:37:29 +0900

    feat(parser): 빈 문자열 처리
    입력 문자열이 빈 문자열이거나 null일 경우 이를 처리하고 기본값을 반환하는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,4 +2,17 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
+
+    var parser = Parser()
+
+    println(parser.parse(null))
+}
+
+class Parser {
+    fun parse(input: String?): List<Int> {
+        if (input.isNullOrEmpty()) {
+            return listOf(0)
+        }
+        return listOf()
+    }
 }
