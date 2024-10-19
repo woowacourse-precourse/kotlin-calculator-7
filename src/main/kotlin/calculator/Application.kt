a9e75096e032c8a98f8ed12c197538b74332a021
Jokwanhee <jonotch1@naver.com>
2024-10-19 22:17:09 +0900

    feat(Application.kt) : 기본 구분자 계산기 유효성 검사 함수 생성
    기본 구분자 계산기에서 발생할 수 있는 예외 핸들링을 위한 함수를 구현합니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -24,7 +24,7 @@ class CalculatorImpl {
         calculator = if (isCustomSeparator(input)) {
             // TODO : 커스텀 구분자 계산기
         } else {
-            // TODO : 기본 구분자 계산기
+            BasicCalculator()
         }
     }
 
@@ -42,5 +42,30 @@ interface Calculator {
 }
 class BasicCalculator : Calculator {
     override fun calculate(input: String): Int {
+        validate(input)
     }
+
+    private fun validate(input: String) {
+        val inputs = input.split(",", ":")
+        return when {
+            input.isEmpty() -> Unit
+            inputs.any { it.first() == '+' || it.first() == '-' } -> throw IllegalArgumentException("+ 또는 - 으로 시작하는 숫자가 있는 경우")
+            inputs.any { it.first() == '0' } -> throw IllegalArgumentException("0 으로 시작한 숫자가 있는 경우")
+            inputs.map { it.toIntOrNull() }.any { it == null } -> throw IllegalArgumentException("숫자 형식(Int)이 올바르지 않은 경우(기본 구분자가 아닌 문자포함, 공백포함, 오버플로우)")
+            inputs.isOverflowOnIntSum() -> throw IllegalArgumentException("합계가 양수(Int)범위를 넘어갈 경우(오버플로우)")
+            else -> Unit
+        }
+    }
+}
+
+
+fun List<String>.isOverflowOnIntSum(): Boolean {
+    var sum = 0
+    this.forEach {
+        sum += it.toInt()
+        if (sum < 0) {
+            return true
+        }
+    }
+    return false
 }
