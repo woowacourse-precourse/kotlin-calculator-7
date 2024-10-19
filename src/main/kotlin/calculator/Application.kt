cd7bf4e107e8bd9a3d24ff838fbeafd5a4f164a9
limch37 <limch37@naver.com>
2024-10-19 23:09:14 +0900

    feat(add): 빈 문자열 입력 처리 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,24 @@
 package calculator
 
+import java.lang.IllegalArgumentException
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = readLine() ?: ""
+
+    try {
+        val result = add(input)
+        println("결과 : $result")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}")
+    }
 }
+
+fun add(input: String): Int {
+    // 기능1. 빈 문자열 입력 처리
+    if (input.isEmpty()) {
+        return 0
+    }
+    throw IllegalArgumentException("입력된 문자열이 유효하지 않습니다.")
+}
+
