db543a026e70bdae600437731e101ed6f5b58a33
Jokwanhee <jonotch1@naver.com>
2024-10-19 22:11:41 +0900

    feat(Application.kt) : 계산기 인터페이스 구현 후 프로퍼티 생성
    계산기 인터페이스를 구현한 뒤에 입력받은 문자열을 기준으로 커스텀 구분자 계산기와 기본 구분자 계산기에 대한 calculator 객체를 생성합니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,10 +8,26 @@ fun main() {
 }
 
 class CalculatorImpl {
+    private var calculator: Calculator? = null
+
     fun start() {
         println("덧셈할 문자열을 입력해 주세요.")
         process(readln())
     }
+
+    private fun process(input: String) {
+        setCalculator(input)
+        print("결과 : ${calculator?.calculate(input = input)}")
+    }
+
+    private fun setCalculator(input: String) {
+        calculator = if (isCustomSeparator(input)) {
+            // TODO : 커스텀 구분자 계산기
+        } else {
+            // TODO : 기본 구분자 계산기
+        }
+    }
+
     private fun isCustomSeparator(input: String): Boolean =
         customSeparatorPattern.matcher(input).find()
 
@@ -19,4 +35,8 @@ class CalculatorImpl {
         private const val CUSTOM_SEPARATOR_PATTERN = """^//.*\\n.*$"""
         private val customSeparatorPattern = Pattern.compile(CUSTOM_SEPARATOR_PATTERN)
     }
+}
+
+interface Calculator {
+    fun calculate(input: String): Int
 }
