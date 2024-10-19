a074e0cefec19c4c51e5214e5244d4483ebd899c
dioo1461 <dioo1461@gmail.com>
2024-10-19 19:21:49 +0900

    feat: 문자열을 파싱하여 구분자 및 피연산자를 식별하는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,6 +3,8 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     val input: MutableList<String> = getInput("덧셈할 문자열을 입력해 주세요.")
+    val operands: MutableList<Long> = parseInput(input)
+
 }
 /** 문자열을 입력받고, 이를 MutableList로 반환
  *
@@ -14,7 +16,36 @@ fun getInput(requestMessage: String): MutableList<String> {
     var lines = mutableListOf<String>()
     lines.add(readLine())
     lines.add(readLine())
-
     return lines
 }
 
+fun parseInput(input: MutableList<String>): MutableList<Long> {
+    val delimiterList = mutableListOf<Char>(',', ':')
+
+    if (input[0].startsWith("//")) {
+        var idx = 2
+        while (input[0][idx] != '\n') {
+            delimiterList.addLast(input[0][idx])
+        }
+        return parseExpression(input[1], delimiterList)
+    }
+    return parseExpression(input[0], delimiterList)
+}
+
+fun parseExpression(expression: String, delimiterList: MutableList<Char>): MutableList<Long> {
+    val numberList = mutableListOf<Long>()
+    var number = mutableListOf<Char>()
+    for (character in expression) {
+        var isDelimiter = false
+        delimiterList.forEach{ delimiter ->
+            if (character == delimiter) isDelimiter = true
+        }
+        if (isDelimiter) {
+            numberList.add(number.joinToString().toLong())
+            number = mutableListOf<Char>()
+            continue
+        }
+        number.add(character)
+    }
+    return numberList
+}
