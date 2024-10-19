0a5c0198ad8ceffa44ef9de441e1c9d3fc2af3d0
DaeYoung Jeong <eodud1025@naver.com>
2024-10-19 20:40:50 +0900

    feat: 분리된 숫자 합하는 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,10 +5,16 @@ fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
 
     val str = readLine()
-    if (hasCustomDelimiter(str)) {
-        val (newDelimiter, s) = splitByCustomDelimiter(str)
-    } else {
-
+    try {
+        val sum = if (hasCustomDelimiter(str)) {
+            val (newDelimiter, s) = splitByCustomDelimiter(str)
+            sumOfStringList(s.split(",", ":", newDelimiter))
+        } else {
+            sumOfStringList(str.split(",", ":"))
+        }
+        println(sum)
+    } catch (e: Exception) {
+        throw IllegalArgumentException("잘못된 입력 형식입니다.")
     }
 }
 
@@ -21,3 +27,5 @@ fun splitByCustomDelimiter(str: String): Pair<String, String> {
     val splitList = str.split("//", "\\n")
     return splitList[1] to splitList[2]
 }
+
+fun sumOfStringList(list: List<String>) = list.sumOf { it.toInt() }
