40045677ba524e68170eb0101103f20f1e015d4a
chanho0908 <uni070@naver.com>
2024-10-20 12:57:24 +0900

    feat(StringExt.kt) : 문자가 음수인지 판별하는 함수

--- src/main/kotlin/calculator/StringExt.kt
+++ src/main/kotlin/calculator/StringExt.kt
@@ -3,3 +3,7 @@ package calculator
 fun String.isNotNumeric(): Boolean {
     return this.toDoubleOrNull() == null
 }
+
+fun String.isNegativeNumber(): Boolean {
+    return this.toIntOrNull()?.let { it < 0 } ?: false
+}
