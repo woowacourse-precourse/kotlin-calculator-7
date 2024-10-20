306758eea5ff0ba523f6d8f34b42d576a27a5888
kangyuri1114 <yuri3334@gmail.com>
2024-10-20 16:10:51 +0900

    feat(Application): 기능 2. 문자열 입력받기
    - 사용자에게 문자열을 입력받는다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,10 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    print("덧셈할 문자열을 입력해 주세요.")
+    print("덧셈할 문자열을 입력해 주세요.\n")
+
+    val input: String = Console.readLine()
+    print(input)
 }
