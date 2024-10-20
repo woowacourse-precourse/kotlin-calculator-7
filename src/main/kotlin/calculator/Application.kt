caae9b1b1c14c402226f9b37ab653c0c63a23241
Labomem-No-004 <kurikame73@gmail.com>
2024-10-20 21:37:22 +0900

    feat(calculator): 계산 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,11 +4,20 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // TODO: 프로그램 구현
-    val parser = Parser()
-    println("계산할 문자열을 입력하시오... (ex: 1,2,3 또는 //;\n1;2;3): ")
-    val input = Console.readLine()
-    val result = parser.parse(input)
-    println(result)
+    val calculator = Calculator()
+    calculator.start()
+}
+
+class Calculator {
+    private val parser = Parser()
+
+    fun start() {
+        println("계산할 문자열을 입력하시오... (ex: 1,2,3 또는 //;\n1;2;3): ")
+        val input = Console.readLine()
+        val numbers = parser.parse(input)
+        val result = numbers.sum()
+        println("결과 값: $result")
+    }
 }
 
 class Parser {
