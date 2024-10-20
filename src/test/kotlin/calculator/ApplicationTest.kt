e0db1c75b328cbd8e29bfaf63076610e8530b396
dioo1461 <dioo1461@gmail.com>
2024-10-20 15:38:18 +0900

    test: 사용자 입력에 대한 테스트 추가
    - 중복된 구분자 사용에 대한 테스트 추가
    - 구분자로 backslash 사용에 대한 테스트 추가
    - 구분자로 숫자 사용에 대한 테스트 추가
    - Long 범위를 넘어가는 값에 대한 예외 테스트 추가
    - 커스텀 구분자 입력 시에 줄바꿈이 누락된 경우에 대한 예외 테스트 추가

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -22,6 +22,44 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `중복 구분자 테스트`() {
+        assertSimpleTest {
+            run("///*---\\n32,1:2*9/56-7")
+            assertThat(output()).contains("결과 : 107")
+        }
+    }
+
+    @Test
+    fun `구분자로 backslash 사용 테스트`() {
+        assertSimpleTest {
+            run("///\\*-\\n32,1:2*9/56-7\\30")
+            assertThat(output()).contains("결과 : 137")
+        }
+    }
+
+    @Test
+    fun `구분자로 숫자 사용 테스트`() {
+        assertSimpleTest {
+            run("///\\*46-\\n32,1:2*9/565-7\\30")
+            assertThat(output()).contains("결과 : 91")
+        }
+    }
+
+    @Test
+    fun `Long 범위를 넘어가는 수에 대한 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("9223372036854775808") }
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 입력 시에 줄바꿈이 누락된 경우에 대한 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("///\\*-32,1:2*9/56-7\\30") }
+        }
+    }
+
     override fun runMain() {
         main()
     }
