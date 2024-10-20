e4c42dcf7cd18d9cecc45c9d78b45e1df189a97b
Tnalxmsk <alstnwkd990@naver.com>
2024-10-20 15:54:40 +0900

    feat: 정규식 패턴 추가

--- src/main/kotlin/calculator/eums/RegexPattern.kt
+++ src/main/kotlin/calculator/eums/RegexPattern.kt
@@ -2,5 +2,6 @@ package calculator.eums
 
 enum class RegexPattern(val pattern: String) {
     CUSTOM_OPERATOR("""//(.*?)\\n"""),
-    SPLIT_OPERATOR("""//(.*?)\\n(.*)""")
+    SPLIT_OPERATOR("""//(.*?)\\n(.*)"""),
+    KOREAN_AND_ALPHABET("([a-zA-Z]|[가-힣]|[ㄱ-ㅎ]|[ㅏ-ㅣ])")
 }
