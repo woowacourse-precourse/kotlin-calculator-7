662e511dd249eae89f6789d9a34debbec21331d7
이용인 <yongin0517@gmail.com>
2024-10-21 08:52:11 +0900

    test: 애플리케이션 테스트 파일 복구

---
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -0,0 +1,28 @@
+package calculator
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ApplicationTest : NsTest() {
+    @Test
+    fun `커스텀 구분자 사용`() {
+        assertSimpleTest {
+            run("//;\\n1")
+            assertThat(output()).contains("결과 : 1")
+        }
+    }
+
+    @Test
+    fun `예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("-1,2,3") }
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+}
