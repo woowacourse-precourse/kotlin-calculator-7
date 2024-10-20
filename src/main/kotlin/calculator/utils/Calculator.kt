0aab4c3d95d455095a0333b7e21b9b45aec9f3e7
sorongosdev <dosora9@gmail.com>
2024-10-21 06:47:24 +0900

    feat(Calculator): 피연산 문자열 업데이트

--- src/main/kotlin/calculator/utils/Calculator.kt
+++ src/main/kotlin/calculator/utils/Calculator.kt
@@ -8,6 +8,7 @@ object Calculator {
     fun calculateString(input: String): String {
         InputManager.oldInput = input
         InputManager.type = checkType(InputManager.oldInput) // 타입 체크
+        InputManager.newInput = updateInput(InputManager.type) // 피연산 문자열
 
         return "결과 : ${InputManager.sum}"
     }
@@ -23,6 +24,14 @@ object Calculator {
             ValidationType.CUSTOM
         } else {
             ValidationType.DEFAULT
+        }
+    }
+
+    /**피연산 문자열을 업데이트하는 함수*/
+    private fun updateInput(type: ValidationType): String {
+        return when (type) {
+            ValidationType.DEFAULT -> InputManager.oldInput // 일반 타입이면 그대로 반환
+            ValidationType.CUSTOM -> InputManager.oldInput.substring(5, InputManager.oldInput.length) // 커스텀 타입이면 뒷부분 반환
         }
     }
 }
