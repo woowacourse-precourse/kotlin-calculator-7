c9db6c3a025500efb18cbafebe2d0a6a40bf8055
jerry8282 <ka06196@naver.com>
2024-10-20 17:37:58 +0900

    feat: 문자열 입력하기
    숫자의 범위를 늘리기 위해 Biginteger import

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,14 @@
 package calculator
 
+import java.math.BigInteger
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    while(true) {
+        print("덧셈할 문자열 입력: ")
+        val userInput = Console.readLine()
+
+        if(userInput=="end")break
+
+    }
 }
