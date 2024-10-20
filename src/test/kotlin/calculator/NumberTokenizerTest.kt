6f7d91c65a6a221215f869340ca419c004283fee
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-20 13:51:08 +0900

    test(NumberTokenizerTest): 예외 처리 테스트 추가
    - 구분된 숫자에 음수가 포함되었을 경우 예외 발생 테스트 추가
      - 요구사항에서 양수로 입력 받는 제한이 있기 때문에
    - 커스텀 구분자외에 숫자외에 문자가 포함될 경우에 예외 발생 테스트 추가
       - 구분자로 나눈 숫자들을 더하는 기능이기 때문에 문자를 제외

--- src/test/kotlin/calculator/NumberTokenizerTest.kt
+++ src/test/kotlin/calculator/NumberTokenizerTest.kt
@@ -3,6 +3,7 @@ package calculator
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class NumberTokenizerTest {
 
@@ -29,22 +30,43 @@ class NumberTokenizerTest {
 
     @Test
     fun `문자열을 콜론과 쉼표를 통해 나누기`() {
-        val result = numberTokenizer.tokenize("1,3:531236.512873921873298472394827928719242591283792187413984731984713:7,82:")
-        val expect = listOf("1", "3", "531236.512873921873298472394827928719242591283792187413984731984713", "7", "82").map { it.toBigDecimal() }
+        val result =
+            numberTokenizer.tokenize("1,3:531236.512873921873298472394827928719242591283792187413984731984713:7,82:")
+        val expect = listOf(
+            "1",
+            "3",
+            "531236.512873921873298472394827928719242591283792187413984731984713",
+            "7",
+            "82"
+        ).map { it.toBigDecimal() }
         assertEquals(expect, result)
     }
 
     @Test
-    fun `문자열에서 커스텀 구분자 추출 테스트`(){
+    fun `문자열에서 커스텀 구분자 추출 테스트`() {
         val result = numberTokenizer.extractCustomDelimiters("//;\\n1;2and3//and\\n//&&\\n")
-        val expect = listOf(";","and","&&")
+        val expect = listOf(";", "and", "&&")
         assertEquals(expect, result)
     }
 
     @Test
-    fun `문자열 커스텀 구분자 포함 나누기 테스트`(){
+    fun `문자열 커스텀 구분자 포함 나누기 테스트`() {
         val result = numberTokenizer.tokenize("//;\\n1;23a43//3a4\\n//&&\\n&&44")
-        val expect = listOf("1","2","3","44").map { it.toBigDecimal() }
+        val expect = listOf("1", "2", "3", "44").map { it.toBigDecimal() }
         assertEquals(expect, result)
+    }
+
+    @Test
+    fun `구분한 숫자가 음수일 경우 예외 처리 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            numberTokenizer.tokenize("1,3,-3,5")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 선언 외에 숫자가 아닌 문자가 있을 경우 예외 처리 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            numberTokenizer.tokenize("//;\\n1;23,a43")
+        }
     }
 }
