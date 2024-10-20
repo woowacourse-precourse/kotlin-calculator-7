fefdc41133f0f49ff2f78509494d45258bf1b96e
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-20 13:29:04 +0900

    refactor(GuideOutput): 안내 출력 메세지 상수로 추출
    - 변수 이름으로 어떤 목적을 가진 메세지를 출력하는지 표현하기 위해서

--- src/main/kotlin/calculator/GuideOutput.kt
+++ src/main/kotlin/calculator/GuideOutput.kt
@@ -1,9 +1,13 @@
 package calculator
 
+const val DELIMITERS_MESSAGE = "기초 구분자는 ,(반점)과 :(콜론) 입니다. "
+const val CUSTOM_DELIMITERS_MESSAGE = "커스텀 구분자 선언 방식 : //구분자\\n (예시 //;\\n3;6,8:7)"
+const val ADDITION_INPUT_MESSAGE = "덧셈할 문자열을 구분자로 나눠서 입력해 주세요.(예시 1,3:5,6)"
+
 class GuideOutput {
-    fun guideDelimiters() = println("기초 구분자는 ,(반점)과 :(콜론) 입니다. ")
+    fun guideDelimiters() = println(DELIMITERS_MESSAGE)
 
-    fun guideCustomDelimiters() = println("커스텀 구분자 선언 방식 : //구분자\\n (예시 //;\\n3;6,8:7)")
+    fun guideCustomDelimiters() = println(CUSTOM_DELIMITERS_MESSAGE)
 
-    fun guideAdditionInput() = println("덧셈할 문자열을 구분자로 나눠서 입력해 주세요.(예시 1,3:5,6)")
+    fun guideAdditionInput() = println(ADDITION_INPUT_MESSAGE)
 }
