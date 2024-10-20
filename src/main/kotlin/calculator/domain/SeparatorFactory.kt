32dbb9f5e2c942fe86945d2a851835690e7f904b
wjdrjs00 <kimjk3409@gmail.com>
2024-10-21 02:26:12 +0900

    feat(SeparatorFactory):  커스텀 구분자 추출 기능 추가
    - 입력값에서 커스텀 구분자 추출
    - 커스텀 구분자 인덱스 위치 확장성을 위한 상수화
    - 커스텀 구분자 검증을 위한 메소드 호출
    - 커스텀 구분자 separator 변수에 적용을 위한 메소드 호출

--- src/main/kotlin/calculator/domain/SeparatorFactory.kt
+++ src/main/kotlin/calculator/domain/SeparatorFactory.kt
@@ -12,8 +12,14 @@ class SeparatorFactory(
         }
     }
 
+    private fun validateAndApplyCustomSeparator() {
+        val customSeparator = inputValue[SEPARATOR_INDEX_NUMBER]
+        validateCustomSeparator(customSeparator)
+        applyCustomSeparator(customSeparator)
+    }
 
     companion object {
         private const val CUSTOM_SEPARATOR_PATTERN_VALUE = "^//(.)\\\\n.*"
+        private const val SEPARATOR_INDEX_NUMBER = 2
     }
 }
