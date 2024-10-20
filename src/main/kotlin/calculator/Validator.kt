b7f0c9b6bf93e2b43fbcd0d99f203f7aedad8fb9
이용인 <yongin0517@gmail.com>
2024-10-21 03:37:28 +0900

    fix: 문자열 구분자인풋 형식 조건문 로직 수정

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -1,15 +1,14 @@
 package calculator
 
 class Validator {
-    //역할과 책임에 따라 더 분리해보기
-    // 커스텀 구분자의 형식 검증
-    fun validateDelimiterFormat(delimiterEndIndex: Int) {
-        if (delimiterEndIndex == -1) throw IllegalArgumentException("잘못된 형식입니다.")
+    // 커스텀 구분자 정의만 존재하고 숫자가 없는 경우 예외발생
+    fun validateDelimiterFormat(delimiterEndIndex: Int, input: String) {
+        if (input.substring(delimiterEndIndex + 1).length == 0) throw IllegalArgumentException("잘못된 형식입니다.")
     }
 
-    // 커스텀 구분자가 빈 문자열일 경우 예외 발생
+    // 커스텀 구분자가 빈 문자열 또는 한 글자 이상일 경우 예외 발생
     fun validateCustomDelimiter(customDelimiter: String) {
-        if (customDelimiter.isEmpty()) throw IllegalArgumentException("잘못된 형식의 구분자입니다.")
+        if (customDelimiter.isEmpty() || customDelimiter.length > 1) throw IllegalArgumentException("잘못된 형식의 구분자입니다.")
     }
 
     // 숫자로 변환하는 함수, 숫자가 아니면 예외 발생
