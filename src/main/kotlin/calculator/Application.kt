40c79fabe02c2a59dd4491a7c4193a21e512cc5c
DoableTuple <doabletuple@gmail.com>
2024-10-20 11:25:27 +0900

    feat: 입력 프롬프트를 출력하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 
 fun main() {
+    println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
     val inputNumbers = splitInput(input)
     val sum = addNumbers(inputNumbers)
