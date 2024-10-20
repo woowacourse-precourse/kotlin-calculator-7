a914e4dd665e0c0c25ea6f08d4e77d5ec805779f
DDangDin <Kcorelec7123@gmail.com>
2024-10-20 18:07:53 +0900

    feat: 예외처리 추가
    - 기본적인 유효성 검사
    - 커스텀 구분자가 비어있을 때 예외 추가
    - 문자열 리스트에서 정수형 리스트로 변환할 때 음수거나 정수로 변환할 수 없을 때 예외 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,11 +8,11 @@ private val DELIMITERS_CONDITIONS = listOf("//", "\\n")
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
 
-    val input = readLine()
+    val input = readLine().validate()
     val customDelimiters = getCustomDelimiter(input)
     val finalDelimiters = createFinalDelimiters(customDelimiters)
     val inputStringList = splitByDelimiters(input, finalDelimiters)
-    val inputIntList = inputStringList.map { it.toInt() }
+    val inputIntList = inputStringList.toIntList()
     val sumOfIntList = inputIntList.sum()
 
     println("결과 : $sumOfIntList")
@@ -27,10 +27,7 @@ private fun splitByDelimiters(
     return removedPrefixInput.split(*delimiters.toTypedArray())
 }
 
-/**
- * 커스텀 구분자를 판별하고
- * 만약 있다면 반환, 없다면 null을 반환한다.
- */
+// 커스텀 구분자를 판별하고 만약 있다면 반환, 없다면 null을 반환한다.
 private fun getCustomDelimiter(input: String): String? {
     var cnt = 0
     DELIMITERS_CONDITIONS.forEach { dc ->
@@ -40,6 +37,7 @@ private fun getCustomDelimiter(input: String): String? {
         return null
     }
     val customDelimiter = input.split(*DELIMITERS_CONDITIONS.toTypedArray())
+    if (customDelimiter[1].isEmpty()) throw IllegalArgumentException()
     return customDelimiter[1]
 }
 
@@ -50,3 +48,18 @@ fun createFinalDelimiters(customDelimiter: String?): List<String> {
     mutableDefaultDelimiters.add(customDelimiter)
     return mutableDefaultDelimiters
 }
+
+// 입력 값에 대한 기본적인 유효성 검사
+fun String.validate(): String =
+    when {
+        this.isEmpty() -> throw IllegalArgumentException()
+        else -> this
+    }
+
+// 문자열 리스트를 정수형 리스트로 변환해준다.
+fun List<String>.toIntList(): List<Int> =
+    this.map {
+        val intValue = it.toIntOrNull()
+        if (intValue == null || intValue < 0) throw IllegalArgumentException()
+        intValue
+    }
