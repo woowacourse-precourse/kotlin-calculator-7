72d5137f8a4529ebefd9692b02f9c329c6ca4e61
emjayMJkim <minjeong030420@gmail.com>
2024-10-21 03:08:41 +0900

    feat(Separator.kt): 커스텀 구분자 기준 문자 분리

--- src/main/kotlin/calculator/Separator.kt
+++ src/main/kotlin/calculator/Separator.kt
@@ -17,14 +17,17 @@ class Separator {
     }
 
     fun splitCustomSeparator(inputString: String): List<String> {
-        // TODO 커스텀 구분자 분리 구현
+        val delimiter = inputString[DELIMITER_INDEX]
+        val splitResult = inputString.substring(CUSTOM_NUMBER_START_INDEX).split(delimiter)
 
-        return emptyList()
+        return splitResult
     }
 
     companion object {
         private const val DELIMITER_COMMA = ","
         private const val DELIMITER_COLON = ":"
+        private const val DELIMITER_INDEX = 2
+        private const val CUSTOM_NUMBER_START_INDEX = 5
 
         private const val CUSTOM_DELIMITER_REGEX = """^//.\\n.*$"""
         val CUSTOM_PATTERN: Regex = Regex(CUSTOM_DELIMITER_REGEX)
