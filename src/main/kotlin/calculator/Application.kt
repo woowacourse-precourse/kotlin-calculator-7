e17b7cf6ca2ee515b3735241a9f092af6a3b7b8f
Seongdeok Hong <sdhong0609@gmail.com>
2024-10-20 18:34:51 +0900

    feat: 커스텀 구분자를 사용한 문자열을 덧셈하여 출력
    1. Empty 문자열은 0으로 변환
    2. 잘못된 커스텀 구분자 형식인 경우 IllegalArgumentException이 발생하고 애플리케이션 종료
    - 커스텀 구분자를 숫자로 입력한 경우
    - 커스텀 구분자가 하나가 아닌 경우

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,15 +5,38 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input: String = Console.readLine()
+    val hasCustomDelimiter: Boolean = input.length >= 5
+            && input.slice(0..1) == "//"
+            && input.slice(3..4) == "\\n"
 
-    val numbers: List<Int> = runCatching {
-        val split: List<String> = input.split(",", ":").map { it.ifEmpty { "0" } }
-        split.map { it.toInt() }
-    }.getOrElse { throw IllegalArgumentException() }
+    val sum: Int = if (hasCustomDelimiter) {
+        val customDelimiter = input[2].toString()
+        if (customDelimiter.toIntOrNull() != null) {
+            throw IllegalArgumentException()
+        }
+        val numbers: List<Int> = runCatching {
+            val newInput: String = input.substring(5)
+            val split: List<String> = newInput.split(",", ":", customDelimiter).map { it.ifEmpty { "0" } }
+            split.map { it.toInt() }
+        }.getOrElse { throw IllegalArgumentException() }
 
-    if (numbers.any { it < 0 }) {
-        throw IllegalArgumentException()
+        if (numbers.any { it < 0 }) {
+            throw IllegalArgumentException()
+        }
+
+        numbers.sum()
+    } else {
+        val numbers: List<Int> = runCatching {
+            val split: List<String> = input.split(",", ":").map { it.ifEmpty { "0" } }
+            split.map { it.toInt() }
+        }.getOrElse { throw IllegalArgumentException() }
+
+        if (numbers.any { it < 0 }) {
+            throw IllegalArgumentException()
+        }
+
+        numbers.sum()
     }
 
-    println("결과 : ${numbers.sum()}")
+    println("결과 : $sum")
 }
