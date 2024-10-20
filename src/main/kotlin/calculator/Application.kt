a3031f3420b4db6f0aa8111c88ff9d1cd7d050e7
Labomem-No-004 <kurikame73@gmail.com>
2024-10-20 19:40:27 +0900

    feat(parser): 커스텀 구분자 파싱
    커스텀 구분자 -> 구분자 목록에 추가
    구분자 입력과 파싱 구분

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // TODO: 프로그램 구현
 
-
     val parser = Parser()
 
     println("계산할 문자열을 입력하시오... (ex: 1,2,3 또는 //;\n1;2;3): ")
@@ -15,10 +14,56 @@ fun main() {
 }
 
 class Parser {
+    private val delimiters = mutableListOf(",", ":", "\n")
+
     fun parse(input: String?): List<Int> {
-        if (input.isNullOrEmpty()) {
-            return listOf(0)
+        val nonNullInput = input ?: return emptyListWithMessage()
+
+        if (hasCustomDelimiter(nonNullInput)) {
+            addCustomDelimiter(nonNullInput)
+            return parseNumbersWithCustomDelimiter()
         }
-        return input.split(",", ":").map { it.toInt() }
+
+        return parseNumbers(nonNullInput)
+    }
+
+    private fun emptyListWithMessage(): List<Int> {
+        println("결과 값: []")
+        return emptyList()
+    }
+
+    private fun hasCustomDelimiter(input: String): Boolean {
+        return input.startsWith("//")
+    }
+
+    private fun addCustomDelimiter(input: String) {
+        if (input.length > 2) {
+            val customDelimiter = input.substring(2).trim().take(1)
+            delimiters.add(customDelimiter)
+            println("구분자가 추가되었습니다: $customDelimiter")
+        } else {
+            println("올바른 구분자를 입력해 주세요.")
+        }
+    }
+
+    private fun parseNumbersWithCustomDelimiter(): List<Int> {
+        println("숫자를 입력하시오 (구분자: ${delimiters.joinToString()}): ")
+        val numbersInput = generateSequence { Console.readLine() }
+            .takeWhile { it.isNotBlank() }
+            .joinToString("\n")
+        return if (numbersInput.isEmpty()) {
+            emptyListWithMessage()
+        } else {
+            parseNumbers(numbersInput)
+        }
+    }
+
+    private fun parseNumbers(input: String): List<Int> {
+        return input.split(*delimiters.toTypedArray())
+            .filter { it.isNotEmpty() }
+            .map { it.toInt() }
     }
 }
+
+
+
