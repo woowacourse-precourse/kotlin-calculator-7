4088409fb6588a3632386964acac6d59065acf72
kangyein9892 <inwon9892@naver.com>
2024-10-21 03:44:38 +0900

    feat(Application.kt): 구분자를 가지지 않은 문자열 처리 함수 구현
    빈 문자열이나 한자리 숫자가 입력으로 들어올 경우 정상적으로 출력할 수 있도록 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,4 +2,17 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
+private fun isEmptyOrSingleNumber(input: String): Boolean {
+    if (input.isEmpty()) {
+        println(MESSAGE_RESULT_NUMBER.replace("%s", "0"))
+        return true
+    }
+
+    val number = input.toLongOrNull()
+    if (number != null) {
+        println(MESSAGE_RESULT_NUMBER.replace("%s", number.toString()))
+        return true
+    }
+    return false
 }
+private const val MESSAGE_RESULT_NUMBER = "결과 : %s"
