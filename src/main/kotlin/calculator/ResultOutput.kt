3b24cf0aa426950cb45e361fdc0cf8f0a3fe507d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-20 13:24:24 +0900

    refactor(ResultOutput): 결과 출력 메세지 상수로 추출
    - 변수 이름으로 어떤 목적을 가진 메세지를 출력하는지 표현하기 위해서

--- src/main/kotlin/calculator/ResultOutput.kt
+++ src/main/kotlin/calculator/ResultOutput.kt
@@ -2,9 +2,12 @@ package calculator
 
 import java.math.BigDecimal
 
+const val ADDITION_RESULT_MESSAGE = "입력하신 수의 총합 결과 : %s 입니다"
+const val CUSTOM_DELIMITERS_RESULT_MESSAGE = "입력하신 커스텀 구분자는 %s 입니다"
+
 class ResultOutput {
 
-    fun outputAddition(sum: BigDecimal) = println("입력하신 수의 총합 결과 : $sum 입니다")
+    fun outputAddition(sum: BigDecimal) = println(ADDITION_RESULT_MESSAGE.format(sum))
 
-    fun outputCustomDelimiters(delimiters: List<String>) = println("입력하신 커스텀 구분자는 $delimiters 입니다")
+    fun outputCustomDelimiters(delimiters: List<String>) = println(CUSTOM_DELIMITERS_RESULT_MESSAGE.format(delimiters))
 }
