9d0bc57269018a9fd2ef36825483b88948c174f4
wjdrjs00 <kimjk3409@gmail.com>
2024-10-21 02:29:48 +0900

    feat(SeparatorFactory): 커스텀 구분자 검증 기능 추가
    - 커스텀 구분자를 받아 문자, 숫자인지 검증
    - 검증 실패 시 IllegalArgumentException 발생
    - 화장성을 위해 에러메시지 상수화

--- src/main/kotlin/calculator/domain/SeparatorFactory.kt
+++ src/main/kotlin/calculator/domain/SeparatorFactory.kt
@@ -18,8 +18,14 @@ class SeparatorFactory(
         applyCustomSeparator(customSeparator)
     }
 
+    private fun validateCustomSeparator(customSeparator: Char) {
+        if (customSeparator.isLetterOrDigit()) {
+            throw IllegalArgumentException(ERROR_INVALID_CUSTOM_SEPARATOR)
+        }
+    }
     companion object {
         private const val CUSTOM_SEPARATOR_PATTERN_VALUE = "^//(.)\\\\n.*"
         private const val SEPARATOR_INDEX_NUMBER = 2
+        private const val ERROR_INVALID_CUSTOM_SEPARATOR = "[ERROR] 커스텀 구분자는 특수문자여야 합니다."
     }
 }
