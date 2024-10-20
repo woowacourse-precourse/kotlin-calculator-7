8b935d8360970b06e2844bec13ce01a39d96d995
kangyein9892 <inwon9892@naver.com>
2024-10-21 04:07:50 +0900

    feat(Application.kt): 구분자 Set 반환 함수 구현 및 delimiterSet 추가
    기본 구분자 상수화, 기본 구분자와 커스텀 구분자를 사용했을 경우를 구분하여 사용할 구분자의 Set을 반환하는 함수를 사용해 구분자 Set인 delimiterSet 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -9,6 +9,8 @@ fun main() {
 
     if (isEmptyOrSingleNumber(input)) return
     val isCustomDelimiter = input.substring(0, 2) == CUSTOM_DELIMITER_PREFIX
+
+    val delimiterSet = getDelimiterSet(isCustomDelimiter, input)
 private fun isEmptyOrSingleNumber(input: String): Boolean {
     if (input.isEmpty()) {
         println(MESSAGE_RESULT_NUMBER.replace("%s", "0"))
@@ -22,6 +24,18 @@ private fun isEmptyOrSingleNumber(input: String): Boolean {
     }
     return false
 }
+private fun getDelimiterSet(isCustomDelimiter: Boolean, input: String): Set<String> {
+    return if (isCustomDelimiter) {
+        val customDelimiter = input
+            .substringAfter(CUSTOM_DELIMITER_PREFIX)
+            .substringBefore(CUSTOM_DELIMITER_SUFFIX)
+        setOf(customDelimiter, BASIC_DELIMITER_COMMA, BASIC_DELIMITER_COLON)
+    } else {
+        setOf(BASIC_DELIMITER_COMMA, BASIC_DELIMITER_COLON)
+    }
+}
 private const val CUSTOM_DELIMITER_PREFIX = "//"
 private const val CUSTOM_DELIMITER_SUFFIX = "\\n"
+private const val BASIC_DELIMITER_COMMA = ","
+private const val BASIC_DELIMITER_COLON = ":"
 private const val MESSAGE_RESULT_NUMBER = "결과 : %s"
