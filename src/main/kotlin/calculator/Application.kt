2a0b720997c1a5e5d70594f710ab5a65bf0e837e
yeobeomhwi <yeobh0828@naver.com>
2024-10-21 01:11:39 +0900

    feat(Application.kt): 커스텀 구분자 구현 및 숫자 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,10 +2,27 @@ package calculator
 
 fun main() {
     val delimiters = mutableListOf(",", ":") // 기본 구분자
-
+    var numbers: List<String> = emptyList()
     // 입력 받기
     val input = readlnOrNull() ?: ""
 
+    // 입력의 시작이 `//`로 시작한다면
+    if (input.startsWith("//")) {
+        // "\n" 기준으로 나누기
+        val parts = input.split("\\n")
+        // 사용자 지정 구분자 추출
+        val customDelimiter = parts[0].substring(2) // "//" 제거
+        // 구분자 업데이트
+        delimiters += customDelimiter
+        // 구분자 출력
+        print("delimiters: $delimiters")
+        // parts[1]을 기준으로 숫자를 분리
+        numbers = parts[1].split(*delimiters.toTypedArray())
+
+    } else {
+        numbers = input.split(*delimiters.toTypedArray())
+    }
+
     // 출력
-    println(input)
+    println(numbers)
 }
