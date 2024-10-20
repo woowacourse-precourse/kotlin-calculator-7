038bbc4f4d665684629cef9562494f8742f5ffdf
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-20 21:07:02 +0900

    feat : 추출된 숫자 합성 결과 출력 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,14 +1,23 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console // 입력할 때 활용
+
 const val PROMPT_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요."
+const val PROMPT_OUTPUT_MESSAGE = "결과 : "
+
 fun main() {
-    // TODO: 프로그램 구현
     println(PROMPT_INPUT_MESSAGE)
 
     val input = Console.readLine()
+
     if (input.isEmpty()) {
         printOutput(0)
         return
     }
 
+    val splitNumberList = SeparatorHandler(input).getSplitNumberList()
+    val result = Calculator(splitNumberList).sumOfSplitNumbers()
+    printOutput(result)
 }
+
+fun printOutput(output: Int) = PROMPT_OUTPUT_MESSAGE + output
