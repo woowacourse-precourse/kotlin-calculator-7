f76bbeaee546034c95846647cef96fc57ae03618
eunmyeungjun <audwns419@gmail.com>
2024-10-20 19:39:35 +0900

    feat: 잘못된 입력이 들어올 시 예외처리하고 종료시키기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,24 +3,39 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
+    try {
+        println("덧셈할 문자열을 입력해 주세요.")
 
-    val input = Console.readLine()
+        val input = Console.readLine()
 
-    val numberList = if (input != null && input.startsWith("//")) {
-        val parts = input.replace("\\n", "\n").split("\n", limit = 2)
-        val custom = parts[0].substring(2)
+        if (input =="") {
+            println("결과 : 0")
+            return
+        }
 
-        parts[1].split(custom)
-    } else {
-        input?.split(",", ":")
+        val numberList = if (input != null && input.startsWith("//")) {
+            val parts = input.replace("\\n", "\n").split("\n", limit = 2)
+
+            if (parts.size < 2 || parts[1].isEmpty()) {
+                throw IllegalArgumentException("잘못된 입력입니다.")
+            }
+
+            val custom = parts[0].substring(2)
+            parts[1].split(Regex(custom))
+        } else {
+            input?.split(Regex("[,:]"))
+        }
+
+        val answer : Long = numberList
+            ?.filter { it.isNotEmpty() }
+            ?.map { it.toLongOrNull() ?: throw IllegalArgumentException("잘못된 입력입니다.") }
+            ?.sum() ?: throw IllegalArgumentException("잘못된 입력입니다.")
+
+        println("결과 : ${answer}")
+
+    } catch (e: IllegalArgumentException) {
+        println("오류가 발생했습니다. ${e.message}")
+    } finally {
+        Console.close()
     }
-
-    val answer = numberList
-        ?.filter { it.isNotEmpty() }
-        ?.map { it.toInt() }
-        ?.sum() ?: 0
-
-    println("결과 : ${answer}")
-    Console.close()
 }
