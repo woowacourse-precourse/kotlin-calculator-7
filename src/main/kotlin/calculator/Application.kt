15e27b466628e3c8b3d3089f4f6fe7ccade74a20
eden <devfeijoa@gmail.com>
2024-10-20 20:37:02 +0900

    docs: 기능 목록 작성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,12 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+    var userInput: String
+
+    println("덧셈할 문자열을 입력해 주세요.")
+    userInput = Console.readLine()
+
 }
