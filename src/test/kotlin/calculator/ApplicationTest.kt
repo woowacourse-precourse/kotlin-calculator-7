ad99e0cb99d8a28294cc978df9661384a73a52a5
noveril <pitterpatterpik@gmail.com>
2024-10-21 00:21:49 +0900

    test: Add unit tests for input validation and error handling

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -22,6 +22,50 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `음수 입력 시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1,-2,3") }
+        }
+    }
+
+    @Test
+    fun `잘못된 형식의 입력 - 숫자가 아닌 값 포함`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1,a,3") }
+            assertThrows<IllegalArgumentException> { runException("2,ㄱ,3") }
+        }
+    }
+
+    @Test
+    fun `잘못된 형식의 입력 - 실수 포함`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1,2.2,3") }
+
+        }
+    }
+
+    @Test
+    fun `잘못된 구분자 사용 시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1,2;3") }
+        }
+    }
+
+    @Test
+    fun `잘못된 구분자 위치 - 숫자 뒤에 구분자만 존재하는 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1,2,") }
+        }
+    }
+
+    @Test
+    fun `잘못된 구분자 위치 - 여러 개의 구분자가 연속으로 오는 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1::3") }
+        }
+    }
+
     override fun runMain() {
         main()
     }
