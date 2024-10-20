30799ce3da0052cc25c5cd9ccc5cd12aa0b33d8a
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-20 21:23:35 +0900

    fix : 커스텀 구분자 문자열 뒷부분 오류 수정

--- src/main/kotlin/calculator/SeparatorHandler.kt
+++ src/main/kotlin/calculator/SeparatorHandler.kt
@@ -3,17 +3,17 @@ package calculator
 class SeparatorHandler(private val input: String) {
 
     private fun isExitCustomSeparator(): Boolean {
-        return input.startsWith("//") && input.contains("\n")
+        return input.startsWith("//") && input.contains("\\n")
     }
 
     private fun getCustomSeparator(): String {
-        val customSeparatorEndIndex = input.indexOf("\n")
+        val customSeparatorEndIndex = input.indexOf("\\n")
         return input.substring(2, customSeparatorEndIndex)
     }
 
     private fun splitWithCustomSeparator(): List<String> {
         val customSeparator = getCustomSeparator()
-        val numberStartIndex = input.indexOf("\n") + 2
+        val numberStartIndex = input.indexOf("\\n") + 2
         val numbers = input.substring(numberStartIndex)
         return numbers.split(customSeparator)
     }
