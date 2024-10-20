8b2b17eec7283630301b83ead5afc0c9c68c977e
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-20 21:04:22 +0900

    feat : 커스텀 구분자 추출 구현

--- src/main/kotlin/calculator/SeparatorHandler.kt
+++ src/main/kotlin/calculator/SeparatorHandler.kt
@@ -5,4 +5,16 @@ class SeparatorHandler(private val input: String) {
     private fun isExitCustomSeparator(): Boolean {
         return input.startsWith("//") && input.contains("\n")
     }
+
+    private fun getCustomSeparator(): String {
+        val customSeparatorEndIndex = input.indexOf("\n")
+        return input.substring(2, customSeparatorEndIndex)
+    }
+
+    private fun splitWithCustomSeparator(): List<String> {
+        val customSeparator = getCustomSeparator()
+        val numberStartIndex = input.indexOf("\n") + 2
+        val numbers = input.substring(numberStartIndex)
+        return numbers.split(customSeparator)
+    }
 }
