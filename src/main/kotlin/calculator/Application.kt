34788a8cda8fb6f3aec88e8c7b386aa13d250111
jiwon <pjw99032986@gmail.com>
2024-10-20 17:53:01 +0900

    feat(Application.kt): 쉼표, 클론 구분자 문자열 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,63 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Console.close
+
 fun main() {
-    // TODO: 프로그램 구현
+    print("덧셈할 문자열을 입력해 주세요.\n")
+    val word = Console.readLine()
+    val separator = Separator(word)
+    try {
+        separator.checkIllegalArgumentException()
+    }catch (e : IllegalArgumentException){
+        close()
+    }
+}
+
+
+class Separator(s: String) {
+    private var word: String
+    private lateinit var divider : String
+    private var sum: Int = 0
+    private var addWord: String = ""
+    init {
+        this.word = s
+    }
+
+
+
+    private fun checkSeparator() {
+        for (i in word) {
+            when ("$i") {
+                ",", ":" -> {
+                    sum += confirmNumber(addWord)
+                    addWord = ""
+                }
+                "\"" -> continue
+                else -> addWord += i
+            }
+        }
+        if (addWord.isNotEmpty()) {
+            sum += confirmNumber(addWord)
+        }
+        println("결과 : $sum")
+    }
+
+    private fun confirmNumber(addWord: String): Int {
+        try {
+            sum = addWord.toInt()
+        } catch (e: NumberFormatException) {
+            divider = addWord
+        }
+        return sum
+    }
+
+    fun checkIllegalArgumentException(){
+        if ((word.contains(":") || word.contains(","))) {
+            this.checkSeparator()
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+
 }
