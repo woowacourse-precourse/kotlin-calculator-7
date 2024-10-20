1e2cc2e1877ffcc4e52a5d60e7612b15f097c066
PIYUJIN <yjyj020201@gmail.com>
2024-10-21 03:08:56 +0900

    feat($Application.kt): 커스텀 구분자를 기준으로 분리한 각 숫자의 합 반환 기능 구현
    문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용하여 문자열 전달시 구분자를 기준으로 분리한 각 숫자의 합 반환 및 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생 시킨후 애플리케이션 종료 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -17,21 +17,43 @@ fun main() {
 
 fun splitInputData(input: String): MutableList<Int> {
 
-    var splitIntegerData = mutableListOf<Int>()
+    var resultList = mutableListOf<Int>()
 
     // 아무 값도 입력되지 않은 경우 0 반환
-    if(input.isBlank()) return splitIntegerData
+    if(input.isBlank()) return resultList
 
-    // 기본 구분자(쉼표, 콜론)을 통한 문자열 구분
-    val splitInputData = input.split(":",",")
+    // 커스텀 구분자가 존재하지 않는 경우
+    if(input.startsWith("//")) {
+        val endIndex = input.indexOf("\\n")
 
-    for(i in splitInputData.indices) {
+        // 해당 구분자가 없는 경우
+        if(endIndex == -1) throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
+        val customCharacter = input.substring(2, endIndex)
+        val deletedCustomCharacter = input.substring(endIndex+2, input.length)
+        val splitInputData = deletedCustomCharacter.split(customCharacter)
+
+        resultList = checkCharacter(splitInputData)
+    } else {
+        // 기본 구분자(쉼표, 콜론)을 통한 문자열 구분
+        val splitInputData = input.split(":", ",")
+
+        resultList = checkCharacter(splitInputData)
+    }
+
+    return resultList
+}
+
+fun checkCharacter(splitInputData: List<String>): MutableList<Int> {
+    var splitIntegerData = mutableListOf<Int>()
+
+    for (i in splitInputData.indices) {
         // 구분자를 여러번 입력하거나 마지막에 입력한 경우
-        if(splitInputData[i].isNotBlank()) {
+        if (splitInputData[i].isNotBlank()) {
             // 잘못된 값을 입력한 경우
-            splitInputData[i].toIntOrNull() ?: throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
+            splitInputData[i].toIntOrNull()
+                ?: throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
             // 음수를 입력한 경우
-            if(splitInputData[i].toInt() < 0) throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
+            if (splitInputData[i].toInt() < 0) throw IllegalArgumentException("잘못된 값입니다. (구분자와 양수로 구성된 문자열 입력)")
             splitIntegerData.add(splitInputData[i].toInt())
         } else {
             continue
