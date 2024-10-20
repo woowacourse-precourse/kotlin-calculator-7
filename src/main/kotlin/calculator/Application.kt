73b3cafa82ed61e5153ab39e5d45c00d1f0aec5e
Min Seon <cbdhoho1@gmail.com>
2024-10-21 04:20:04 +0900

    문자열에서 쉼표와 쌍따옴표를 기준으로 숫자를 추출하는 기능
    SplitChar()함수를 만들었으며, map을 이용하여 split하여 얻은 리스트를 편의성을 위해 Int형으로 형변환하였다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,4 +2,11 @@ package calculator
 
 fun main() {
     var char = readLine()
+    var splitedChar = SplitChar(char)
+}
+
+fun SplitChar(char: String?): List<Int>? {
+    var splitedChar = char?.split(',','"')
+    var splitedCharToInt = splitedChar?.map { it.toInt() }
+    return splitedCharToInt
 }
