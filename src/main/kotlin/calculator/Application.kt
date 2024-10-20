7bef2f88a5322ba0d2543a3fbd7f70967e471d9f
Seongdeok Hong <sdhong0609@gmail.com>
2024-10-20 18:07:03 +0900

    feat: 기본 구분자를 사용한 문자열을 덧셈하여 출력
    1. Empty 문자열은 0으로 변환
    2. 양수가 아닌 다른 값(문자, 음수, 공백 등)을 입력한 경우 IllegalArgumentException이 발생하고 애플리케이션 종료

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,4 +5,15 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input: String = Console.readLine()
+
+    val numbers: List<Int> = runCatching {
+        val split: List<String> = input.split(",", ":").map { it.ifEmpty { "0" } }
+        split.map { it.toInt() }
+    }.getOrElse { throw IllegalArgumentException() }
+
+    if (numbers.any { it < 0 }) {
+        throw IllegalArgumentException()
+    }
+
+    println("결과 : ${numbers.sum()}")
 }
