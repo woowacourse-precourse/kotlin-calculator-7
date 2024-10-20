7e05a47ce2918f6e604422495e1462892923dba2
wjdrjs00 <kimjk3409@gmail.com>
2024-10-21 02:50:00 +0900

    feat(Operand): 커스텀구분자를 통한 피연산자 추출 기능 추가
    - 커스텀 구분자 패턴 정규식 변수 추가
    - 재사용성을 위한 정규식 상수화
    - 입력값에서 구분자 패턴 제거 기능 추가
    - 구분자 패턴 제거된 입력값에서 기본구분자, 커스텀구분자를 통해 문자 피연산자 추출
    - 추출된 문자 피연산자 값 숫자로 변환을 위한 메서드 호출

--- src/main/kotlin/calculator/domain/Operand.kt
+++ src/main/kotlin/calculator/domain/Operand.kt
@@ -5,9 +5,22 @@ class Operand(
     private val separator: Separator
 ) {
     private val operand = mutableListOf<Int>()
+    private val generateCustomSeparatorPattern = Regex(GENERATE_CUSTOM_SEPARATOR_PATTERN)
 
     private fun splitByDefaultSeparators() {
         val stringOperand = inputValue.split(separator.colon, separator.comma)
         parseAndAddOperands(stringOperand)
+    }
+
+    private fun splitByCustomSeparator() {
+        val newValue = removeCustomSeparatorPattern()
+        val stringOperand = newValue.split(separator.colon, separator.comma, separator.custom!!)
+        parseAndAddOperands(stringOperand)
+    }
+
+    private fun removeCustomSeparatorPattern() =
+        inputValue.replace(generateCustomSeparatorPattern, "")
+    companion object {
+        private const val GENERATE_CUSTOM_SEPARATOR_PATTERN = "^//(.)\\\\n"
     }
 }
