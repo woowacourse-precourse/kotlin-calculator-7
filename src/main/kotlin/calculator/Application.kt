7b25686c337b7eb23b2852a198264dba2dacd617
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-10-21 02:47:59 +0900

    feat: customDel 메서드에 예외 처리 기능 추가
    문자열이 구분자 정의와 숫자 부분으로 분리되지 않을 경우 예외 발생.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -23,6 +23,9 @@ class Calculate(private val input: String) {
     // 커스텀 구분자에 대해 숫자 분리하는 메서드
     private fun customDel(): List<String> {
         val part = input.split("\\n")
+        if (part.size != 2) {   // \n을 기준으로 입력을 두 부분(구분자 정의와 숫자 부분)으로 분리
+            throw IllegalArgumentException("잘못된 입력입니다. 구분자와 숫자를 포함해야 합니다.")
+        }
         val delimiter = part[0][2]    // 커스텀 구분자 추출
         return part[1].split(delimiter.toString())  // \n의 다음 부분을 구분자 기준으로 분리
     }
