091d8f3e90256e08649579507854e3acc3301181
DDangDin <Kcorelec7123@gmail.com>
2024-10-20 19:05:18 +0900

    refactor: 코드 리팩토링
    - 문자열 포장
    - 메서드 위치 변경
    - 모든 if 식에 중괄호 추가
    - 메서드가 한 가지 일만 하도록 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 private val DEFAULT_DELIMITERS = listOf(",", ":")
 private val DELIMITERS_CONDITIONS = listOf("//", "\\n")
+private const val INPUT_NOTICE_TEXT = "덧셈할 문자열을 입력해 주세요."
+private const val RESULT_TEXT = "결과 : "
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
+    println(INPUT_NOTICE_TEXT)
 
     val input = readLine().validate()
     val customDelimiters = getCustomDelimiter(input)
@@ -15,7 +17,27 @@ fun main() {
     val inputIntList = inputStringList.toIntList()
     val sumOfIntList = inputIntList.sum()
 
-    println("결과 : $sumOfIntList")
+    println(RESULT_TEXT + sumOfIntList)
+}
+
+// 커스텀 구분자를 판별하고 만약 있다면 반환, 없다면 null을 반환한다.
+private fun getCustomDelimiter(input: String): String? {
+    if (input.findCustomDelimiter()) {
+        return null
+    }
+    val customDelimiter = input.split(*DELIMITERS_CONDITIONS.toTypedArray())
+    if (customDelimiter[1].isEmpty()) {
+        throw IllegalArgumentException()
+    }
+    return customDelimiter[1]
+}
+
+private fun String.findCustomDelimiter(): Boolean {
+    var cnt = 0
+    DELIMITERS_CONDITIONS.forEach { dc ->
+        if (dc in this) cnt++
+    }
+    return cnt != DELIMITERS_CONDITIONS.size
 }
 
 // 구분자를 기준으로 문자열을 나눈다.
@@ -23,27 +45,18 @@ private fun splitByDelimiters(
     input: String,
     delimiters: List<String>,
 ): List<String> {
-    val removedPrefixInput = input.split(DELIMITERS_CONDITIONS[1]).last()
+    val removedPrefixInput =
+        input
+            .split(DELIMITERS_CONDITIONS[1])
+            .last()
     return removedPrefixInput.split(*delimiters.toTypedArray())
-}
-
-// 커스텀 구분자를 판별하고 만약 있다면 반환, 없다면 null을 반환한다.
-private fun getCustomDelimiter(input: String): String? {
-    var cnt = 0
-    DELIMITERS_CONDITIONS.forEach { dc ->
-        if (dc in input) cnt++
-    }
-    if (cnt != DELIMITERS_CONDITIONS.size) {
-        return null
-    }
-    val customDelimiter = input.split(*DELIMITERS_CONDITIONS.toTypedArray())
-    if (customDelimiter[1].isEmpty()) throw IllegalArgumentException()
-    return customDelimiter[1]
 }
 
 // 최종적으로 사용할 구분자 리스트를 만든다.
 fun createFinalDelimiters(customDelimiter: String?): List<String> {
-    if (customDelimiter == null) return DEFAULT_DELIMITERS
+    if (customDelimiter == null) {
+        return DEFAULT_DELIMITERS
+    }
     val mutableDefaultDelimiters = DEFAULT_DELIMITERS.toMutableList()
     mutableDefaultDelimiters.add(customDelimiter)
     return mutableDefaultDelimiters
@@ -60,6 +73,8 @@ fun String.validate(): String =
 fun List<String>.toIntList(): List<Int> =
     this.map {
         val intValue = it.toIntOrNull()
-        if (intValue == null || intValue < 0) throw IllegalArgumentException()
+        if (intValue == null || intValue < 0) {
+            throw IllegalArgumentException()
+        }
         intValue
     }
