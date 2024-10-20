20c3418ba311448f37e69d1d5f8bb6acfdcddfab
DongjooSEO <readyjoo24@gmail.com>
2024-10-21 02:47:07 +0900

    feat: 문자열 입력 및 커스텀 구분자 유무 확인
    Application.kt 문자열을 입력하고 커스텀 구분자의 유무를 확인한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,17 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
     // TODO: 프로그램 구현
+    // 문자열 입력받기
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+
+}
+// 커스텀 구분자 사용 여부 확인 함수
+fun isCustomDelimiter(input: String): Boolean {
+    return input.startsWith("//")
+}
 }
