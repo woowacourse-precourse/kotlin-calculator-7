f6d7af04cfb5d778a136932e32299c6947ad5afb
46112 <duf325@naver.com>
2024-10-20 16:28:24 +0900

    feat(calculator): add input validation and exception handling
    - Throws IllegalArgumentException if operands are not numbers
    - Throws IllegalArgumentException if operands are not positive numbers
    - Throws IllegalArgumentException if "//" exists but "\n" is missing
    - Application terminates using 'camp.nextstep.edu.missionutils.Console.close()'

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -11,19 +11,28 @@ fun main() {
         println("결과: 0")
         return
     }
+    try {
+        // 구분자와 구분자를 제외한 문자열 추출
+        val (delimiter, numbers) = getDelimiterAndNumbers(input)
+        println(delimiter.toString())
+        // 구분자를 이용한 문자열 스플릿
+        val tokens = numbers.split(delimiter).map { it.trim() }
+        println(tokens)
 
-    // 구분자와 구분자를 제외한 문자열 추출
-    val (delimiter, numbers) = getDelimiterAndNumbers(input)
-    println(delimiter.toString())
-    // 구분자를 이용한 문자열 스플릿
-    val tokens = numbers.split(delimiter).map { it.trim() }
-    println(tokens)
+        // 문자열을 숫자로 변환하고 더하기
+        val sum = tokens.map { token ->
+            val number = token.toIntOrNull() ?: throw IllegalArgumentException("피연산자가 숫자가 아닙니다: $token")
+            if (number <= 0) throw IllegalArgumentException("피연산자가 양의 정수가 아닙니다: $token")
+            number
+        }.sum()
 
-    // 문자열을 숫자로 변환하고 더하기
-    val sum = tokens.map { it.toIntOrNull() ?: 0 }.sum()
-
-    // 결과값 출력
-    println("결과 : $sum")
+        // 결과값 출력
+        println("결과 : $sum")
+    } catch (error: IllegalArgumentException) {
+        println("에러 로그: ${error.message}")
+    } finally {
+        Console.close()
+    }
 }
 
 fun getDelimiterAndNumbers(input: String): Pair<Regex, String> {
@@ -31,7 +40,7 @@ fun getDelimiterAndNumbers(input: String): Pair<Regex, String> {
         // 커스텀 구분자 처리
         val delimiterEndIndex = input.lastIndexOf("\\n")
         if (delimiterEndIndex == -1) {
-            throw IllegalArgumentException("잘못된 커스텀 구분자 형식입니다:")
+            throw IllegalArgumentException("잘못된 커스텀 구분자 형식입니다: $input 내에 '\\n'이 존재하지 않습니다.")
         }
 
         val customDelimiter = input.substring(2, delimiterEndIndex)
