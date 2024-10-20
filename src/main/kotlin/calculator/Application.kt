3fe7313eadca502a2dafd6c1a486920f45906102
chanho0908 <uni070@naver.com>
2024-10-20 12:51:21 +0900

    feat(Application.kt) : 구분자 상태 판별 함수

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -13,5 +13,13 @@ private fun checkInputIsEmpty(input: String) {
     if (input.isEmpty()) throw IllegalArgumentException(MESSAGE_INPUT_EMPTY)
 }
 
+private fun getSeparatorState(input: String): SeparatorState {
+    return if (input.startsWith("//") && input.contains("\\n")) {
+        val rangeForFindSeparator = 2..3
+        SeparatorState.CustomSeparator(input.substring(rangeForFindSeparator))
+    } else {
+        SeparatorState.DefaultSeparator
+    }
+}
 private const val MESSAGE_FOR_GUIDE_CALCULATE = "덧셈할 문자열을 입력해 주세요."
 private const val MESSAGE_INPUT_EMPTY = "빈 값을 입력하였습니다."
