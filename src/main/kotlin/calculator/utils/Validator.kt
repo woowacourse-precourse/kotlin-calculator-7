3fb1b9bb26197690eb1a4453f2b388cdeedf84cc
Tnalxmsk <alstnwkd990@naver.com>
2024-10-20 15:54:16 +0900

    feat: 입력값 검증 기능 구현
    공백 혹은 문자가 포함되어 있으면 에러를 던지는 기능

--- src/main/kotlin/calculator/utils/Validator.kt
+++ src/main/kotlin/calculator/utils/Validator.kt
@@ -1,10 +1,21 @@
 package calculator.utils
 
+import calculator.eums.DefaultOperator.*
+import calculator.eums.Error.*
+import calculator.eums.RegexPattern.*
+
 object Validator {
+
+    fun validateInput(input: String) {
+        require(!input.contains(" ")) { CONTAIN_GAP.message }
+        input.split(COLON.value, COMMA.value).forEach {
+            require(!KOREAN_AND_ALPHABET.pattern.toRegex().matches(it)) { ONLY_NUMBER.message }
+        }
+    }
 
     fun validateNumbers(numbers: List<Int>) {
         for (idx in numbers.indices) {
-            require(numbers[idx] > 0)
+            require(numbers[idx] > 0) { CONTAIN_NEGATIVE_NUMBER.message }
         }
     }
 }
