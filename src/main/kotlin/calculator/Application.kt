1c1ffaafbfa6f84eafff3cf5aaa96e5e1c5505e1
Sunghyeon <tjdgustjdan@gmail.com>
2024-10-21 00:34:51 +0900

    feat : 문자열 입력 기능 추가
    입력 시 camp.nextstep.edu.missionutils.Console의 readLine 라이브러리 사용

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,15 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    var result = 0
+    // todo : 문자열 입력하기
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+    println(input)
+    println("결과 : $result")
+
 }
+
+
