e5a2e453ce1897a4df17c85a9f18ddc598d3833a
jerry8282 <ka06196@naver.com>
2024-10-20 22:07:46 +0900

    feat: 구분자 추출 함수 추가
    커스텀 구분자 출력을 위해 첫 숫자의 인덱스 반환 함수 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,5 +10,33 @@ fun main() {
 
         if(userInput=="end")break
 
+        try {
+            val result = delimiterExtraction(userInput)
+            println("결과 : $result")
+        } catch (e: IllegalArgumentException) {
+            println("오류: ${e.message}")
+        }
     }
+}
+
+//구분자 추출하는 함수
+private fun delimiterExtraction(input: String): Pair<String, String> {
+    return if (input.startsWith("//")) { // //로 시작할 때 커스텀 구분자로 분류시작
+        val delimiterEndIndex = findFirstDigitIndex(input)
+        val delimiter = input.substring(2, delimiterEndIndex-2) // 커스텀 구분자만 따로 분류
+        val numbersString = input.substring(delimiterEndIndex) // 커스텀 구분자 선언부 제외 문자열 분류
+        Pair(delimiter, numbersString)
+    } else {
+        Pair("[,|:]", input) //기본 구문자 선언
+    }
+}
+
+//문자열에 있는 첫 숫자의 인덱스를 반환하는 함수
+fun findFirstDigitIndex(input: String): Int {
+    for (i in input.indices) {
+        if (input[i].isDigit()) {
+            return i // 숫자가 발견되면 그 인덱스를 반환
+        }
+    }
+    return throw IllegalArgumentException("숫자를 입력해주세요.")
 }
