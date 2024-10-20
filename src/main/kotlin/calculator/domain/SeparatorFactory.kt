9fae42bec00914d95b26ab93c8e96e11a9abeb43
wjdrjs00 <kimjk3409@gmail.com>
2024-10-21 02:21:07 +0900

    feat(SeparatorFactory): 커스텀 구분자 패턴 판별 기능 구현
    - 커스텀 구뷴자 패턴 정규식 변수 추가
    - 확장성을 위한 커스텀 구분자 패턴 상수화
    - 입력값에서 커스텀 구분자 패펀 있는지 판별
    - 있는경우 검증, 커스텀 구분자 추가를 위한 메소드 호출

--- src/main/kotlin/calculator/domain/SeparatorFactory.kt
+++ src/main/kotlin/calculator/domain/SeparatorFactory.kt
@@ -4,4 +4,16 @@ class SeparatorFactory(
     private val inputValue: String
 ) {
     private var separator: Separator = Separator()
+    private val customSeparatorPatternValue = Regex(CUSTOM_SEPARATOR_PATTERN_VALUE)
+
+    private fun processCustomSeparatorPattern() {
+        if (customSeparatorPatternValue.matches(inputValue)) {
+            validateAndApplyCustomSeparator()
+        }
+    }
+
+
+    companion object {
+        private const val CUSTOM_SEPARATOR_PATTERN_VALUE = "^//(.)\\\\n.*"
+    }
 }
