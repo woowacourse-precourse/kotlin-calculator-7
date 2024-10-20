41021ee186463e22248a823161ac1fe0540359d7
DongjooSEO <readyjoo24@gmail.com>
2024-10-21 02:50:05 +0900

    feat: 커스텀 구분자 유효성 검사 및 지정
    Application.kt extractCustomDelimiter 함수에서 커스텀 문자가 유효한 지 확인한다. 유효한 경우에 커스텀 구분자와 숫자 부분을 분리해 반환한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -14,4 +14,24 @@ fun main() {
 fun isCustomDelimiter(input: String): Boolean {
     return input.startsWith("//")
 }
+
+// 유효한 커스텀 구분자를 추출하고 지정하는 함수
+fun extractCustomDelimiter(input: String): Pair<String, String> {
+    // "\n"이 있는 지 확인
+    if (input[3] == '\\' && input[4] == 'n') {
+        // 구분자를 추출
+        val delimiter = input.substring(2, 3)
+        val numbersPart = input.substring(5)  // 숫자 부분 추출
+
+        // 숫자 제외 모든 문자 가능 여부 확인
+        val isValidDelimiter = delimiter.all { it !in '0'..'9' }
+
+        // 구분자가 유효한 경우
+        if (isValidDelimiter) {
+            return Pair(delimiter, numbersPart)
+        } else {
+            throw IllegalArgumentException("유효한 구분자가 아닙니다.")
+        }
+    }
+    throw IllegalArgumentException("유효한 커스텀 구분자가 없습니다.")
 }
