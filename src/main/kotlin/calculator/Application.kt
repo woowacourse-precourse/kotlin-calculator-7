7539d098307cccef3f542d4d49543d718a78ec30
noveril <pitterpatterpik@gmail.com>
2024-10-21 00:05:37 +0900

    feat: add to read input for calculation

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,10 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
+private const val INPUT_SUM_STRING_MESSAGE = "덧셈할 문자열을 입력해 주세요."
+
 fun main() {
-    // TODO: 프로그램 구현
+    println(INPUT_SUM_STRING_MESSAGE)
+    val input = Console.readLine().trim()
 }
