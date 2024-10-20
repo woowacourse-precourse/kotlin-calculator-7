c0bd9dd895f4aa864fd2116058b8903ea0fdb719
chanho0908 <uni070@naver.com>
2024-10-20 12:43:52 +0900

    feat(Application.kt) : 사용자 입력 Null Check
    - 사용자가 빈 값을 입력할 경우 IllegalArgumentException을 발생시킵니다.
    - 에러 메시시는 상수로 관리하였습니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,6 +5,13 @@ import camp.nextstep.edu.missionutils.Console.readLine
 fun main() {
     println(MESSAGE_FOR_GUIDE_CALCULATE)
     val userInput = readLine()
+
+    checkInputIsEmpty(userInput)
+}
+
+private fun checkInputIsEmpty(input: String) {
+    if (input.isEmpty()) throw IllegalArgumentException(MESSAGE_INPUT_EMPTY)
 }
 
 private const val MESSAGE_FOR_GUIDE_CALCULATE = "덧셈할 문자열을 입력해 주세요."
+private const val MESSAGE_INPUT_EMPTY = "빈 값을 입력하였습니다."
