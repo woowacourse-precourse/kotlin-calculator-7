dd29ba36688f90f1b0b436a0453b26d7328f0058
kangyuri1114 <yuri3334@gmail.com>
2024-10-20 22:55:36 +0900

    refactor(Application): 변수명 변경
    - 변수명 변경 및 가독성을 위한 리팩토링을 진행한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,9 @@
 package calculator
 
+import calculator.ErrorMessages.CUSTOM_SEPARATOR_NOT_PROVIDED
+import calculator.ErrorMessages.INVALID_CHARACTER
+import calculator.ErrorMessages.INVALID_CUSTOM_FORMAT
+import calculator.ErrorMessages.NUMBER_NOT_PROVIDED
 import camp.nextstep.edu.missionutils.Console
 
 const val CUSTOM_PREFIX = "//"
@@ -7,52 +11,49 @@ const val CUSTOM_SUFFIX = "\\n"
 
 fun main() {
     print("덧셈할 문자열을 입력해 주세요.\n")
-    val originCalculation: String = Console.readLine()
-    val separatorList = mutableListOf(",", ":")
+    val originInput: String = Console.readLine()
+    val separators = mutableListOf(",", ":")
 
-    val processedCalculation = extractCustomSeparator(originCalculation, separatorList)
-    val result = calculateSum(processedCalculation, separatorList)
+    val parsedInput = extractCustomSeparator(originInput, separators)
+    val result = calculateSum(parsedInput, separators)
 
     print("결과 : $result")
 }
 
-fun extractCustomSeparator(originCalculation: String, separatorList: MutableList<String>): String {
-    var processedCalculation = originCalculation
+fun extractCustomSeparator(originInput: String, separators: MutableList<String>): String {
+    var parsedInput = originInput
 
-    while (processedCalculation.contains(CUSTOM_PREFIX) && processedCalculation.contains(CUSTOM_SUFFIX)) {
-        val prefixIndex = processedCalculation.indexOf(CUSTOM_PREFIX) + CUSTOM_PREFIX.length
-        val suffixIndex = processedCalculation.indexOf(CUSTOM_SUFFIX)
+    while (parsedInput.contains(CUSTOM_PREFIX) && parsedInput.contains(CUSTOM_SUFFIX)) {
+        val prefixIndex = parsedInput.indexOf(CUSTOM_PREFIX) + CUSTOM_PREFIX.length
+        val suffixIndex = parsedInput.indexOf(CUSTOM_SUFFIX)
 
         if (prefixIndex < suffixIndex) {
-            val customSeparator = processedCalculation.substring(prefixIndex, suffixIndex)
-            separatorList.takeIf { !it.contains(customSeparator) }?.add(customSeparator)
+            val customSeparator = parsedInput.substring(prefixIndex, suffixIndex)
+            separators.takeIf { !it.contains(customSeparator) }?.add(customSeparator)
 
             customSeparator.takeIf { it.isNotBlank() }
-                ?: throw IllegalArgumentException(ErrorMessages.CUSTOM_SEPARATOR_NOT_PROVIDED)
+                ?: throw IllegalArgumentException(CUSTOM_SEPARATOR_NOT_PROVIDED)
 
-            processedCalculation = processedCalculation.substring(suffixIndex + CUSTOM_SUFFIX.length)
+            parsedInput = parsedInput.substring(suffixIndex + CUSTOM_SUFFIX.length)
         } else {
-            throw IllegalArgumentException(ErrorMessages.INVALID_CUSTOM_FORMAT)
+            throw IllegalArgumentException(INVALID_CUSTOM_FORMAT)
         }
     }
 
-    return processedCalculation
+    return parsedInput
 }
 
-fun calculateSum(processedCalculation: String, separatorList: List<String>): Int {
-    // 구분자들을 |로 묶어 정규식 패턴을 만들고 문자열을 쪼갠다
-    val separatorPattern = separatorList.joinToString("|") { Regex.escape(it) }
-    val numbers = processedCalculation.split(Regex(separatorPattern))
+fun calculateSum(parsedInput: String, separators: List<String>): Int {
+    val separatorPattern = separators.joinToString("|") { Regex.escape(it) }
+    val numbers = parsedInput.split(Regex(separatorPattern))
     var sum = 0
 
     for (number in numbers) {
         if (number.isBlank()) {
-            throw IllegalArgumentException(ErrorMessages.NUMBER_NOT_PROVIDED)
+            throw IllegalArgumentException(NUMBER_NOT_PROVIDED)
         }
-
-        sum += number.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessages.INVALID_CHARACTER)
+        sum += number.toIntOrNull() ?: throw IllegalArgumentException(INVALID_CHARACTER)
     }
-
     return sum
 }
 
