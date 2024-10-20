a5a94d3ed0329d4e11c302b72fba76ff019b2a4f
wjdrjs00 <kimjk3409@gmail.com>
2024-10-21 02:45:01 +0900

    feat(Operand): 기본구분자를 통한 피연산자 추출 기능 구현
    - 입력값에서 기본구분자를 통해 문자 피연산자 추출
    - 추출된 문자 피연산자 값 숫자로 변환을 위한 메서드 호출

--- src/main/kotlin/calculator/domain/Operand.kt
+++ src/main/kotlin/calculator/domain/Operand.kt
@@ -5,4 +5,9 @@ class Operand(
     private val separator: Separator
 ) {
     private val operand = mutableListOf<Int>()
+
+    private fun splitByDefaultSeparators() {
+        val stringOperand = inputValue.split(separator.colon, separator.comma)
+        parseAndAddOperands(stringOperand)
+    }
 }
