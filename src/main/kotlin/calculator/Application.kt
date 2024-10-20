9d4302230adb286c0b438b1b0f3cb545a7455c0b
Min Seon <cbdhoho1@gmail.com>
2024-10-21 05:16:59 +0900

    커스텀 구분자 지정 및 합 변환 기능
    앞서 작성한 코드에서 지정자를 리스트로 변경하였다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,13 +1,24 @@
 package calculator
 
 fun main() {
+    println("덧셈할 문자열을 입력해주세요.")
     var char = readLine()
     var splitedChar = SplitChar(char)
     var sum = splitedChar?.sum()
+    print("결과 : $sum")
 }
 
 fun SplitChar(char: String?): List<Int>? {
-    var splitedChar = char?.split(',','"')
-    var splitedCharToInt = splitedChar?.map { it.toInt() }
-    return splitedCharToInt
+    if (char == null) return null
+
+    var modifiedChar = char
+    val separators = mutableListOf(',', ':')
+
+    if (modifiedChar.startsWith("/")) {
+        modifiedChar.getOrNull(2)?.let { separators.add(it) }
+        modifiedChar = modifiedChar.drop(5)
+    }
+
+    val splitedChar = modifiedChar.split(*separators.toCharArray())
+    return splitedChar.mapNotNull { it.toIntOrNull() }
 }
