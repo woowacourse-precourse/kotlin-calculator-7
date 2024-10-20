bc006e1a9ef40753b4781a86e6867b8529dc2c93
DDangDin <Kcorelec7123@gmail.com>
2024-10-21 00:39:48 +0900

    refactor: 코드 캡슐화 진행 및 일부 메서드 수정
    - 구분자 정보를 갖고 있는 DelimiterInfo 추가
    - DelimiterInfo를 생성해주는 유틸리티 클래스 DelimiterInfoManager 추가
    - StringPlusCalculator(문자열 덧셈 계산기) 클래스 추가
    - StringPlusCalculator - splitByDelimiters 메서드 수정
    - Utils 파일 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,84 +2,9 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console.readLine
 
-private val DEFAULT_DELIMITERS = listOf(",", ":")
-private val DELIMITERS_CONDITIONS = listOf("//", "\\n")
-private const val INPUT_NOTICE_TEXT = "덧셈할 문자열을 입력해 주세요."
-private const val RESULT_TEXT = "결과 : "
-
 fun main() {
-    executeCalculator {
-        val input = readLine().validate()
-        val customDelimiters = getCustomDelimiter(input)
-        val finalDelimiters = createFinalDelimiters(customDelimiters)
-        val inputStringList = splitByDelimiters(input, finalDelimiters)
-        val inputIntList = inputStringList.toPositiveIntList()
-        inputIntList.sum()
+    val stringPlusCalculator = StringPlusCalculator()
+    stringPlusCalculator.execute {
+        readLine()
     }
-}
-
-// 커스텀 구분자를 판별하고 만약 있다면 반환, 없다면 null을 반환한다.
-private fun getCustomDelimiter(input: String): String? {
-    if (input.findCustomDelimiter()) {
-        return null
-    }
-    val customDelimiter = input.split(*DELIMITERS_CONDITIONS.toTypedArray())
-    if (customDelimiter[1].isEmpty()) {
-        throw IllegalArgumentException()
-    }
-    return customDelimiter[1]
-}
-
-private fun String.findCustomDelimiter(): Boolean {
-    var cnt = 0
-    DELIMITERS_CONDITIONS.forEach { dc ->
-        if (dc in this) cnt++
-    }
-    return cnt != DELIMITERS_CONDITIONS.size
-}
-
-// 구분자를 기준으로 문자열을 나눈다.
-private fun splitByDelimiters(
-    input: String,
-    delimiters: List<String>,
-): List<String> {
-    val removedPrefixInput =
-        input
-            .split(DELIMITERS_CONDITIONS[1])
-            .last()
-    return removedPrefixInput.split(*delimiters.toTypedArray())
-}
-
-// 최종적으로 사용할 구분자 리스트를 만든다.
-fun createFinalDelimiters(customDelimiter: String?): List<String> {
-    if (customDelimiter == null) {
-        return DEFAULT_DELIMITERS
-    }
-    val mutableDefaultDelimiters = DEFAULT_DELIMITERS.toMutableList()
-    mutableDefaultDelimiters.add(customDelimiter)
-    return mutableDefaultDelimiters
-}
-
-// 입력 값에 대한 기본적인 유효성 검사
-fun String.validate(): String =
-    when {
-        this.isEmpty() -> throw IllegalArgumentException()
-        else -> this
-    }
-
-// 문자열 리스트를 정수형 리스트로 변환해준다.
-fun List<String>.toPositiveIntList(): List<Int> =
-    this.map {
-        val intValue = it.toIntOrNull()
-        if (intValue == null || intValue < 0) {
-            throw IllegalArgumentException()
-        }
-        intValue
-    }
-
-// 계산기 로직을 실행하고 결과를 출력한다.
-fun executeCalculator(call: () -> Int) {
-    println(INPUT_NOTICE_TEXT)
-    val sumOfIntList = call()
-    println(RESULT_TEXT + sumOfIntList)
 }
