b5154c1755a1ebb17e4260c1c58fe571ee3d747d
Tnalxmsk <alstnwkd990@naver.com>
2024-10-20 15:56:02 +0900

    refactor: 추출 로직 변경
    에러 검증 후 numbers 추출

--- src/main/kotlin/calculator/utils/Extractor.kt
+++ src/main/kotlin/calculator/utils/Extractor.kt
@@ -7,8 +7,16 @@ import calculator.extensions.toIntListAppliedCustomOperator
 object Extractor {
 
     fun extractNumbers(input: String): List<Int> {
-        return if (!input.isContainCustomOperator()) {
-            input.toIntListByOperator()
-        } else input.toIntListAppliedCustomOperator()
+        Validator.validateInput(input)
+        val result = mutableListOf<Int>()
+
+        if (!input.isContainCustomOperator()) {
+            result.addAll(input.toIntListByOperator())
+        } else {
+            result.addAll(input.toIntListAppliedCustomOperator())
+        }
+
+        Validator.validateNumbers(result)
+        return result
     }
 }
