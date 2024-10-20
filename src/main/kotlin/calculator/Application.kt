4ee574a2575bfbec8dbfe7122663341d77c26b4e
eden <devfeijoa@gmail.com>
2024-10-21 00:03:32 +0900

    feat: 구분자 추출 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -9,4 +9,18 @@ fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     userInput = Console.readLine()
 
+
+}
+
+fun extractDelimiter(userInput: String): Pair<Regex, String> {
+    val customDelimiterRegex = """^//(.)\n(.*)$""".toRegex()
+    val matchResult = customDelimiterRegex.find(userInput)
+
+    return if (matchResult != null) {
+        val customDelimiter = matchResult.groupValues[1]
+        val numbers = matchResult.groupValues[2]
+        Regex("[$customDelimiter,:]") to numbers
+    } else {
+        Regex("[,:]") to userInput
+    }
 }
