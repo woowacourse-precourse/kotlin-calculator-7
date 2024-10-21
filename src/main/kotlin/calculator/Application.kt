4cf61a3201da359799c1aff19812bf05cb904f27
Your Name <your-email@example.com>
2024-10-21 23:34:36 +0900

    수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,46 +1,95 @@
 package calculator
-
 import camp.nextstep.edu.missionutils.Console
-
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
-    val input = Console.readLine() // 사용자 입력 받기
-    try {
-        val result = add(input) // 입력 문자열 덧셈 계산
-        println("결과 : $result") // 결과 출력
-    } catch (e: IllegalArgumentException) {
-        // 잘못된 입력일 경우 프로그램이 조용히 종료되도록 함
-        return
+    val input = Console.readLine()
+    val textCalculator: TextCalculator = TextSumCalculator()
+    val result = textCalculator.calculate(input)
+    println("결과 : $result")
+}
+interface TextCalculator {
+    fun calculate(input: String): Number
+}
+class TextSumCalculator : TextCalculator {
+    private val textParser = TextParser()
+    override fun calculate(input: String): Number {
+        val numbers = textParser.parse(input)
+        return numbers.getSum()
+    }
+    private fun List<Double>.getSum(): Number {
+        var sum = 0.0
+        this.forEach { number -> sum += number }
+        return if (sum % 1 == 0.0) sum.toLong() else sum
     }
 }
+class TextParser {
+    private val divider = mutableListOf<String>(",", ":")
+    private var isCustomDividerUsed: Boolean = true
+    fun parse(input: String): List<Double> {
+        val splitInput = input.split(CUSTOM_DIVIDER_PREFIX, CUSTOM_DIVIDER_SUFFIX)
 
-fun add(input: String): Int {
-    if (input.isBlank()) {
-        return 0 // 빈 문자열의 경우 0 반환
-    }
-
-    // 사용자 정의 구분자 처리
-    if (input.startsWith("//")) {
-        val customDelimiterEnd = input.indexOf("\n") // 사용자 정의 구분자가 끝나는 위치 찾기
-        if (customDelimiterEnd == -1) {
-            return 0 // 줄바꿈이 없는 경우 빈 문자열로 처리하여 0 반환
+        if (!checkInputTypeAndValidity(splitInput)) throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
+        if (!checkInputTypeAndValidity(splitInput)) {
+            throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
         }
-        val customDelimiter = input.substring(2, customDelimiterEnd) // 사용자 정의 구분자 추출
-        val numbers = input.substring(customDelimiterEnd + 1) // 사용자 정의 구분자 이후의 숫자 추출
-        return splitAndSum(numbers, "[,|:|${Regex.escape(customDelimiter)}]") // 사용자 정의 구분자 및 기본 구분자로 숫자 분리 후 합 계산
-    } else {
-        return splitAndSum(input) // 기본 구분자(쉼표, 콜론)로 숫자 분리 후 합 계산
-    }
-}
 
-fun splitAndSum(input: String, delimiter: String = "[,|:]"): Int {
-    // 입력 문자열을 구분자로 분리한 뒤 각 숫자를 더하는 함수
-    if (input.isBlank()) {
-        return 0
+        return splitInput.splitByDivider()
     }
-    val numbers = input.split(Regex(delimiter)) // 사용자 정의 구분자 및 기본 구분자를 정규식으로 사용하여 분리
-        .map {
-            it.toIntOrNull() ?: throw IllegalArgumentException() // 숫자로 변환 불가능한 경우 예외 발생
+    private fun checkInputTypeAndValidity(input: List<String>): Boolean {
+        return when {
+            isInputExceeding(input) -> false
+            isCustomDividerNotUsed(input) -> true
+            isInvalidCustom(input) -> false
+            else -> {
+                addCustomDivider(input)
+                true
+            }
         }
-    return numbers.sum() // 숫자들의 합을 반환
+    }
+    private fun isInputExceeding(input: List<String>): Boolean {
+        return input.size >= CUSTOM_DIVIDER_EXCEEDING_SIZE
+    }
+    private fun isCustomDividerNotUsed(input: List<String>): Boolean {
+        val isCustomDividerNotUsed = input.size == CUSTOM_DIVIDER_NONE_SIZE
+        isCustomDividerUsed = isCustomDividerNotUsed.not()
+        return isCustomDividerNotUsed
+    }
+    private fun isInvalidCustom(input: List<String>): Boolean {
+        return input.first().isNotBlank()
+    }
+    private fun addCustomDivider(input: List<String>) {
+        divider.add(input[CUSTOM_DIVIDER_INDEX])
+    }
+    private fun List<String>.splitByDivider(): List<Double> {
+        if (this == listOf("")) return emptyList()
+        val regex = divider.joinToString("|") { Regex.escape(it) }.toRegex()
+        val expression = when (isCustomDividerUsed) {
+            true -> this[NUMBERS_WITH_CUSTOM_DIVIDER_INDEX]
+            false -> this[NUMBERS_WITHOUT_CUSTOM_DIVIDER_INDEX]
+        }
+        val splitResult = expression.split(regex)
+        return splitResult.map { it.toValidNumber() }
+    }
+    private fun String.toValidNumber(): Double {
+        return try {
+            val number = this.toDouble()
+            checkIsPositiveNumber(number)
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
+        }
+    }
+    private fun checkIsPositiveNumber(number: Double): Double {
+        if (number <= 0.0) throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
+        return number
+    }
+    companion object {
+        private const val INVALID_INPUT_MESSAGE = "유효하지 않은 입력입니다."
+        private const val CUSTOM_DIVIDER_PREFIX = "//"
+        private const val CUSTOM_DIVIDER_SUFFIX = "\\n"
+        private const val CUSTOM_DIVIDER_EXCEEDING_SIZE = 4
+        private const val CUSTOM_DIVIDER_NONE_SIZE = 1
+        private const val CUSTOM_DIVIDER_INDEX = 1
+        private const val NUMBERS_WITH_CUSTOM_DIVIDER_INDEX = 2
+        private const val NUMBERS_WITHOUT_CUSTOM_DIVIDER_INDEX = 0
+    }
 }
