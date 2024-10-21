71576bac94e6db402d6af65cb457de5a6f9ea7fd
hwsg0912@naver.com <hwsg0912@naver.com>
2024-10-21 20:31:04 +0900

    refactor: try-catch 문에서 runCatching 문으로 변경

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -7,15 +7,12 @@ class Validator {
     /**
      * 입력한 문자들이 양수인지 검증
      */
-    fun validatePositiveNumber(number: String): Boolean {
-        return try {
-            if (number.toLong() >= 0) {
-                true
-            } else {
-                throw IllegalArgumentException(Message.POSITIVE_NUMBER_ERROR)
-            }
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException(Message.INPUT_NUMBER_ERROR)
-        }
+    fun validatePositiveNumber(number: String) = runCatching {
+        number.toLong()
+    }.map {
+        if (it >= 0) true
+        else throw IllegalArgumentException(Message.POSITIVE_NUMBER_ERROR)
+    }.getOrElse {
+        throw IllegalArgumentException(Message.INPUT_NUMBER_ERROR)
     }
 }
