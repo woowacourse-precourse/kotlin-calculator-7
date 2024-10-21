d5703057d574d0a9635977ebb8414e7f6e9c14fe
dotoriysa <sunnahappy@naver.com>
2024-10-21 23:07:29 +0900

    feat: 잘못된 입력에 대한 예외 처리 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -14,18 +14,22 @@ fun main() {
 fun add(input: String?): Int {
     if (input.isNullOrEmpty()) return 0
 
-    // 커스텀 구분자 처리
-    if (input.startsWith("//")) {
-        val parts = input.split("\n", limit = 2) // 커스텀 구분자 부분과 숫자 부분 분리
-        val customDelimiter = parts[0].substring(2) // 구분자 추출 (예: "//;"에서 ";"만 추출)
-        return parts[1].split(customDelimiter) // 커스텀 구분자로 숫자 분리
+    try {
+        // 커스텀 구분자 처리
+        if (input.startsWith("//")) {
+            val parts = input.split("\n", limit = 2)
+            val customDelimiter = parts[0].substring(2)
+            return parts[1].split(customDelimiter)
+                .map { it.toInt() }
+                .sum()
+        }
+
+        // 기본 구분자 쉼표와 콜론 처리
+        val delimiters = "[,:]".toRegex()
+        return input.split(delimiters)
             .map { it.toInt() }
             .sum()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("잘못된 입력입니다.")
     }
-
-    // 기본 구분자 쉼표와 콜론 처리
-    val delimiters = "[,:]".toRegex()
-    return input.split(delimiters)
-        .map { it.toInt() }
-        .sum()
 }
