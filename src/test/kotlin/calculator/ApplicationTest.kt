7fc8e57f402f70604061bbecf2a52b672a92cd93
Jordan <leesoyoung0112@gmail.com>
2024-10-21 13:18:55 +0900

    test(ApplicationTest.kt) : add exception case, more than Int.MAX_VALUE

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -10,8 +10,8 @@ class ApplicationTest : NsTest() {
 
     @Test
     fun `일반 구분자 사용`() {
-        val testInput = arrayOf("\n", "1", "1,2", "1:2", "1,2:3")
-        val testOutput = arrayOf("결과 : 0", "결과 : 1", "결과 : 3", "결과 : 3", "결과 : 6")
+        val testInput = arrayOf("\n", "1", "1,2", "1:2", "1,2:3", "2147483647","2147483648", "2147483647,2147483647,12")
+        val testOutput = arrayOf("결과 : 0", "결과 : 1", "결과 : 3", "결과 : 3", "결과 : 6", "결과 : 2147483647", "결과 : 2147483647", "결과 : 4294967306")
         assertSimpleTest {
             for(i in 0 until testInput.size) {
                 run(testInput[i])
