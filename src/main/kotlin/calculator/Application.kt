33697ff740e07508d185c96df9ff983f37e9574d
yoon <cbj0010@naver.com>
2024-10-21 09:10:37 +0900

    feat : null 값을 입력했을 경우의 예외처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,6 +6,22 @@ fun main() {
     println("문자열을 입력해주세요.")
     val input = Console.readLine()
 
+    try {
+        checkValid(input)
+        println("입력된 값: $input")
+    } catch (e: IllegalArgumentException) {
+        println(e.message) // 예외 메시지를 출력
+    }
+}
 
+fun checkValid(input: String?) {
+    // 입력값이 null이거나 비어있으면 예외 발생
+    if (input.isNullOrEmpty()) {
+        throw IllegalArgumentException("잘못된 값을 입력했습니다. 종료합니다.")
+    }
+    findCustomDelimiter(input)
+}
+
+fun findCustomDelimiter(input: String) {
 
 }
