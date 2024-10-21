ea8550fc06cea2ff60d9ba180378eeb3a3a267c0
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-10-21 20:05:31 +0900

    feat: 쉼표 또는 콜론을 구분자로 가지는 문자열에 대한 결과처리
    1. 입력 메세지 "덧셈할 문자열을 입력해 주세요" 추가
    2. 쉼표 또는 콜론을 구분자로 갖는 문자열에 대한 덧셈 연산기능 추가
    3. 구분자를 기준으로 각 숫자의 합을 출력
    
    todo
    1. 커스텀 구분자 확인
    2. 오류처리
    3. 테스트
    4. model, view, controller로 구분

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,31 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+    var result = 0
+
+    val numList = mutableListOf<String>()
+
+    if (input.isNotEmpty()) {
+        for (n in input.split(",")) {
+            if (n.contains(":")) {
+                for (s in n.split(":")) {
+                    numList.add(s)
+                }
+            } else {
+                numList.add(n)
+            }
+        }
+
+        for (s in numList) {
+            result += s.toInt()
+        }
+
+        println("결과 : $result")
+    } else {
+        println("결과 : $result")
+    }
 }
