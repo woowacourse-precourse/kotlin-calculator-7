f9bc5baf2370ac1c8f7e0fa0ec1f113db9e87406
Eddie-Pak <foalstn@naver.com>
2024-10-21 16:17:31 +0900

    Feat: 빈 문자열, 기본구분자 처리기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,22 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+
+    val calculator = StringCalculator()
+    val result = calculator.calculate(input)
+
+    println("결과 : $result")
+}
+
+class StringCalculator {
+    fun calculate(input: String): Int {
+        if (input.isEmpty()) return 0
+
+        val numbers = input.split(",", ":")
+        return numbers.map { it.toInt() }.sum()
+    }
 }
