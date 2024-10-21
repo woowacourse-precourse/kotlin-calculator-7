95d83478ea51b7fe4547b64827fc4b20fe56e6c7
won0845 <tpqms775@gmail.com>
2024-10-21 19:31:21 +0900

    feat : CalculatorController Class 구현

--- src/main/kotlin/calculator/CalculatorController.kt
+++ src/main/kotlin/calculator/CalculatorController.kt
@@ -1,4 +1,17 @@
 package calculator
 
 class CalculatorController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun run() {
+        try {
+            val input = inputView.getInput()
+            val result = AddCalculator().calculate(input)
+            outputView.displayResult(result)
+        }catch (e: Exception) {
+            println(e.message)
+        }
+
+    }
 }
