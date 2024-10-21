ca4ae7a4a187a1984032dd774b79572e3a905927
조형석 <chs9902@naver.com>
2024-10-21 17:02:59 +0900

    :sparkles: feat: 문자열 분리 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,9 +8,41 @@ class Calculator {
 
     fun findCustom(line: String): Char? {
         val regex = "^//.\\n".toRegex()
-        if (regex.matches(line)){
+        if (regex.matches(line)) {
             return line.elementAt(2)
         }
         return null
+    }
+
+    fun separate(line: String, custom: Char?) {
+        val numbers = mutableListOf<Int>()
+        if (custom == null) {
+            var number = 0
+            for (each in line) {
+                if (each == ':' || each == ',') {
+                    numbers.add(number)
+                    number = 0
+                    continue
+                } else if (each in '0'..'9') {
+                    number = number * 10 + each.digitToInt()
+                } else {
+                    throw IllegalArgumentException()
+                }
+            }
+        } else {
+            val noCustom = line.substring(5)
+            var number = 0
+            for (each in noCustom) {
+                if (each == ':' || each == ',' || each == custom) {
+                    numbers.add(number)
+                    number = 0
+                    continue
+                } else if (each in '0'..'9') {
+                    number = number * 10 + each.digitToInt()
+                } else {
+                    throw IllegalArgumentException()
+                }
+            }
+        }
     }
 }
