1ec8e459ef8c45825a64e5f77bd82e461dcfb9bc
yeonjeen <jeendev2019@gmail.com>
2024-10-21 10:18:11 +0900

    feat: 기본 및 커스텀 구분자 분해

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,5 +5,20 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val inputText = Console.readLine()
-    println(inputText)
+    val (inputNumbers, delimiters) = divideDelimiters(inputText)
+    println(delimiters)
+}
+
+fun divideDelimiters(inputText: String): Pair<String, List<String>> {
+    return when {
+        inputText.startsWith("//") -> {
+            val customDelimiterEndIndex = inputText.indexOf("\\n")
+            val customDelimiter = inputText.substring(2, customDelimiterEndIndex)
+            val inputNumber = inputText.substring(customDelimiterEndIndex + 2)
+            Pair(inputNumber, listOf(customDelimiter))
+        }
+        else -> {
+            Pair(inputText, listOf(",", ":"))
+        }
+    }
 }
