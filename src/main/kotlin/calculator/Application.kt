7a7c2ed032013ce74121ac225a44c85f6a9a57b4
oungsi2000 <oungsi1000@gmail.com>
2024-10-21 23:41:09 +0900

    feat:content modify -modified content 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,155 @@
 package calculator
 
+class Token(kind: String, value: String = "") {
+    var kind = kind
+    var value = value
+}
+
+class CharReader(code:String="", var idx:Int=0) {
+
+    var codeArray = code.toCharArray()
+    fun read():Char {
+        if (idx == codeArray.size) {
+            //EOF Token
+            return '!'
+        }
+        return codeArray[idx]
+    }
+
+    fun advance() {
+        if (idx == codeArray.size) {
+            return
+        }
+        idx += 1
+    }
+
+    fun retreat() {
+        idx +- 1
+    }
+}
+
+class Tokenizer(code:String="") {
+    var reader = CharReader(code)
+    var customOperator:MutableList<Char> = mutableListOf()
+
+    fun exceptionThrower() {
+        throw IllegalArgumentException("잘못된 값을 입력하셨습니다")
+    }
+
+    fun tokenize():Token {
+        var char = reader.read()
+        reader.advance()
+
+        if (char in ":,!\n") {
+
+            return Token("Operator", char.toString())
+
+        } else if (char == '\"') {
+            return Token("defineCode", char.toString())
+
+        } else if (char == '/') {
+            var nextChar = reader.read()
+            if (nextChar == '/') {
+                reader.advance()
+
+                var nextChar = reader.read()
+
+                while (true){
+                    customOperator.add(nextChar)
+                    reader.advance()
+                    nextChar = reader.read()
+
+                    if (nextChar == '\\') {
+                        reader.advance()
+                        nextChar = reader.read()
+                        if (nextChar == 'n') {
+                            reader.advance()
+                            break
+                        }
+                    }
+                }
+
+            } else {
+                this.exceptionThrower()
+            }
+
+        } else if (char in "0123456789") {
+            var numbers: MutableList<Char> = mutableListOf()
+            numbers.add(char)
+
+            while (true) {
+                var nextChar = reader.read()
+                if (nextChar in "0123456789") {
+                    reader.advance()
+                    numbers.add(nextChar)
+                    continue
+                }
+
+                var value = numbers.joinToString(separator = "")
+                return Token("number", value)
+            }
+
+        } else {
+            if (customOperator.size == 0) {
+                this.exceptionThrower()
+            }
+            for ((iter, customChar) in customOperator.withIndex()) {
+                if (char == customChar) {
+
+                    if (iter != customOperator.size - 1) {
+                        char = reader.read()
+                        reader.advance()
+                    }
+
+                } else {
+                    this.exceptionThrower()
+                }
+            }
+
+            var value = customOperator.joinToString(separator = "")
+            return Token("customOperator", value)
+        }
+
+        return Token("")
+    }
+
+    fun getTokens():List<Token> {
+        var tokens: MutableList<Token> = mutableListOf()
+
+        while (reader.read() != '!') {
+            tokens.add(this.tokenize())
+        }
+        return tokens
+    }
+}
+
+class Parser(var tokens:List<Token>) {
+    fun result():Int {
+        var expression = ""
+        for (token in tokens) {
+            if (token.kind == "defineCode") {
+                continue
+            } else if (token.kind == "number") {
+                expression += token.value
+            } else if (token.kind == "Operator") {
+                expression += "+"
+            } else {
+                continue
+            }
+        }
+        val numbers = expression.split("+").map { it.toInt() }
+        val result = numbers.sum()
+        return result
+    }
+
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    print("덧셈할 문자열을 입력해 주세요\n")
+    val name = camp.nextstep.edu.missionutils.Console.readLine()
+    val tokens = Tokenizer(name).getTokens()
+    val result = Parser(tokens).result()
+    print("결과 : " + result)
+
 }
