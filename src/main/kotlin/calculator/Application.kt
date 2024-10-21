0d80a3e1e2ef8835e48c48143a9d126e128a7128
cucumber99 <dlsguq1@gmail.com>
2024-10-21 16:50:55 +0900

    feat: 프로그램 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,8 @@
 package calculator
 
+import calculator.controller.CalculationController
+
 fun main() {
-    // TODO: 프로그램 구현
+    val calculationController = CalculationController()
+    calculationController.start()
 }
