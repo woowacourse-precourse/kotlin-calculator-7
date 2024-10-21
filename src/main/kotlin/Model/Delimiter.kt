7d46b80b3a91010eba19565731a4e07d1d1aaf3f
nuyhhyun <sarahs19991115@gmail.com>
2024-10-21 17:47:29 +0900

    style($Delimiter): 포맷팅 및 필요 없는 코드 삭제
    단락 구분으로 가독성을 높이고 디버깅용으로 작성했던 코드를 삭제하였다.

--- src/main/kotlin/Model/Delimiter.kt
+++ src/main/kotlin/Model/Delimiter.kt
@@ -4,6 +4,7 @@ class Delimiter(private val input: String) {
     private var specified = 0
     fun numberSplit(): List<Int> {
         val delimiter = getDelimiter()
+
         val numbers = input.substring(specified).split(*delimiter).map { it.toInt() }
 
         return numbers
@@ -11,10 +12,10 @@ class Delimiter(private val input: String) {
 
     private fun getDelimiter(): Array<String> {
         val delimiters = mutableListOf(",", ":")
-        println(input)
 
         val regex = Regex("^//.\\\\n")
         val specifiedString = regex.find(input)
+
         if (specifiedString != null) {
             val specifiedDelimiter = specifiedString.value[2]
             delimiters.add(specifiedDelimiter.toString())
