a70297140182e62c442856ed5db10451b7448c31
won0845 <tpqms775@gmail.com>
2024-10-21 20:03:12 +0900

    docs : AddCalculator 추가 및 calculate, getDelimiter, splitNumbers 프로토타입구현

---
+++ src/main/kotlin/calculator/AddCalculator.kt
@@ -0,0 +1,19 @@
+package calculator
+
+class AddCalculator {
+    fun calculate(input: String?): Int {
+        if (input.isNullOrBlank()) {
+            return 0
+        }
+        val delimiter = getDelimiter(input)
+        val numbers = splitNumbers(input, delimiter)
+        return numbers.sum()
+    }
+
+    fun getDelimiter(input: String): String {
+        TODO()
+    }
+    fun splitNumbers(input:String, delimiter:String):List<Int>{
+        TODO()
+    }
+}
