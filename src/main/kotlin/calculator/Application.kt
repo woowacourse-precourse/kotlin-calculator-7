e5266daa998fbad7e60e93401f9c222b61d1b69b
park99999 <sungung1026@naver.com>
2024-10-21 14:23:39 +0900

    feat : 기본 입력 기능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,13 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val strInput = Console.readLine()
+    println("입력값: $strInput")
+}
+
+fun splitNumbers(input: String): List<String> {
+    return input.split(",", ":")
 }
