7f1e20838db8610ee44e96b59548349517c51fff
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-21 20:48:42 +0900

    feat(separator): add function getCustomSeparates
    Added function getCustomSeparates
    - If input includes custom separators (// and \n), extracts characters between them.
    - Required to correctly parse and handle input strings with custom separators.

--- src/main/kotlin/calculator/Separator.kt
+++ src/main/kotlin/calculator/Separator.kt
@@ -1,6 +1,20 @@
 package calculator
 
 class Separator {
-    val separates: MutableList<String> = mutableListOf(",", ":")
-    val customSeparates: List<String> = listOf("//", "\n")
+    val separates: MutableList<Char> = mutableListOf(',', ':')
+    val customSeparates = Pair("//", "\n")
+
+    private fun getCustomSeparate(input: String) {
+        if (!input.startsWith(customSeparates.first)) return
+
+        val startIndex = customSeparates.first.length
+        val endIndex = input.indexOf(customSeparates.second)
+        require(endIndex != 1) {"커스텀 구분자를 이용하려면 //, \n 사이에 작성하세요."}
+
+        separates.addAll(input.substring(startIndex, endIndex).toList())
+    }
+
+    fun run(input: String) {
+        getCustomSeparate(input)
+    }
 }
