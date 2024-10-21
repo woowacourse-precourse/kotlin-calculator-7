881d990831ee33c0b1816b5c27521a18775a3ffd
Min Seon <cbdhoho1@gmail.com>
2024-10-21 17:03:52 +0900

    예외처리 코드 작성 및 예외처리목록 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,7 +3,15 @@ package calculator
 fun main() {
     println("덧셈할 문자열을 입력해주세요.")
     var char = readLine()
+    if(char == ""){
+        throw IllegalArgumentException("String value must be filled.")
+    }
     var splitedChar = SplitChar(char)
+    splitedChar?.forEach { ch->
+        if(ch < 0){
+            throw IllegalArgumentException("Int value must be positive number.")
+        }
+    }
     var sum = splitedChar?.sum()
     print("결과 : $sum")
 }
@@ -12,13 +20,36 @@ fun SplitChar(char: String?): List<Int>? {
     if (char == null) return null
 
     var modifiedChar = char
-    val separators = mutableListOf(',', ':')
+    val newLine = modifiedChar.indexOf('n')
 
-    if (modifiedChar.startsWith("/")) {
-        modifiedChar.getOrNull(2)?.let { separators.add(it) }
-        modifiedChar = modifiedChar.drop(5)
+    if (modifiedChar.startsWith("//")) {
+        var newSeparator = modifiedChar.substring(2,3)
+        val separators: String
+        separators = newSeparator
+        if(separators.toIntOrNull() == null){
+            modifiedChar = modifiedChar.drop(newLine+2)
+            var splitedChar = modifiedChar.split(separators)
+            if(splitedChar.contains("")){
+                throw IllegalArgumentException("String must not contain whitespace.")
+            } else{
+                return splitedChar.mapNotNull { it.toIntOrNull() }
+            }
+        } else{
+            throw IllegalArgumentException("Separtator must not be Integer.")
+        }
+    } else {
+        if(modifiedChar.startsWith(',') or modifiedChar.startsWith(":")){
+            throw IllegalArgumentException("String must start with Integer.")
+        } else{
+            val separators = mutableListOf<Char>()
+            separators.add(',')
+            separators.add(':')
+            var splitedChar = modifiedChar.split(*separators.toCharArray())
+            if(splitedChar.contains("")){
+                throw IllegalArgumentException("String must not contain whitespace.")
+            } else{
+                return splitedChar.mapNotNull { it.toIntOrNull() }
+            }
+        }
     }
-
-    val splitedChar = modifiedChar.split(*separators.toCharArray())
-    return splitedChar.mapNotNull { it.toIntOrNull() }
 }
