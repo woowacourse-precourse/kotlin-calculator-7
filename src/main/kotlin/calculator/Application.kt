efe9043928ebdf39ccf3dd3e6b4ae0da43d8fabf
yeonjeen <jeendev2019@gmail.com>
2024-10-21 11:04:05 +0900

    feat: 잘못된 문자열에 따른 에러대응 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,19 +3,31 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
-    val inputText = Console.readLine()
-    val (inputNumbers, delimiters) = divideDelimiters(inputText)
-    val numberList = divideNumbers(inputNumbers, delimiters)
-    val sum = calculateSum(numberList)
-    println(inputNumbers)
-    println("결과 : $sum")
+    try {
+        println("덧셈할 문자열을 입력해 주세요.")
+        val inputText = Console.readLine()
+        val (inputNumbers, delimiters) = divideDelimiters(inputText)
+        val numberList = divideNumbers(inputNumbers, delimiters)
+        val sum = calculateSum(numberList)
+        println(inputNumbers)
+        println("결과 : $sum")
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
 
 fun divideDelimiters(inputText: String): Pair<String, List<String>> {
+    if (inputText.isEmpty()) {
+        throw IllegalArgumentException("빈 문자열은 입력할 수 없습니다.")
+    }
+
     return when {
         inputText.startsWith("//") -> {
             val customDelimiterEndIndex = inputText.indexOf("\\n")
+            if (customDelimiterEndIndex == -1) {
+                throw IllegalArgumentException("구분자 형식이 올바르지 않습니다.")
+            }
+
             val customDelimiter = inputText.substring(2, customDelimiterEndIndex)
             val inputNumber = inputText.substring(customDelimiterEndIndex + 2)
             Pair(inputNumber, listOf(customDelimiter))
@@ -33,5 +45,15 @@ fun divideNumbers(inputNumbers: String, delimiters: List<String>): List<String> 
 }
 
 fun calculateSum(numberList: List<String>): Int {
-    return numberList.sumOf { it.toInt() }
+    try {
+        return numberList.sumOf { number ->
+            val parsedNumber = number.toInt()
+            if (parsedNumber < 0) {
+                throw IllegalArgumentException("음수는 입력할 수 없습니다.")
+            }
+            parsedNumber
+        }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자 형식이 올바르지 않습니다.")
+    }
 }
