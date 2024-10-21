bcadc809836a52c66b94c226f730ab84ec0933b8
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-10-21 21:07:20 +0900

    feat: extract number from input

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,6 +2,8 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console.readLine
 
+val standard = mutableListOf(",", ":")
+
 fun main() {
     val input = getInput()
 
@@ -11,4 +13,8 @@ fun getInput(): String {
     println("덧셈할 문자열을 입력해 주세요.")
 
     return readLine()
+}
+
+fun extractNumber(input: String): List<String> {
+    return input.split(*standard.toTypedArray())
 }
