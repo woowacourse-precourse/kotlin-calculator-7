1411f57e389b68e4dba671c0cd18f58109894123
jusungwon <tjddnjspower@naver.com>
2024-10-21 20:43:09 +0900

    feat(delimiter): 커스텀 구분자를 추출하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     val userInputString = userInputString()
-    val splitList = splitString(userInputString)
-    val sum = sumSplitList(splitList)
-    println(sum)
+    println(findCustomDelimiter(userInputString))
+//    val splitList = splitString(userInputString)
+//    val sum = sumSplitList(splitList)
+//    println(sum)
+
 }
 
 fun userInputString():String {
@@ -15,6 +17,7 @@ fun userInputString():String {
 }
 
 fun splitString(userInputString:String):List<Int> {
+
     val basicSplitList = userInputString.split(",",":").map{it.toInt()}
     return basicSplitList
 
@@ -23,4 +26,10 @@ fun splitString(userInputString:String):List<Int> {
 fun sumSplitList(splitList:List<Int>):Int {
     val sum = splitList.sum()
     return sum
+}
+
+fun findCustomDelimiter(userInputString:String):String {
+    val start = userInputString.indexOf("//")
+    val end = userInputString.indexOf("\\n")
+    return userInputString.substring(start+2,end)
 }
