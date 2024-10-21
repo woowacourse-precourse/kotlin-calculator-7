73d656d6faa4aa332cc42ff8a66b6cb4ea747c5b
won0845 <tpqms775@gmail.com>
2024-10-21 19:43:28 +0900

    feat : addCalculator 추가

--- src/main/kotlin/calculator/CalculatorController.kt
+++ src/main/kotlin/calculator/CalculatorController.kt
@@ -1,13 +1,14 @@
 package calculator
 
-class CalculatorController {
-    private val inputView = InputView()
-    private val outputView = OutputView()
-
+class CalculatorController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    private val addCalculator: AddCalculator
+) {
     fun run() {
         try {
             val input = inputView.getInput()
-            val result = AddCalculator().calculate(input)
+            val result = addCalculator.calculate(input)
             outputView.displayResult(result)
         }catch (e: Exception) {
             println(e.message)
