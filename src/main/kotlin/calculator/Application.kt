259623f82a0f66957553f82df2374c5badb876b7
thsamajiki <ant1102@naver.com>
2024-10-21 17:48:52 +0900

    feat: 기본 구분자(쉼표, 콜론) 가져오는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,4 +5,20 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
+
+    val result = when {
+        input.isBlank() -> 0
+        input[0].isDigit() || input[0] == ',' || input[0] == ':' -> getDefaultDelimiter(input)
+        else -> {}
+    }
+}
+
+fun getDefaultDelimiter(input: String): Int {
+    return input.split(",", ":").calculateSum()
+}
+
+fun List<String>.calculateSum(): Int {
+    return sumOf { number ->
+        number.toIntOrNull() ?: throw IllegalArgumentException()
+    }
 }
