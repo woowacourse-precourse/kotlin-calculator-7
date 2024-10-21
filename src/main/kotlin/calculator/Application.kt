5c4ac04cc302266d9c2a876bbd6ae27376356ab4
joo <yjy010222gmail.com>
2024-10-21 22:02:09 +0900

    fix: 입력받는 명령어 readLine으로 정정 및 null 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,19 +1,22 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
-    val inp = readln()
     var sum = 0
-    val n = inp.length
+    val n :Int
     var num = 0
 
     var arr = arrayOf(',',':')
     var a = 0
+    val inp = readLine()
 
-    if(inp==""){
+    if(inp==""||inp==null){
         println("결과 : 0")
         return
     }
+    n = inp.length
     if(inp[0]=='/'&&inp[1]=='/'){
         arr+=inp[2]
         a=5
