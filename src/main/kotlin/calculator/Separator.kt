f30b657eb0f98b4606c7e9022b40f522beab7492
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-21 20:59:52 +0900

    feat(separator): return 0 for blank input
    if input is empty or " " return 0
    if input include only custom separates return 0

--- src/main/kotlin/calculator/Separator.kt
+++ src/main/kotlin/calculator/Separator.kt
@@ -3,18 +3,27 @@ package calculator
 class Separator {
     val separates: MutableList<Char> = mutableListOf(',', ':')
     val customSeparates = Pair("//", "\n")
+    var startIndex = 0
+    var endIndex = 0
+    val result: MutableList<Int> = mutableListOf(0)
 
     private fun getCustomSeparate(input: String) {
         if (!input.startsWith(customSeparates.first)) return
 
-        val startIndex = customSeparates.first.length
-        val endIndex = input.indexOf(customSeparates.second)
+        startIndex = customSeparates.first.length
+        endIndex = input.indexOf(customSeparates.second)
         require(endIndex != 1) {"커스텀 구분자를 이용하려면 //, \n 사이에 작성하세요."}
 
         separates.addAll(input.substring(startIndex, endIndex).toList())
     }
 
-    fun run(input: String) {
-        getCustomSeparate(input)
+    fun run(input: String): List<Int> {
+        if (input.isBlank()) return listOf(0)
+        if (input.startsWith(customSeparates.first)) {
+            getCustomSeparate(input)
+            if (endIndex+customSeparates.second.length == input.length) return listOf(0)
+        }
+
+        return result
     }
 }
