d5d0d547d361b2b74df25a53f6a6f87203dc3dfc
ezhoon <david0275@naver.com>
2024-10-21 22:07:55 +0900

    feat: 빈 문자열 또는 null 입력 시 0을 반환하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,20 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    // TODO : 프로그램 구현
+
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+    val result = add(input)
+    println("결과 : $result")
+}
+
+fun add(input: String?): Int {
+    if (input.isNullOrEmpty()) {
+        return 0
+    }
+    return -1  // 나머지 기능은 이후에 구현
 }
