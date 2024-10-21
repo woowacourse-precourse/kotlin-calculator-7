571831a383da7a802b5acad34420f171131d6c5d
PEANUTBUTTER1001 <yangjk9203@naver.com>
2024-10-21 20:47:07 +0900

    feat: 입력받기 및 결과출력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,24 @@
 package calculator
 
+
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    val userInput = Console.readLine()
+    val customDelimiterChecker = CustomDelimiterChecker()
+    val customDelimiterInputValidator = CustomDelimiterInputValidator()
+    val delimitersInputValidator = DelimitersInputValidator()
+    val additionCalculator = AdditionCalculator()
+
+    val verifiedUserInput = if (customDelimiterChecker.customDelimiterTest(userInput)) {
+        customDelimiterInputValidator.customDelimitersInputErrorCheck(userInput)
+    } else {
+        delimitersInputValidator.delimitersInputErrorCheck(userInput)
+    }
+
+    println("결과 : " + additionCalculator.calculate(verifiedUserInput))
 }
