c80d436716c752004d929b98308737e289a03108
조형석 <chs9902@naver.com>
2024-10-21 16:24:27 +0900

    :sparkles: feat: 커스텀 문자 처리 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,3 +3,14 @@ package calculator
 fun main() {
 
 }
+
+class Calculator {
+
+    fun findCustom(line: String): Char? {
+        val regex = "^//.\\n".toRegex()
+        if (regex.matches(line)){
+            return line.elementAt(2)
+        }
+        return null
+    }
+}
