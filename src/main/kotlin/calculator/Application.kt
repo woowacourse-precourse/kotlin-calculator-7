bd1bc2221c186b22d5da35c5cc172b4508229e7f
jooiss <jusong513@pusan.ac.kr>
2024-10-21 18:34:45 +0900

    refactor: 함수로 분리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,26 +1,48 @@
 package calculator
 
 fun main() {
-    lateinit var userInputList: List<String>
-    var sum = 0
+    val userInput = getInput()
+    if (handleEmptyInput(userInput)) return
+    val userInputList = parseInput(userInput!!)
+    val uIntInputList = convertToUIntList(userInputList)
+    val sum = calculateSum(uIntInputList)
+    printResult(sum)
+}
+
+fun getInput(): String? {
     println("덧셈할 문자열을 입력해 주세요.")
-    val userInput = readlnOrNull()
-    if (userInput.isNullOrEmpty()) {
+    return readlnOrNull()
+}
+
+fun handleEmptyInput(input: String?): Boolean {
+    if (input.isNullOrEmpty()) {
         println("결과 : 0")
-        return
+        return true
     }
-    if (userInput != null) {
-        if (userInput.startsWith("//")) {
-            val separatorList = userInput.split("\\n")
-            val indicator = separatorList[0].substring(2)
-            userInputList = separatorList[1].split(indicator)
-        }
-        else {
-            userInputList = userInput.split(",", ":")
-        }
+    return false
+}
+
+fun parseInput(input: String): List<String> {
+    val userInputList: List<String>
+    if (input.startsWith("//")) {
+        val separatorList = input.split("\\n")
+        val indicator = separatorList[0].substring(2)
+        userInputList = separatorList[1].split(indicator)
     }
-    val uIntInputList = userInputList.map { it.toUIntOrNull() ?: throw IllegalArgumentException("잘못된 값($it)을 입력했습니다.") }
-    val intInputList = uIntInputList.map { it.toInt() }
-    sum += intInputList.sum()
+    else {
+        userInputList = input.split(",", ":")
+    }
+    return userInputList
+}
+
+fun convertToUIntList(stringList: List<String>): List<UInt> {
+    return stringList.map { it.toUIntOrNull() ?: throw IllegalArgumentException("잘못된 값($it)을 입력했습니다.")}
+}
+
+fun calculateSum(uIntList: List<UInt>): Int {
+    return uIntList.map { it.toInt() }.sum()
+}
+
+fun printResult(sum: Int) {
     println("결과 : $sum")
 }
