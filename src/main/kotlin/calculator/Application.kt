b1a4fd1f59176475e5b553eef586a87cef9095bc
Sunghyeon <tjdgustjdan@gmail.com>
2024-10-21 15:29:08 +0900

    feat : 기본 구분자로 구성된 문자열에서 숫자 추출
    - 추출한 숫자의 조건 확인 : 양수가 아닐 경우 IllegalArgumentException를 발생시킨다.
    
    - 조건에 맞다면 문자열에서 숫자 형태 추출 후 int형으로 변환해준다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     var result = 0
-
-    val splitInput = listOf<Int>()
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
 
@@ -15,7 +13,22 @@ fun main() {
 
     if (input.contains(",") || input.contains(":")) {
         // , 또는 : 구분자를 기준으로 분리한다.
-        val numFromInput= input.split(",",":").map { it.toInt() }
+        val numFromInput= input.split("," , ":").map { it.toInt() }
+
+        // 조건에 맞는지 검사하기
+        for (num in numFromInput){
+            try {
+                if (num <= 0) {
+                    println("양수만 입력해주세요")
+                    Console.close()
+                    throw IllegalArgumentException("양수만 입력해주세요")
+                }
+            } catch (e: NumberFormatException){
+                println("프로그램을 종료합니다.")
+                Console.close()
+                throw IllegalArgumentException()
+            }
+        }
         println(numFromInput)
     }
 }
