f64e1bd54105d61f8df7e484c232242336c5f9fa
Sunghyeon <tjdgustjdan@gmail.com>
2024-10-21 15:17:30 +0900

    feat : 기본 구분자로 문자열 구분
    - input : Console.readLine()을 통해 받은 문자열
    - input이 공백이라면 0을 출력
    - input이 구분자로 , 또는 ; 을 가진다면 문자열 구분 후 int형으로 변환

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,21 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    var result = 0
+
+    val splitInput = listOf<Int>()
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+
+    if (input.isBlank()){
+        result = 0
+    }
+
+    if (input.contains(",") || input.contains(":")) {
+        // , 또는 : 구분자를 기준으로 분리한다.
+        val numFromInput= input.split(",",":").map { it.toInt() }
+        println(numFromInput)
+    }
 }
