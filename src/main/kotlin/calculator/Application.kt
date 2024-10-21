3a5a2022f677c3c265921dad482f1387fb244fe7
Your Name <your-email@example.com>
2024-10-21 23:41:34 +0900

    chore: 함수 가독성 개선

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,7 @@
 package calculator
 import camp.nextstep.edu.missionutils.Console
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")//
+    println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
     val textCalculator: TextCalculator = TextSumCalculator()
     val result = textCalculator.calculate(input)
@@ -22,6 +22,7 @@ class TextSumCalculator : TextCalculator {
         return if (sum % 1 == 0.0) sum.toLong() else sum
     }
 }
+
 class TextParser {
     private val divider = mutableListOf<String>(",", ":")
     private var isCustomDividerUsed: Boolean = true
@@ -35,6 +36,7 @@ class TextParser {
 
         return splitInput.splitByDivider()
     }
+
     private fun checkInputTypeAndValidity(input: List<String>): Boolean {
         return when {
             isInputExceeding(input) -> false
@@ -46,20 +48,25 @@ class TextParser {
             }
         }
     }
+
     private fun isInputExceeding(input: List<String>): Boolean {
         return input.size >= CUSTOM_DIVIDER_EXCEEDING_SIZE
     }
+
     private fun isCustomDividerNotUsed(input: List<String>): Boolean {
         val isCustomDividerNotUsed = input.size == CUSTOM_DIVIDER_NONE_SIZE
         isCustomDividerUsed = isCustomDividerNotUsed.not()
         return isCustomDividerNotUsed
     }
+
     private fun isInvalidCustom(input: List<String>): Boolean {
         return input.first().isNotBlank()
     }
+
     private fun addCustomDivider(input: List<String>) {
         divider.add(input[CUSTOM_DIVIDER_INDEX])
     }
+
     private fun List<String>.splitByDivider(): List<Double> {
         if (this == listOf("")) return emptyList()
         val regex = divider.joinToString("|") { Regex.escape(it) }.toRegex()
@@ -70,6 +77,7 @@ class TextParser {
         val splitResult = expression.split(regex)
         return splitResult.map { it.toValidNumber() }
     }
+
     private fun String.toValidNumber(): Double {
         return try {
             val number = this.toDouble()
@@ -78,18 +86,24 @@ class TextParser {
             throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
         }
     }
+
     private fun checkIsPositiveNumber(number: Double): Double {
         if (number <= 0.0) throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
         return number
     }
+
     companion object {
         private const val INVALID_INPUT_MESSAGE = "유효하지 않은 입력입니다."
+
         private const val CUSTOM_DIVIDER_PREFIX = "//"
         private const val CUSTOM_DIVIDER_SUFFIX = "\\n"
+
         private const val CUSTOM_DIVIDER_EXCEEDING_SIZE = 4
         private const val CUSTOM_DIVIDER_NONE_SIZE = 1
+
         private const val CUSTOM_DIVIDER_INDEX = 1
         private const val NUMBERS_WITH_CUSTOM_DIVIDER_INDEX = 2
+
         private const val NUMBERS_WITHOUT_CUSTOM_DIVIDER_INDEX = 0
     }
 }
