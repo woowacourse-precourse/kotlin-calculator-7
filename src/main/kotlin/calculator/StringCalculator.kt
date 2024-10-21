eddce9b7e3bcc809cc7e3cbe2dec6561d87f5490
Yujin Choe <dev.yujinchoe@gmail.com>
2024-10-21 23:12:07 +0900

    fix(calculator): 잘못된 입력(음수)시 예외 처리

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -14,7 +14,11 @@ class StringCalculator {
             "," to input.split(",", ":") // 기본 구분자 사용
         }
 
-        // 분리된 숫자를 정수로 변환하고 합산
-        return numbers.map { it.toInt() }.sum()
+        // 숫자 변환 및 합산, 예외 처리
+        return numbers.map {
+            val number = it.toIntOrNull() ?: throw IllegalArgumentException("잘못된 입력입니다.") // 정수 변환 실패 시 예외 발생
+            if (number < 0) throw IllegalArgumentException("음수는 허용되지 않습니다.") // 음수일 경우 예외 발생
+            number
+        }.sum()
     }
 }
