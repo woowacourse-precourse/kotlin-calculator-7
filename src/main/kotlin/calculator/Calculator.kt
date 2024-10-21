949c4cb1344c34434e930d2ac5d6bd5bcefb5a36
ss99x2002 <ss99x200211@gmail.com>
2024-10-21 09:46:28 +0900

    refac : 숫자 합산 로직 부가적인 확장함수 -> kotlin 고차함수로 변경

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,23 +2,13 @@ package calculator
 
 class Calculator(private val numbers: List<String>) {
 
-    fun sumOfSplitNumbers(): Int {
-        if (numbers.areAllDigits()) {
-            return numbers.sumOfNumbers()
-        } else {
-            throw IllegalArgumentException(INVALID_INPUT)
-        }
-    }
+    fun sumOfSplitNumbers(): Int = numbers
+        .map { it.toIntOrNull() ?: throw IllegalArgumentException(ErrorType.INVALID_NON_DIGIT_INPUT.message) }
+        .also { validateAllPositive(it) }
+        .sum()
 
-    private fun List<String>.sumOfNumbers(): Int {
-        return this.sumOf { it.toIntOrNull() ?: 0 }
-    }
-
-    private fun List<String>.areAllDigits(): Boolean {
-        return this.all { it.all { char -> char.isDigit() } }
-    }
-
-    companion object {
-        const val INVALID_INPUT = "잘못된 입력값이 들어왔습니다."
+    private fun validateAllPositive(numbers: List<Int>) {
+        if (numbers.any { it <= 0 })
+            throw IllegalArgumentException(ErrorType.INVALID_NON_POSITIVE_INPUT.message)
     }
 }
