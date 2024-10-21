3379d56f1eb0208ca6be42d8b2841fed178ef72c
limch37 <limch37@naver.com>
2024-10-21 21:20:19 +0900

    feat(StringValidator): 커스텀 구분자와 숫자 유효성 검사를 위한 함수 refactor

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,5 @@
 package calculator
 
-import calculator.ValidData.Companion.delimiters
-import calculator.ValidData.Companion.validNumbers
 import java.lang.IllegalArgumentException
 
 fun main() {
@@ -9,45 +7,12 @@ fun main() {
     val input = readLine() ?: ""
 
     try {
-        val result = add(input)
+        // 유효성 검사 및 숫자 리스트 반환 후 ValidData에 저장
+        ValidData.validNumbers = StringValidator().checkValidString(input).toMutableList()
+        // 합산 계산
+        val result = Calculator.sumNumbers()
         println("결과 : $result")
     } catch (e: IllegalArgumentException) {
         println("${e.message}")
     }
-}
-
-fun add(input: String): Int {
-    // 기능1. 빈 문자열 입력 처리
-    if (input.isEmpty()) {
-        return 0
-    }
-
-    // 기능2. 기본구분자(쉼표 또는 콜론)를 구분자로 가지는 문자열 덧셈
-    var numbers = input
-
-    // 기능3. 커스텀 구분자 지정
-    if (input.startsWith("//")) {
-        val customDelimiterPattern = Regex("//(.)\\\\n(.*)")
-        val matchResult = customDelimiterPattern.find(input)
-
-        if (matchResult != null) {
-            val customDelimiter = matchResult.groupValues[1] // 커스텀 구분자
-            delimiters += "|${Regex.escape(customDelimiter)}" // 기본 구분자와 커스텀 구분자를 함께 사용
-            numbers = matchResult.groupValues[2] // 숫자 부분
-        } else {
-            throw IllegalArgumentException("올바른 구분자가 입력되지 않았습니다.")
-        }
-    }
-
-    //구분자를 사용하여 숫자 분리
-    val splitNumbers = numbers.split(Regex(delimiters))
-
-    // 기능4. 숫자 변환 및 예외 처리(음수, 숫자가 아닌 값)
-    validNumbers = splitNumbers.map {
-        val number = it.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자가 입력되지 않았습니다.")
-        if (number < 0) throw IllegalArgumentException("음수는 허용되지 않습니다.")
-        number
-    }.toMutableList()
-
-    return Calculator.sumNumbers(validNumbers)
 }
--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,8 +2,9 @@ package calculator
 
 class Calculator {
     companion object {
-        fun sumNumbers(validNumbers: List<Int>): Int {
-            return validNumbers.sum()
+        //기능4. 유효한 숫자들의 덧셈을 반환
+        fun sumNumbers(): Int {
+            return ValidData.validNumbers.sum()
         }
     }
 }
