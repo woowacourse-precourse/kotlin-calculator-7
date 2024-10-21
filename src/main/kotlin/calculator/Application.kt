1990dbcde3f0a697b0bd65eabefc7c92f3c9201c
mcdmaisd <tkim0156@gmail.com>
2024-10-21 21:39:40 +0900

    feat: 커스텀 구분자 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,10 +4,38 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun add(input: String): Int {
     if (input.isEmpty()) return 0
-    return input.split(",", ":")
-        .sumOf { it.toInt() }
+
+    val (delimiter, numbers) = parseInput(input)
+    return calculateSum(numbers, delimiter)
 }
 
+fun parseInput(input: String): Pair<String, String> =
+    if (input.startsWith("//")) {
+        val parts = input.split("\\n", limit = 2)
+        if (parts.size != 2 || parts[0].length < 3) {
+            throw IllegalArgumentException("입력값이 올바르지 않습니다.")
+        }
+        val customDelimiter = Regex.escape(parts[0].substring(2))
+        customDelimiter to parts[1]
+    } else {
+        ",|:" to input
+    }
+
+fun calculateSum(numbers: String, delimiter: String): Int {
+    if (numbers.isEmpty()) return 0
+
+    val tokens = numbers.split(delimiter.toRegex())
+    var sum = 0
+    for (token in tokens) {
+        if (token.isNotEmpty()) {
+            val number = token.toInt()
+            sum += number
+        }
+    }
+    return sum
+}
+
+
 fun main() { // test
-    println(add("1,2:3"))
+    println(add("//;\\n1;2;3"))
 }
