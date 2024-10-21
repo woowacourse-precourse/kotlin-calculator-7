da75655eebc8c5e16d5c0aca0b9d54fbc8cd0307
yoon <cbj0010@naver.com>
2024-10-21 20:05:04 +0900

    feat : 커스텀 연산자로 나누기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,20 +8,33 @@ fun main() {
 
     try {
         checkValid(input)
-        println("입력된 값: $input")
+
     } catch (e: IllegalArgumentException) {
         println(e.message) // 예외 메시지를 출력
     }
 }
 
-fun checkValid(input: String?) {
-    // 입력값이 null이거나 비어있으면 예외 발생
-    if (input.isNullOrEmpty()) {
-        throw IllegalArgumentException("잘못된 값을 입력했습니다. 종료합니다.")
+
+
+private fun tokenizeString(inputText: String): Pair<String, List<String>> {
+    return if (inputText.startsWith("//")) {
+        val customDelimiterEndIndex = inputText.indexOf("\\n")
+        val customDelimiter = inputText.substring(2, customDelimiterEndIndex)
+        val inputNumber = inputText.substring(customDelimiterEndIndex + 2)
+        Pair(inputNumber, listOf(customDelimiter))
+    } else {
+        // 기본 구분자 설정
+        val defaultDelimiters = listOf(",", ":")
+        // 마지막 \n을 기준으로 나누기
+        val parts = inputText.split("\n")
+        val operators = parts.dropLast(1).joinToString("") // 마지막 \n 앞의 모든 부분을 연산자로
+        val numbers = parts.last() // 마지막 부분을 숫자로
+        Pair(numbers, defaultDelimiters + operators) // 숫자와 기본 구분자 + 연산자 반환
     }
-    findCustomDelimiter(input)
 }
 
-fun findCustomDelimiter(input: String) {
 
-}
+
+
+
+
