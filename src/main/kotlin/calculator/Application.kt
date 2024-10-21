3f3d22ecdfd9a2e2da265d49c92024c33a4cec60
parkjiwon <ji11won@naver.com>
2024-10-21 23:15:49 +0900

    feat : Console.readLine() 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,7 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // 정규식
     val regexExpression = "^[0-9,:]+$"
@@ -9,7 +11,7 @@ fun main() {
     // 입력 값
     val exampleInput = "12:3,4,2" // "" "1,2" "1,2,3" "1,2:3"
     println("덧셈할 문자열을 입해 주세요.")
-    println(exampleInput)
+    val customInput = Console.readLine()
 
     // 입력값 검사
     if (!regex.matches(exampleInput)) throw IllegalArgumentException("잘못된 입력 값을 입력하였습니다. ([0-9], [,], [:], [커스텀구분자]) 만 입력 가능합니다.")
