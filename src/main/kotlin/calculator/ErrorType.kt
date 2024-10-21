ed8d9f4a093044d482aaf95a6dd8a4e1b49adb91
ss99x2002 <ss99x200211@gmail.com>
2024-10-21 09:40:27 +0900

    feat : ErrorType 구분 클래스 구현

---
+++ src/main/kotlin/calculator/ErrorType.kt
@@ -0,0 +1,8 @@
+package calculator
+
+enum class ErrorType(val message: String) {
+    INVALID_NON_DIGIT_INPUT("구분자 또는 숫자가 아닌 잘못된 입력값이 들어왔습니다."),
+    INVALID_NON_POSITIVE_INPUT("양수가 아닌 잘못된 입력값이 들어왔습니다."),
+    INVALID_CUSTOM_DELIMITER("커스텀 구분자 지정이 잘못되었습니다."),
+    INVALID_EMPTY_INPUT("빈 문자열 입력값이 들어왔습니다.")
+}
