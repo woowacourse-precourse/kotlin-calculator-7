d937e22bb47db662a6d848bacb71b10650c19575
yeonjeen <jeendev2019@gmail.com>
2024-10-21 13:21:17 +0900

    refactor: 클래스명 'StringAdditionCalculator'로 변경

--- src/main/kotlin/calculator/StringAdditionCalculator.kt
+++ src/main/kotlin/calculator/StringAdditionCalculator.kt
@@ -2,7 +2,7 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
-class StringAddingCalculator {
+class StringAdditionCalculator {
     fun runCalculator() {
             println("덧셈할 문자열을 입력해 주세요.")
             val inputText = Console.readLine()
