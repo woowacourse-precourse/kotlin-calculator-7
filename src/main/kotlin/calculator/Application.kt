b38adcc17e705aa671ada353cd3bbbce28ee9644
SANDY <nnal0256@naver.com>
2024-10-21 15:48:42 +0900

    feat: 문자열 콘솔 입력받기
    콘솔 입력값 저장
     - getInput() : 콘솔로 입력받은 문자열을 리턴한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,19 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+    val input = getInput()
+}
+
+private const val INPUT_STR = "덧셈할 문자열을 입력해 주세요."
+/**
+ * 콘솔 입력값을 저장하는 기능을 하는 함수
+ * return : 콘솔 입력값
+ */
+fun getInput(): String {
+    println(INPUT_STR)
+    val input = Console.readLine()
+    return input
 }
