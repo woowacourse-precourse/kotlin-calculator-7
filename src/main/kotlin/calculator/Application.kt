cb8b98106468d268cd527de5ac7a2f8c629f4216
park99999 <sungung1026@naver.com>
2024-10-21 15:31:21 +0900

    feat : 예외 처리 업데이트

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,8 +8,8 @@ fun main() {
 
     // \n을 개행 문자로 바꿔주기
     val formattedInput = strInput.replace("\\n", "\n")
-
     val result = addNumbers(formattedInput)
+
     println("결과: $result")
     Console.close()
 }
@@ -27,6 +27,7 @@ fun addNumbers(input: String): Int {
 fun parseInput(input: String): Pair<String, String> {
     return if (input.startsWith("//")) {
         val delimiterEndIndex = input.indexOf("\n")
+        if(delimiterEndIndex == -1) throw IllegalArgumentException("잘못된 값 입니다.")
         val customDelimiter = input.substring(2, delimiterEndIndex)
         val numbers = input.substring(delimiterEndIndex + 1)
         Pair(customDelimiter, numbers)
@@ -36,13 +37,19 @@ fun parseInput(input: String): Pair<String, String> {
 }
 
 fun splitNumbers(input: String, delimiter: String): List<String> {
-    return input.split(Regex(delimiter))
+    val result = input.split(delimiter)
+
+    return result
 }
+
 fun validateNumbers(numbers: List<String>) {
     for (number in numbers) {
-        val num = number.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.")
+        val num = number.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다: '$number'")
         if (num < 0) {
-            throw IllegalArgumentException("음수는 허용되지 않습니디: $num")
+            throw IllegalArgumentException("음수는 허용되지 않습니다: $num")
         }
     }
 }
+
+
+
