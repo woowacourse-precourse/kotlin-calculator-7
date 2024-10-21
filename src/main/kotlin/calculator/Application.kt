7d0409e38134605beea4ea7ce36a3ded90c06202
Junghs-cloud <rabbit4935@gmail.com>
2024-10-21 09:53:51 +0900

    refactor: 클래스 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,18 +3,6 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    try {
-        val input = Console.readLine()
-        val delimiterParser = DelimiterParser(input)
-        val operands = delimiterParser.getOperands()
-        val calculator = Calculator(operands)
-        val result = calculator.calculate()
-        println("결과 : $result")
-    }
-    catch (e: java.util.NoSuchElementException) {
-        println("결과 : 0")
-    }
-    catch (e: IllegalArgumentException) {
-        throw e
-    }
+    val calculator = Calculator()
+    calculator.execute()
 }
--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,7 +1,25 @@
 package calculator
 
-class Calculator(private val operands: List<String>) {
-    fun calculate(): Int {
+import camp.nextstep.edu.missionutils.Console
+
+class Calculator() {
+    fun execute() {
+        try {
+            val input = Console.readLine()
+            val delimiterParser = DelimiterParser(input)
+            val operands = delimiterParser.getOperands()
+            val result = calculate(operands)
+            println("결과 : $result")
+        }
+        catch (e: java.util.NoSuchElementException) {
+            println("결과 : 0")
+        }
+        catch (e: IllegalArgumentException) {
+            throw e
+        }
+    }
+
+    private fun calculate(operands: List<String>): Int {
         return operands.sumOf{it.toInt()}
     }
 }
