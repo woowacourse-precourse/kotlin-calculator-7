0fb4614c85fb75dca350d69a62004872b2dfd5fb
Your Name <your-email@example.com>
2024-10-21 23:37:08 +0900

    fix: 로직 오류 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,7 @@
 package calculator
 import camp.nextstep.edu.missionutils.Console
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
+    println("덧셈할 문자열을 입력해 주세요.")//
     val input = Console.readLine()
     val textCalculator: TextCalculator = TextSumCalculator()
     val result = textCalculator.calculate(input)
