0481fd9b6ffad0de0188cefdc6a488823dad3837
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-21 14:37:51 +0900

    refactor(ApplicationTest): 테스트 함수 네이밍 수정
    - '~ 했을 때 ~ 하다' 로 수정하여 어떤 테스트고 어떻게 작동하는지 메서드 명을 보고 알 수 있도록 수정

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -23,7 +23,7 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `숫자만 입력`() {
+    fun `숫자만 입력했을 때는 입력한 숫자 그대로 반환한다`() {
         assertSimpleTest {
             run("1234")
             assertThat(output()).contains("결과 : 1234")
@@ -31,49 +31,49 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `입력 값 예외 테스트_숫자 없이 구분자만 입력`() {
+    fun `숫자 없이 구분자만 입력할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException(",,:") }
         }
     }
 
     @Test
-    fun `입력 값 예외 테스트_공밴만 입력`() {
+    fun `공백만 입력할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("    ") }
         }
     }
 
     @Test
-    fun `입력 값 예외 테스트_기본 구분자로 시작`() {
+    fun `기본 구분자로 시작할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException(",2:3") }
         }
     }
 
     @Test
-    fun `입력 값 예외 테스트_기본 구분자로 끝남`() {
+    fun `기본 구분자로 끝날 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("2:3,") }
         }
     }
 
     @Test
-    fun `입력 값 예외 테스트_커스텀 구분자로 시작`() {
+    fun `커스텀 구분자로 끝날 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("//#\n2#") }
         }
     }
 
     @Test
-    fun `알 수 없는 문자열 사용`() {
+    fun `알 수 없는 문자열(기본 구분자 및 커스텀 구분자가 아닐 때)을 사용할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("//n\n3#4") }
         }
     }
 
     @Test
-    fun `커스텀 구분자와 기본 구분자 섞어 사용`() {
+    fun `기본 구분자와 커스텀 구분자를 섞어 사용할 경우 문자열 덧셈 계산기는 작동한다`() {
         assertSimpleTest {
             run("//$\\n3$4:5")
             assertThat(output()).contains("결과 : 12")
@@ -81,21 +81,21 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `커스텀 구분자 예외 테스트_공백`() {
+    fun `커스텀 구분자를 공백으로 선언할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("// \\n3 4") }
         }
     }
 
     @Test
-    fun `커스텀 구분자 예외 테스트_숫자`() {
+    fun `커스텀 구분자를 숫자로 선언할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("//2\\n324") }
         }
     }
 
     @Test
-    fun `커스텀 구분자 예외 테스트_Dash`() {
+    fun `커스텀 구분자를 -(dash)로 선언할 경우 IllegalArgumentException 을 발생시킨다`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("//-\\n3-2-4") }
         }
