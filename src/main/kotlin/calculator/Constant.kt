f4867874f3aedfeb1a634de84fd8e9add89e0621
joh9911 <joh9911@naver.com>
2024-10-21 20:49:42 +0900

    feat: 커스텀 구분자 추출 기능 DelimiterExtractor 추가
    입력 문자열에서 커스텀 구분자 추출
    기본 구분자와 커스텀 구분자와 함께 반환
    Delimiter Extract 관련 예외 메시지 Constant 추가

--- src/main/kotlin/calculator/Constant.kt
+++ src/main/kotlin/calculator/Constant.kt
@@ -1,6 +1,17 @@
 package calculator
 
 object Constant {
+    // 커스텀 구분자
+    const val CUSTOM_DELIMITER_PREFIX = "//"
+    const val CUSTOM_DELIMITER_SUFFIX = "\n"
+    const val DEFAULT_DELIMITER_COMMA = ","
+    const val DEFAULT_DELIMITER_COLON = ":"
+
     const val INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요."
     const val OUTPUT_PROMPT = "결과 : "
+
+    private const val INVALID_INPUT_MESSAGE = "잘못된 입력입니다."
+    const val CUSTOM_DELIMITER_NEWLINE_MISSING = "$INVALID_INPUT_MESSAGE 커스텀 구분자 다음에 줄 바꿈이 필요합니다."
+    const val CUSTOM_DELIMITER_EMPTY = "$INVALID_INPUT_MESSAGE 커스텀 구분자가 비어 있습니다."
+
 }
