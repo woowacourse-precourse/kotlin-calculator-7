0a3b4ae7ea3b3676b8fe2eb9f02bae650f370143
Soobin-Park <jrkd2165@gmail.com>
2024-10-21 18:36:01 +0900

    feat: 구분자로 문자열을 분리하는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,8 +6,21 @@ fun main() {
     val input = readLine() ?: throw IllegalArgumentException("문자열은 구분자와 양수로만 이루어져야 합니다.")
 }
 
-fun inputString(input: String): Int {
+fun calculateString(input: String): Int {
     if (input.isBlank()) return 0
+
+    //기본 구분자
+    val delimiters = mutableListOf(",", ":")
+    var numbersPart = input
+
+    // 커스텀 구분자 처리
+    if (input.startsWith("//")) {
+        val parts = input.split("\n", limit = 2)
+        if (parts.size < 2) throw IllegalArgumentException("Invalid custom delimiter format")
+        val customDelimiter = parts[0].substring(2)
+        delimiters.add(customDelimiter)
+        numbersPart = parts[1]
+    }
 
     return input.length
 }
