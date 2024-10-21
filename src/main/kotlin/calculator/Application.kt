24955c2e92c8049aa802ca7bdb58c043c945e923
parkjiwon <ji11won@naver.com>
2024-10-21 23:16:11 +0900

    feat : 실제 입력값에 대한 결과 출력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,18 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // 정규식
-    val regexExpression = "^[0-9,:]+$"
-    val regex = regexExpression.toRegex()
-
+    val basicExpression = "^[0-9,:]+$"
+    val regex = basicExpression.toRegex()
 
     // 입력 값
-    val exampleInput = "12:3,4,2" // "" "1,2" "1,2,3" "1,2:3"
     println("덧셈할 문자열을 입해 주세요.")
     val customInput = Console.readLine()
 
     // 입력값 검사
-    if (!regex.matches(exampleInput)) throw IllegalArgumentException("잘못된 입력 값을 입력하였습니다. ([0-9], [,], [:], [커스텀구분자]) 만 입력 가능합니다.")
-    val input = exampleInput.split(",", ":")
+    if (!regex.matches(customInput)) throw IllegalArgumentException("잘못된 입력 값을 입력하였습니다. ([0-9], [,], [:], [커스텀구분자]) 만 입력 가능합니다.")
+    val input = customInput.split(Regex("[,:]"))
 
     // field
     var result = 0
