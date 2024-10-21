2f77ddc20f67156822a339802ceb29ff53da2527
Jordan <leesoyoung0112@gmail.com>
2024-10-21 11:26:34 +0900

    refactor(Application.kt) : separate functions by function

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,42 +4,32 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
-    var calc = readLine()
-    var result = 0
-    when(calc.length) {
-        0 -> {
-            // 아무것도 입력하지 않았을 때
-        }
-        else -> {
-            var custom: String? = null
-            var delimiters = mutableListOf(",", ":")
+    var input = readLine()
+    println("결과 : ${calculate(input)}")
 
-            // 커스텀 구분자인지 판단
-            if (calc.length >= 2 && calc.substring(0..1) == "//") {
-                // 커스텀 구분자인 경우
-                custom = calc.substring(2 until calc.indexOf("\\n"))
-                calc = calc.substring(calc.indexOf("\\n")+2 until calc.length)
-                delimiters.add(custom)
+}
 
-            }
-
-            val numbers = calc.split(*delimiters.toTypedArray()).map {
-                try {
-                    it.toInt()
-                } catch (e: NumberFormatException){
-                    throw IllegalArgumentException("Input cannot be entered as values other than the specified delimiter")
-                }
-
-            }
-            numbers.forEach{
-                if (it < 0) {
-                    throw IllegalArgumentException("Input values must be positive numbers only.")
-                }
-            }
-            result = numbers.sum()
-
-        }
+fun calculate(input: String?): Int {
+    if (input.isNullOrEmpty()) return 0
+    var calc = input
+    val delimiters = mutableListOf(",", ":")
+    // 커스텀 구분자인지 판단
+    if (calc.startsWith("//")) {
+        // 커스텀 구분자인 경우
+        val custom = calc.substring(2 until calc.indexOf("\\n"))
+        calc = calc.substring(calc.indexOf("\\n")+2 until calc.length)
+        delimiters.add(custom)
     }
-    println("결과 : ${result}")
+    val numbers = calc.split(*delimiters.toTypedArray()).map { isProperNumber(it)}
+    return numbers.sum()
+}
 
+fun isProperNumber(it: String): Int {
+    try {
+        val number = it.toInt()
+        if (number < 0) throw IllegalArgumentException("Input values must be positive numbers only.")
+        return number
+    } catch(e: NumberFormatException) {
+        throw IllegalArgumentException("Input cannot be entered as values other than the specified delimiter")
+    }
 }
