8820a6edf4ee2ba0283b489f502962793f7b9b4f
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-10-21 20:47:45 +0900

    feat: 오류처리 숫자가 양수가 아닌 경우
    1. 입력된 문자열에 숫자가 양수가 아닌경우 "양수로 구성된 문자열을 입력해 주세요."라는 오류메세지와 함께 어플을 종료시킨다
    
    todo
    1. 오류처리
    2. 테스트
    3. model, view, controller로 구분

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,52 +5,79 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
-    var result = 0
-
-    val numList = mutableListOf<String>()
 
     if (input.isNotEmpty()) {
         if (input.contains("//")) {
-            val newSeparator = input.substringAfter("//").substringBefore("\\n")
-            val newInput = input.substringAfter("\\n")
-
-            for (n in newInput.split(newSeparator)) {
-                if (n.contains(":")) {
-                    for (s in n.split(":")) {
-                        numList.add(s)
-                    }
-                } else if (n.contains(",")) {
-                    for (s in n.split(",")) {
-                        numList.add(s)
-                    }
-                } else {
-                    numList.add(n)
-                }
-            }
-
-            for (s in numList) {
-                result += s.toInt()
-            }
-
-            println("결과 : $result")
+            customSeparator(input)
         } else {
-            for (n in input.split(",")) {
-                if (n.contains(":")) {
-                    for (s in n.split(":")) {
-                        numList.add(s)
-                    }
-                } else {
-                    numList.add(n)
-                }
-            }
-
-            for (s in numList) {
-                result += s.toInt()
-            }
-
-            println("결과 : $result")
+            defaultSeparator(input)
         }
     } else {
-        println("결과 : $result")
+        println("결과 : 0")
+    }
+}
+
+private fun defaultSeparator(
+    input: String,
+) {
+    var result = 0
+    val numList = mutableListOf<String>()
+
+    for (n in input.split(",")) {
+        if (n.contains(":")) {
+            for (s in n.split(":")) {
+                errorNotPositiveNumber(s)
+                numList.add(s)
+            }
+        } else {
+            errorNotPositiveNumber(n)
+            numList.add(n)
+        }
+    }
+
+    for (s in numList) {
+        result += s.toInt()
+    }
+
+    println("결과 : $result")
+}
+
+private fun customSeparator(
+    input: String,
+) {
+    var result = 0
+    val numList = mutableListOf<String>()
+
+    val newSeparator = input.substringAfter("//").substringBefore("\\n")
+    val newInput = input.substringAfter("\\n")
+
+    for (n in newInput.split(newSeparator)) {
+        if (n.contains(":")) {
+            for (s in n.split(":")) {
+                errorNotPositiveNumber(s)
+                numList.add(s)
+            }
+        } else if (n.contains(",")) {
+            for (s in n.split(",")) {
+                errorNotPositiveNumber(s)
+                numList.add(s)
+            }
+        } else {
+            errorNotPositiveNumber(n)
+            numList.add(n)
+        }
+    }
+
+    for (s in numList) {
+        result += s.toInt()
+    }
+
+    println("결과 : $result")
+}
+
+
+private fun errorNotPositiveNumber(s: String) {
+    if (s.toInt() < 1) {
+        throw IllegalArgumentException("양수로 구성된 문자열을 입력해 주세요.")
     }
 }
