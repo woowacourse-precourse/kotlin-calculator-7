f91a089efc04d9a7f5dc20493afcc2832cd901bf
jusungwon <tjddnjspower@naver.com>
2024-10-21 20:42:04 +0900

    feat(split): 쉼표와 콜론을 기준으로 문자열 분리 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,10 +3,17 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    userInputString()
+    val userInputString = userInputString()
+    val splitList = splitString(userInputString)
+    println(splitList)
 }
 
 fun userInputString():String {
     val input = Console.readLine()
     return input
+}
+
+fun splitString(userInputString:String):List<Int> {
+    val basicSplitList = userInputString.split(",",":").map{it.toInt()}
+    return basicSplitList
 }
