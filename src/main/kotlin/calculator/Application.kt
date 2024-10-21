19b018457354f9b23287ae0537217120811168d6
jusungwon <tjddnjspower@naver.com>
2024-10-21 20:42:37 +0900

    feat(sum): 추출된 숫자의 합 계산 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val userInputString = userInputString()
     val splitList = splitString(userInputString)
-    println(splitList)
+    val sum = sumSplitList(splitList)
+    println(sum)
 }
 
 fun userInputString():String {
@@ -16,4 +17,10 @@ fun userInputString():String {
 fun splitString(userInputString:String):List<Int> {
     val basicSplitList = userInputString.split(",",":").map{it.toInt()}
     return basicSplitList
+
+}
+
+fun sumSplitList(splitList:List<Int>):Int {
+    val sum = splitList.sum()
+    return sum
 }
