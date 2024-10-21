2a0c41042a3412926c782e9313792216f9d75ced
nuyhhyun <sarahs19991115@gmail.com>
2024-10-21 17:39:55 +0900

    feat($Delimiter): 커스텀 구분자 기준 숫자 분리 기능 구현
    커스텀 구분자가 존재할 경우에 숫자를 분리할 수 있게 하였다.

--- src/main/kotlin/Model/Delimiter.kt
+++ src/main/kotlin/Model/Delimiter.kt
@@ -1,9 +1,11 @@
 package Model
 
 class Delimiter(private val input: String) {
+    private var specified = 0
     fun numberSplit(): List<Int> {
         val delimiter = getDelimiter()
-        val numbers = input.split(*delimiter).map { it.toInt() }
+        val numbers = input.substring(specified).split(*delimiter).map { it.toInt() }
+
         return numbers
     }
 
@@ -16,6 +18,7 @@ class Delimiter(private val input: String) {
         if (specifiedString != null) {
             val specifiedDelimiter = specifiedString.value[2]
             delimiters.add(specifiedDelimiter.toString())
+            specified = 5
         }
 
         return delimiters.toTypedArray()
