8694d10b5b6248139c7c22dca404f338fa49f124
doingkite <155817735+doingkite@users.noreply.github.com>
2024-10-21 11:36:27 +0900

    feat: 유효한 커스텀 구분자를 추출&반환

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -30,4 +30,18 @@ fun calculateSum(input: String): Int {
 // 숫자추출
 fun extractNumbers(input: String): List<Int> {
     val numberList: List<String>
+
+    // 유효한 커스텀 구분자를 추출하고 반환하는 함수
+    fun getCustomDelimiter(input: String): Pair<String, String> {
+        if (input[3] == '\\' && input[4] == 'n') {
+            val customDelimiter = input.substring(2, 3)
+            val numberSegment = input.substring(5)
+
+            if (customDelimiter.all { it !in '0'..'9' }) {
+                return Pair(customDelimiter, numberSegment)
+            } else {
+                throw IllegalArgumentException("유효하지 않은 구분자입니다.")
+            }
+        }
+        throw IllegalArgumentException("올바른 커스텀 구분자가 없습니다.")
 }
