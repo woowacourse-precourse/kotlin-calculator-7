9e5b74c7f9cf41d68887847bc9280a684c656742
Grove1212 <stella223@korea.ac.kr>
2024-10-21 18:16:59 +0900

    feat: make delimiterList

--- src/main/kotlin/calculator/Controller.kt
+++ src/main/kotlin/calculator/Controller.kt
@@ -1,10 +1,28 @@
 package calculator
 
 class Controller(private val view: View) {
-    fun run(){
-        try{
+    private fun parseInput(input: String): Data {
+        val delimiters = mutableListOf(",", ":")
 
-        } catch (e: IllegalArgumentException){
+        val customDelimiterPattern = Regex("//(.)\\n")
+        val matchResult = customDelimiterPattern.find(input)
+        val customDelimiter = matchResult?.groupValues?.get(1) // 구분자만 추출
+        delimiters.add(customDelimiter.toString())
+
+        val numbers = listOf(-1)
+
+        return Data(numbers, delimiters)
+    }
+
+    fun add(data: Data): Int {
+        return data.numbers.sum()
+    }
+
+    fun run() {
+        try {
+            var input = view.getInput()
+            val data = parseInput(input)
+        } catch (e: IllegalArgumentException) {
             view.showError(e)
         }
     }
