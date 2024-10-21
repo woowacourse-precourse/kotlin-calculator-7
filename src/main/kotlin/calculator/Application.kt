79839ae7ac281d52fe27619db84671adc3932122
park99999 <sungung1026@naver.com>
2024-10-21 14:40:55 +0900

    feat : 커스텀 구분자 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,15 +5,31 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val strInput = Console.readLine()
-    val result = addNumbers(strInput)
+
+    // \n을 개행 문자로 바꿔주기
+    val formattedInput = strInput.replace("\\n", "\n")
+
+    val result = addNumbers(formattedInput)
     println("결과: $result")
 }
 
 fun addNumbers(input: String): Int {
-    val numbers = splitNumbers(input)
-    return numbers.sumOf { it.toInt() }
+    val (delimiter, numbers) = parseInput(input)
+    val numberList = splitNumbers(numbers, delimiter)
+    return numberList.sumOf { it.toInt() }
 }
 
-fun splitNumbers(input: String): List<String> {
-    return input.split(",", ";")
+fun parseInput(input: String): Pair<String, String> {
+    return if (input.startsWith("//")) {
+        val delimiterEndIndex = input.indexOf("\n")
+        val customDelimiter = input.substring(2, delimiterEndIndex)
+        val numbers = input.substring(delimiterEndIndex + 1)
+        Pair(customDelimiter, numbers)
+    } else {
+        Pair(",|:", input)
+    }
+}
+
+fun splitNumbers(input: String, delimiter: String): List<String> {
+    return input.split(Regex(delimiter))
 }
