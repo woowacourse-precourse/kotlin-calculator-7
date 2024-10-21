20015dc1d5cf5ab33def6cb6b1040c262939d16a
park99999 <sungung1026@naver.com>
2024-10-21 14:25:41 +0900

    feat : 파싱된 결과 더하기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val strInput = Console.readLine()
-    println("입력값: $strInput")
+    val result = addNumbers(strInput)
+    println("결과: $result")
+}
+
+fun addNumbers(input: String): Int {
+    val numbers = splitNumbers(input)
+    return numbers.sumOf { it.toInt() }
 }
 
 fun splitNumbers(input: String): List<String> {
-    return input.split(",", ":")
+    return input.split(",", ";")
 }
