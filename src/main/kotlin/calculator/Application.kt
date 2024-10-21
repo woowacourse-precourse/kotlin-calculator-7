67a6cc7024fe02dca353f9815d6bd757703d6be5
윤승일(Seung-Il Yoon) <liqurt@gmail.com>
2024-10-21 14:40:26 +0900

    feat(input) : 예외 처리
    음수에 대한 예외 처리

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     var result : Int = 0
+    var numbers : Array<Int>
 
     println("덧셈할 문자열을 입력해 주세요.")
     val inputStr = Console.readLine()
@@ -12,10 +13,15 @@ fun main() {
     if(inputStr.isNotEmpty()){
         if(inputStr[0] == '/' && inputStr[1] == '/' && inputStr[3]=='\\' && inputStr[4] == 'n'){ // 커스텀 구분자 있음
             val customGubun = inputStr[2].toString()
-            result = inputStr.substring(5).split(",",":",customGubun).sumOf { it.toInt() }
+            numbers = inputStr.substring(5).split(",",":",customGubun).map { it.toInt() }.toTypedArray()
         }else{
-            result = inputStr.split(",",":").sumOf { it.toInt() }
+            numbers = inputStr.split(",",":").map { it.toInt() }.toTypedArray()
         }
+
+        if(numbers.any { i -> i<0 }){ // 예외 : 음수
+            throw IllegalArgumentException()
+        }
+        result = numbers.sum()
     }
 
     println("결과 : $result")
