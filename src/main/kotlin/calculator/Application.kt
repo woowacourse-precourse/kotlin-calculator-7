82b7ddc4a883ce2a8ba03e437e9a6647d31710df
chaemina <chaemina82@naver.com>
2024-10-21 18:30:38 +0900

    feat($Application) : 쉼표와 콜론이 들어간 문자열의 경우
    defaultSeparator 함수 구현
    - 입력 받은 문자열에 쉼표와 콜론이 들어간 경우 호출
    - split 메소드로 구분자 제거
    - 제거 후 리턴된 배열의 요소를 map을 이용해 Int형으로 변환
    - sum 메소드로 숫자의 합을 반환

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,17 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    print("덧셈할 문자열을 입력해 주세요: ")
+    val input: String = Console.readLine()
+
+    fun defaultSeparator(input: String): Int {
+        val numbers = input.split(",", ":").map { it.toInt() }
+        return numbers.sum()
+    }
+
+    val result: Int = defaultSeparator(input)
+    println("결과: $result")
 }
+
