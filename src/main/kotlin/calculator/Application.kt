347f8a88be1becda9d2c4711b554128f0a082f69
gixxla <gigixquisite@gmail.com>
2024-10-21 19:08:32 +0900

    feat: 숫자 추출 기능 추가
    입력한 문자열에서 숫자 추출하는 기능 추가
    - 기본 구분자 , : 사용
    - 사용자가 커스텀 구분자 지정 가능

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,5 +4,13 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.");
-    var input: String? = Console.readLine();
+    val input: String = Console.readLine();
+    var textArray: List<String>;
+
+    if (input.contains("//") && input.contains("\\n")) {
+        val temp = input.split("//", "\\n");
+        textArray = temp[2].split(temp[1], ",", ":");
+    } else {
+        textArray = input.split(",", ":");
+    }
 }
