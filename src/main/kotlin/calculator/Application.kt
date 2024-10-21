4ac705ffb0f89172229ae756db3ef36623a74b30
SANDY <nnal0256@naver.com>
2024-10-21 16:14:14 +0900

    feat: 기본 구분자를 이용한 숫자 추출
    문자열에서 숫자 추출
     - 전달받은 문자열에서 기본 구분자(:,)를 기준으로 분리한 숫자가 저장되어 있는 리스트를 리턴

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // TODO: 프로그램 구현
     val input = getInput()
+    val nums = getNumbers(input)
 }
 
 private const val INPUT_STR = "덧셈할 문자열을 입력해 주세요."
@@ -16,4 +17,13 @@ fun getInput(): String {
     println(INPUT_STR)
     val input = Console.readLine()
     return input
+}
+
+/**
+ * 문자열에서 숫자를 List로 추출하는 함수
+ */
+fun getNumbers(input: String): List<Int> {
+    // 기본 구분자(,:) 분리
+    val numbers = input.split(",", ":").map { it.toInt() }
+    return numbers
 }
