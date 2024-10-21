b2862148f981b01a5033c3ae67ef21eacbd1846c
이지윤 <02leejiyun@naver.com>
2024-10-21 20:48:42 +0900

    refactor: 함수 역할 분리

--- src/main/kotlin/calculator/domain/Calculator.kt
+++ src/main/kotlin/calculator/domain/Calculator.kt
@@ -7,46 +7,62 @@ class Calculator(
     private val numbers = mutableListOf<Int>()
 
     init {
-        parseCustomDelimiter()
-        parseNumbers()
+        processExpression()
     }
 
     fun add(): Int = numbers.sum()
 
-    private fun parseCustomDelimiter() {
-        if (expression.startsWith(CUSTOM_DELIMITER_PREFIX)) {
-            val customDelimiterStartIndex = CUSTOM_DELIMITER_PREFIX.length
-            val customDelimiterEndIndex = expression.indexOf(CUSTOM_DELIMITER_SUFFIX)
-
-            if (customDelimiterEndIndex != -1) {
-                val customDelimiter = expression.substring(customDelimiterStartIndex, customDelimiterEndIndex)
-                validateCustomDelimiter(customDelimiter)
-                delimiters.add(customDelimiter[0])
-                expression = expression.substring(customDelimiterEndIndex + CUSTOM_DELIMITER_SUFFIX.length)
-            }
+    private fun processExpression() {
+        if (hasCustomDelimiter()) {
+            parseCustomDelimiter()
         }
+        parseNumbers()
+    }
+
+    private fun hasCustomDelimiter(): Boolean =
+        expression.startsWith(CUSTOM_DELIMITER_PREFIX) && expression.indexOf(CUSTOM_DELIMITER_SUFFIX) != -1
+
+    private fun parseCustomDelimiter() {
+        val customDelimiter = findCustomDelimiter()
+        delimiters.add(customDelimiter)
+        removeCustomDelimiterFromExpression()
+    }
+
+    private fun findCustomDelimiter(): Char {
+        val customDelimiterStartIndex = CUSTOM_DELIMITER_PREFIX.length
+        val customDelimiterEndIndex = expression.indexOf(CUSTOM_DELIMITER_SUFFIX)
+
+        val customDelimiter =
+            expression.substring(customDelimiterStartIndex, customDelimiterEndIndex)
+        validateCustomDelimiter(customDelimiter)
+        return customDelimiter[0]
+    }
+
+    private fun removeCustomDelimiterFromExpression() {
+        val customDelimiterEndIndex = expression.indexOf(CUSTOM_DELIMITER_SUFFIX)
+        expression = expression.substring(customDelimiterEndIndex + CUSTOM_DELIMITER_SUFFIX.length)
     }
 
     private fun parseNumbers() {
-        if (expression.isBlank()) {
-            return
-        }
+        if (expression.isBlank()) return
 
-        val parsedExpression: List<String> = expression.split(*delimiters.toCharArray())
-        for (number in parsedExpression) {
-            val n = number.toIntOrNull()
-            validateNumber(n)
-            validatePositiveNumber(n!!)
-            numbers.add(n)
+        val splitNumbers = splitExpressionByDelimiters()
+        for (number in splitNumbers) {
+            val parsedNumber = parseNumber(number)
+            validatePositiveNumber(parsedNumber)
+            numbers.add(parsedNumber)
         }
     }
+
+    private fun splitExpressionByDelimiters(): List<String> {
+        return expression.split(*delimiters.toCharArray())
+    }
+
+    private fun parseNumber(number: String): Int =
+        number.toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_EXPRESSION)
 
     private fun validateCustomDelimiter(delimiter: String) {
         if (delimiter.length != 1) throw IllegalArgumentException(ERROR_INVALID_CUSTOM_DELIMITER)
-    }
-
-    private fun validateNumber(number: Int?) {
-        if (number == null) throw IllegalArgumentException(ERROR_INVALID_NUMBER)
     }
 
     private fun validatePositiveNumber(number: Int) {
@@ -59,8 +75,8 @@ class Calculator(
         private const val CUSTOM_DELIMITER_PREFIX = "//"
         private const val CUSTOM_DELIMITER_SUFFIX = "\\n"
 
-        private const val ERROR_INVALID_NUMBER = "[ERROR] 구분자 이외의 문자는 입력할 수 없습니다."
-        private const val ERROR_INVALID_CUSTOM_DELIMITER = "[ERROR] 커스텀 구분자는 한 문자여야 합니다."
+        private const val ERROR_INVALID_EXPRESSION = "[ERROR] 구분자와 양수로 구성된 문자열을 입력해야 합니다."
+        private const val ERROR_INVALID_CUSTOM_DELIMITER = "[ERROR] 커스텀 구분자 형식이 올바르지 않습니다."
         private const val ERROR_NUMBER_NOT_POSITIVE = "[ERROR] 숫자는 0 이상이어야 합니다."
     }
 }
