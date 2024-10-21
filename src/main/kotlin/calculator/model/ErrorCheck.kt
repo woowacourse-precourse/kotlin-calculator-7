3b66dc06ba798d8bc8b10e0201da3ad45dd26d5f
박지민 <pjm5489@naver.com>
2024-10-21 17:37:38 +0900

    feat: isNumberCheck() 메서드 추가
    inputValueList의 원소들이 숫자만 있는지 확인

--- src/main/kotlin/calculator/model/ErrorCheck.kt
+++ src/main/kotlin/calculator/model/ErrorCheck.kt
@@ -8,13 +8,28 @@ class ErrorCheck {
             divider.isDefault(inputValue) -> return 0
             divider.isCustom(inputValue) -> return 1
         }
+
         return -1
     }
 
-    fun negativeNumberCheck(inputValueList: List<Int>): Boolean {
-        for (i in inputValueList.indices) {
-            if (inputValueList[i] <= 0) return true
+    fun negativeNumberCheck(inputValueList: List<String>): Boolean {
+        if (!isNumberCheck(inputValueList)) {
+            val newInputValueList = inputValueList.map { it.toInt() }
+            for (i in newInputValueList.indices) {
+                if (newInputValueList[i] <= 0) return true
+            }
         }
+
+        return false
+    }
+
+    fun isNumberCheck(inputValueList: List<String>): Boolean {
+        try {
+            inputValueList.map { it.toInt() }
+        } catch (e: Exception) {
+            return true
+        }
+
         return false
     }
 }
