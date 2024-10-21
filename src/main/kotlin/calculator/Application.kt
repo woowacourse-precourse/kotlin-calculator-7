f68b28224dacc0764a38b024099cdc44986c65d4
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-10-21 20:28:30 +0900

    feat: 커스텀 구분자 확인
    1. 커스텀 구분자를 subStringAfter, subStringBefore로 구분, 이는 정규식을 사용하는 것보다 직관적이라고 생각하여 정함
    
    todo
    1. 오류처리
    2. 테스트
    3. model, view, controller로 구분

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -10,21 +10,46 @@ fun main() {
     val numList = mutableListOf<String>()
 
     if (input.isNotEmpty()) {
-        for (n in input.split(",")) {
-            if (n.contains(":")) {
-                for (s in n.split(":")) {
-                    numList.add(s)
+        if (input.contains("//")) {
+            val newSeparator = input.substringAfter("//").substringBefore("\\n")
+            val newInput = input.substringAfter("\\n")
+
+            for (n in newInput.split(newSeparator)) {
+                if (n.contains(":")) {
+                    for (s in n.split(":")) {
+                        numList.add(s)
+                    }
+                } else if (n.contains(",")) {
+                    for (s in n.split(",")) {
+                        numList.add(s)
+                    }
+                } else {
+                    numList.add(n)
                 }
-            } else {
-                numList.add(n)
             }
-        }
 
-        for (s in numList) {
-            result += s.toInt()
-        }
+            for (s in numList) {
+                result += s.toInt()
+            }
 
-        println("결과 : $result")
+            println("결과 : $result")
+        } else {
+            for (n in input.split(",")) {
+                if (n.contains(":")) {
+                    for (s in n.split(":")) {
+                        numList.add(s)
+                    }
+                } else {
+                    numList.add(n)
+                }
+            }
+
+            for (s in numList) {
+                result += s.toInt()
+            }
+
+            println("결과 : $result")
+        }
     } else {
         println("결과 : $result")
     }
