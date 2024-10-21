846fd73b81657f78c667726944396d3e429e68fc
dotoriysa <sunnahappy@naver.com>
2024-10-21 23:04:28 +0900

    feat: 쉼표와 콜론을 구분자로 숫자 합산 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,3 +4,13 @@ fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     // TODO: 프로그램 구현
 }
+
+fun add(input: String?): Int {
+    if (input.isNullOrEmpty()) return 0 // 입력이 빈 문자열일 경우 0 반환
+
+    // 쉼표 또는 콜론을 구분자로 숫자를 분리
+    val delimiters = "[,:]".toRegex()
+    return input.split(delimiters)
+        .map { it.toInt() } // 각 숫자를 Int로 변환
+        .sum() // 숫자들의 합을 반환
+}
