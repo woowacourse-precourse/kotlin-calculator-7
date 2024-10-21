1add2b617b0997558b6f8d6986590938fb415aed
Soobin-Park <jrkd2165@gmail.com>
2024-10-21 18:29:55 +0900

    feat: 입력시 문구 추가, 예외 처리 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,9 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    // 입력을 받는 부분
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = readLine() ?: throw IllegalArgumentException("문자열은 구분자와 양수로만 이루어져야 합니다.")
 }
 
 fun inputString(input: String): Int {
