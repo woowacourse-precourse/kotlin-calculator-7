1f72b15891315b28dc2dd59255676675331ebb98
doingkite <155817735+doingkite@users.noreply.github.com>
2024-10-21 11:30:49 +0900

    feat: 사용자 입력 처리 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,15 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    // 사용자 입력
+    println("덧셈할 문자열을 입력해 주세요.")
+    val userInput = Console.readLine() ?: ""
+
+    // 입력이 비어있으면 0 출력
+    if (userInput.isBlank()) {
+        println("결과: 0")
+        return
+    }
 }
