a9875bc7c55c83a4865e5f5a5b98c09c286e317b
nuyhhyun <sarahs19991115@gmail.com>
2024-10-21 17:23:07 +0900

    feat($Delimiter): 커스텀 구분자 식별 기능 구현
    커스텀 구분자를 식별해내 구분자로 등록하도록 하였다.

--- src/main/kotlin/Model/Delimiter.kt
+++ src/main/kotlin/Model/Delimiter.kt
@@ -1,8 +1,23 @@
 package Model
 
-class Delimiter {
-    fun numberSplit(input: String): List<Int> {
-        val numbers = input.split(",", ":").map { it.toInt() }
+class Delimiter(private val input: String) {
+    fun numberSplit(): List<Int> {
+        val delimiter = getDelimiter()
+        val numbers = input.split(*delimiter).map { it.toInt() }
         return numbers
+    }
+
+    private fun getDelimiter(): Array<String> {
+        val delimiters = mutableListOf(",", ":")
+        println(input)
+
+        val regex = Regex("^//.\\\\n")
+        val specifiedString = regex.find(input)
+        if (specifiedString != null) {
+            val specifiedDelimiter = specifiedString.value[2]
+            delimiters.add(specifiedDelimiter.toString())
+        }
+
+        return delimiters.toTypedArray()
     }
 }
