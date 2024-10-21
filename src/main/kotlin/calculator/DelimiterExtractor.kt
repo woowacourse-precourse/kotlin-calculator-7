ae56e0a8149686bab262581222668c199429c747
joh9911 <joh9911@naver.com>
2024-10-21 21:18:33 +0900

    feat: 주석 추가

--- src/main/kotlin/calculator/DelimiterExtractor.kt
+++ src/main/kotlin/calculator/DelimiterExtractor.kt
@@ -2,6 +2,10 @@ package calculator
 
 object DelimiterExtractor {
 
+    /**
+     * 입력 문자열에서 커스텀 구분자 추출
+     * 기본 구분자와 커스텀 구분자 함께 반환
+     */
     fun extractDelimiters(input: String): Pair<List<String>, String> {
         return if (input.startsWith(Constant.CUSTOM_DELIMITER_PREFIX)) {
             val delimiterEndIndex = input.indexOf(Constant.CUSTOM_DELIMITER_SUFFIX)
