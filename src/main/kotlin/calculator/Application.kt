f2ab745baa840c637ccb754c73ee91c45404fe39
jusungwon <tjddnjspower@naver.com>
2024-10-21 20:41:18 +0900

    feat(input): 문자열 입력받는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,12 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    userInputString()
+}
+
+fun userInputString():String {
+    val input = Console.readLine()
+    return input
 }
