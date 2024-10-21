669f4492dc636d9b4fe069b87439a9ec42db35a8
SANDY <nnal0256@naver.com>
2024-10-21 17:16:42 +0900

    feat: 커스텀 구분자를 이용한 숫자 추출
    문자열에서 숫자 추출
    - 1. 커스텀 구분자가 있는지 확인한다
    - 2. 커스텀 구분자가 있는 경우 커스텀 구분자를 해당 구분자로, 커스텀 구분자가 없는 경우 기본 구분자(,:)를 해당 구분자로 설정한다.
    - 3. 커스텀 구분자가 있는 경우 커스텀 구분자 영역을 제거한 문자열에서 숫자를 구분해서 추출한다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -21,9 +21,50 @@ fun getInput(): String {
 
 /**
  * 문자열에서 숫자를 List로 추출하는 함수
+ * return : 분리한 숫자들이 저장되어있는 List
  */
 fun getNumbers(input: String): List<Int> {
-    // 기본 구분자(,:) 분리
-    val numbers = input.split(",", ":").map { it.toInt() }
+    // 커스텀 구분자가 있는지 확인
+    val isValidCustom = isCustomDelimiter(input)
+    val delimiter = arrayOf(
+        // 커스텀 구분자
+        if(isValidCustom) getCustomDelimiter(input)
+        // 기본 구분자
+        else ",", ":"
+    )
+    val validInput = if(isValidCustom) input.removeCustomDelimiter() else input
+    val numbers = validInput.split(*delimiter).map { it.toInt() }
     return numbers
+}
+
+/**
+ * 문자열에서 커스텀 구분자를 확인하고 그 결과를 Boolean으로 반환하는 함수
+ * 커스텀 구분자의 조건 : "//"으로 시작해서 "\n"으로 끝난다
+ * return : 커스텀 구분자를 가지고 있는지 여부
+ */
+private fun isCustomDelimiter(input: String): Boolean {
+    val isStartCustom = input.startsWith("//")
+    val isEndCustom = input.contains("""\n""")
+    return isStartCustom && isEndCustom
+}
+
+/**
+ * 문자열에서 커스텀 구분자를 추출하는 함수
+ * 커스텀 구분자의 조건 : "//"으로 시작해서 "\n"으로 끝난다
+ * return : 추출한 커스텀 구분자
+ */
+private fun getCustomDelimiter(input: String): String {
+    val delimiterEndIndex = input.indexOf("""\n""")
+    val customDelimiter = input.substring(2, delimiterEndIndex)
+    return customDelimiter
+}
+
+/**
+ * 문자열에서 커스텀 구분자 영역을 제거하는 확장함수
+ * return : 커스텀 구분자 영역을 제거한 문자열
+ */
+private fun String.removeCustomDelimiter(): String {
+    val delimiterEndIndex = indexOf("""\n""")
+    val removeDelimiterStr = substring(delimiterEndIndex + 2)
+    return removeDelimiterStr
 }
