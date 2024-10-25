5ca129a0b0afb9fe7e596b341b34a268dec44105
giovannijunseokim <kingjunseo@gmail.com>
2024-10-25 17:00:34 +0900

    chore: remove logic controlling data from View

--- src/main/kotlin/calculator/CommandLineView.kt
+++ src/main/kotlin/calculator/CommandLineView.kt
@@ -3,12 +3,12 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 object CommandLineView : View {
+    private val viewModel = ViewModel()
+
     override fun read(): String = readLine()
     override fun show(content: String) = print(content)
     override fun sum() {
-        val input = read()
-        val numbers: List<Double> = Parser.parseToNumbers(input)
-        val result = Calculator.sum(numbers)
-        show(if (result % 1.0 == 0.0) "결과 : ${result.toInt()}" else "결과 : $result")
+        viewModel.userInput = read()
+        show(viewModel.sumOutput)
     }
 }
