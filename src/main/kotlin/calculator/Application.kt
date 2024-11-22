817efd9db6a2e7549384e62cf7aa0de42a1c580b
ijh1298 <imjh0417@gmail.com>
2024-11-22 11:06:59 +0900

    refactor: main 함수 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,54 +1,9 @@
 package calculator
 
-import camp.nextstep.edu.missionutils.Console
-
-// 문자열 유효성 검사
-fun isValidString(string: String): Boolean {
-    val isCustomSeparator = isCustomSeparator(string)
-
-    val allowedCharacters = mutableListOf(',', ':', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    if (isCustomSeparator)
-        allowedCharacters += string[2]
-
-    val checkString = if (isCustomSeparator) string.substring(startIndex = 5) else string
-    for (c in checkString)
-        if (c !in allowedCharacters)
-            return false
-    return true
-}
-
-// 숫자 추출
-fun extractNumbers(string: String): List<Int> {
-    if (string == "")
-        return emptyList()
-
-    val isCustomSeparator = isCustomSeparator(string)
-
-    val separators = mutableListOf(',', ':')
-    if (isCustomSeparator)
-        separators += string[2]
-
-    var checkString = if (isCustomSeparator) string.substring(startIndex = 5) else string
-
-    if (checkString.first() in separators) checkString = checkString.drop(1)
-    if (checkString.last() in separators) checkString = checkString.dropLast(1)
-
-    return if (checkString == "") emptyList()
-    else if (isCustomSeparator) checkString.split(separators[0], separators[1], separators[2]).map { it.toInt() }
-    else checkString.split(separators[0], separators[1]).map { it.toInt() }
-}
-
-// 연산
-fun calculate(numbers: List<Int>): Int {
-    return numbers.sum()
-}
-
+import calculator.controller.CalcController
+import calculator.view.InputView
+import calculator.view.OutputView
 
 fun main() {
-    val string = input()
-    if (!isValidString(string))
-        throw IllegalArgumentException("잘못된 값을 입력했습니다.")
-    val numbers = extractNumbers(string)
-    val result = calculate(numbers)
-    printResult(result)
+    CalcController(InputView(), OutputView()).run()
 }
